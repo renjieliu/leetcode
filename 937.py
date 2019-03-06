33a988c520b6acc60c74d7e40bf2eca5df8f25a6
renjieliu <anlrj@126.com>
2019-03-05 21:02:52 -0500

    Add files via upload

--- 937.py
+++ 937.py
@@ -1,32 +1,22 @@
 def reorderLogFiles(logs: 'List[str]'):
-    output = []
-    lst_digit=[]
+    lst_digit = []
     dict_letter = {}
-    lst_letter_key = []
     lst_letter = []
-    for i in logs :
+    for i in logs:
         t = i.split(" ")
-        if 97<=ord(t[1][0]) <= 122:  #the second word starts with letter
+        if 97 <= ord(t[1][0]) <= 122:  # the second word starts with letter
             dict_letter[" ".join(t[1:])] = t[0]
         else:
             lst_digit.append(i)
 
     for i in dict_letter.keys():
-        lst_letter_key.append(i)
+        lst_letter.append(i)
 
-    lst_letter_key.sort()
+    lst_letter.sort()
 
-    for i in lst_letter_key:
-        lst_letter.append( dict_letter[i] + ' ' + i)
+    for i in range(len(lst_letter)):
+        lst_letter[i] = dict_letter[lst_letter[i]] + ' ' + lst_letter[i]
 
-
-    for i in lst_letter:
-        output.append(i)
-
-    for i in lst_digit:
-        output.append(i)
-
-
-    return output
+    return lst_letter + lst_digit
 
 print(reorderLogFiles( ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]))
