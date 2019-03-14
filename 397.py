32fc0ee6e1a560ee0fa920bdf70034191079debd
renjieliu <anlrj@126.com>
2019-03-13 22:46:56 -0400

    Add files via upload

--- 397.py
+++ 397.py
@@ -5,7 +5,7 @@ def integerReplacement(n: int) -> int:
         if n%2==0:
             n/=2
         else:
-            if (int(math.log(n+1,2)) == math.log(n+1,2) or (n+1)%4 ==0) and n!=3:
+            if (int(math.log(n+1,2)) == math.log(n+1,2) or (n+1)%4 ==0) and n!=3: #if it can be divided by 4, it's the fastest, other than 3. as it can reach to 2 immediately.
                 n +=1
             else:
                 n -=1
