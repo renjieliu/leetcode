b2c4516dc6334500b45bb70129bc6d1c388ea522
renjieliu <anlrj@126.com>
2019-04-10 11:08:33 -0400

    Add files via upload

--- 922.py
+++ 922.py
@@ -1,20 +1,36 @@
 def sortArrayByParityII(A: 'List[int]'):
-    odd = []
-    even = []
-    output =[]
-    for i in A :
-        if i &1==0:
-            even.append(i)
+    output = [None] * len(A)
+    curr_odd = 1
+    curr_even = 0
+    for i in A:
+        if i&1==0:
+            output[curr_even] = i
+            curr_even+=2
         else:
-            odd.append(i)
-    for i in range(len(A)):
-        if i &1==0:
-            output.append(even.pop())
-        else:
-            output.append(odd.pop())
+            output[curr_odd] = i
+            curr_odd += 2
 
     return output
 
+
+    #
+    #
+    # odd = []
+    # even = []
+    # output =[]
+    # for i in A :
+    #     if i &1==0:
+    #         even.append(i)
+    #     else:
+    #         odd.append(i)
+    # for i in range(len(A)):
+    #     if i &1==0:
+    #         output.append(even.pop())
+    #     else:
+    #         output.append(odd.pop())
+    #
+    # return output
+    #
 
 
 
