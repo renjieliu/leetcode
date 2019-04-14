ecf28f06ab68d4706fa70f08b94d8ee6cd67b482
renjieliu <anlrj@126.com>
2019-04-14 13:01:56 -0400

    Add files via upload

--- 967.py
+++ 967.py
@@ -1,21 +1,19 @@
 def numsSameConsecDiff(N, K):
     #N - digits
     #K - diff
-    temp =[0,1,2,3,4,5,6,7,8,9]
+    temp = [str(_) for _ in range(10)]
     output = []
     width = 1
     iter = 0
     while width < N:
-
         length = len(temp)
         while iter < length:
             curr = temp[iter]
-
             cal_add = int(str(curr)[-1])+K
             if cal_add <10:
                 temp.append ( str(curr) + str(cal_add))
             cal_minus = int(str(curr)[-1])-K
-            if cal_minus >=0:
+            if cal_minus >=0 and cal_minus!=cal_add:
                 temp.append( str(curr) + str(cal_minus) )
 
             iter+=1
@@ -25,11 +23,14 @@ def numsSameConsecDiff(N, K):
         width+=1
 
     for i in temp:
-        if len(str(int(i)))== N:
-            output.append(int(i))
+        o = int(i)
+        if len(str(o))== N:
+            output.append(o)
 
-    return list(set(output))
+    return output
 
 print( numsSameConsecDiff(3,3))
 print( numsSameConsecDiff(9,1))
 print( numsSameConsecDiff(3,7))
+print( numsSameConsecDiff(3,0))
+print( numsSameConsecDiff(1,0))
