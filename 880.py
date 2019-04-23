41e93249d6c70c1f88dc6ef111da6f2c35417bd3
renjieliu <anlrj@126.com>
2019-04-22 22:15:56 -0400

    Add files via upload

---
+++ 880.py
@@ -0,0 +1,35 @@
+def decodeAtIndex( S: str, K: int):
+    temp = ""
+    length = 0
+    for i in range(len(S)):
+        if S[i].isdigit() == False:
+            length+=1
+            temp += S[i]
+        else:
+            length = length * int(S[i])
+
+    for c in reversed(S):
+        K %= length
+        if K == 0 and c.isalpha():
+            return c
+
+        if c.isdigit():
+            length /= int(c)
+        else:
+            length -= 1
+
+
+
+
+
+print( decodeAtIndex(S = "leet2code3", K = 10))
+print(decodeAtIndex("yyuele72abcd2cndsa5", 100))
+# print( decodeAtIndex(S = "ha22", K = 5))
+# print( decodeAtIndex(S = "a2345678999999999999999", K = 1))
+# print( decodeAtIndex(S = "czjkk9elaqwiz7s6kgvl4gjixan3ky7jfdg3kyop3husw3fm289thisef8blt7a7zr5v5lhxqpntenvxnmlq7l34ay3jaayikjps",
+#                      K = 768077956)) #c
+# print( decodeAtIndex(S = "yyuele72uthzyoeut7oyku2yqmghy5luy9qguc28ukav7an6a2bvizhph35t86qicv4gyeo6av7gerovv5lnw47954bsv2xruaej",
+#                      K = 123365626)) #l
+
+
+
