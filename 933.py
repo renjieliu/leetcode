738738888ff6cbc93fedaf78231b5e4964bccdd6
renjieliu <anlrj@126.com>
2019-04-27 13:34:10 -0400

    Add files via upload

--- 933.py
+++ 933.py
@@ -1,29 +1,17 @@
-class RecentCounter(object):
+class RecentCounter:
 
     def __init__(self):
         self.pings = []
-        self.currMax = -1
 
-    def ping(self, t):
-        """
-        :type t: int
-        :rtype: int
-        """
+    def ping(self, t: int) -> int:
         self.pings.append(t)
-        if self.pings[0] >=t-3000:
+        if self.pings[0] >= t - 3000:
             return len(self.pings)
         else:
-            start = self.currMax if self.currMax!=-1 else 0
-            for i in range(start, len(self.pings)):
-                if self.pings[i] < t-3000:
-                    self.currMax=i  #find how many need to removed...
-                else:
-                    break
-            if self.currMax ==-1:
-                return len(self.pings)
-            else:
-                return len(self.pings) - (self.currMax+1)
+            while self.pings[0] < t - 3000:
+                self.pings.pop(0)
 
+            return len(self.pings)
 
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
