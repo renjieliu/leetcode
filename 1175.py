19a4bf5763de6fd421e0d9b26db57a77cc07f0a1
renjieliu <anlrj@qq.com>
2019-09-02 12:09:29 -0400

    Add files via upload

---
+++ 1175.py
@@ -0,0 +1,31 @@
+def numPrimeArrangements(n: int):
+    def fact(n):
+        output = 1
+        for i in range(1, n+1):
+            output *= i
+        return output
+
+    def primeCnt(n):
+        if n <=1:
+            return 0
+        output = 0
+        for i in range(2,n+1):
+            prime = 1
+            for j in range(2,int(i**0.5)+1):
+                if i %j == 0:
+                    prime = 0
+                    break
+            if prime ==1:
+                output+=1
+
+
+        return output
+
+    return (fact( n- primeCnt(n)) *  fact(primeCnt(n)) ) % (10**9 + 7)
+
+print(numPrimeArrangements(100))
+print(numPrimeArrangements(1))
+print(numPrimeArrangements(12))
+print(numPrimeArrangements(5))
+print(numPrimeArrangements(77))
+
