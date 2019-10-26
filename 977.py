b412c00a79612481c5195fed1d81ff38c3cc0e94
renjieliu <anlrj@qq.com>
2019-10-25 23:57:02 -0400

    Add files via upload

--- 977.py
+++ 977.py
@@ -1,9 +1,25 @@
 def sortedSquares(A: 'List[int]'):
     output = []
-    for i in A:
-        output.append(int(i)**2)
+    i, j = 0, len(A) - 1
+    while i <= j:
+        if abs(A[i]) < abs(A[j]):
+            output.append(A[j] ** 2)
+            j -= 1
+        else:
+            output.append(A[i] ** 2)
+            i += 1
 
-    output.sort()
-    return output
+    return output[::-1]
+
+    # previous codes
+
+    # output = []
+    # for i in A:
+    #     output.append(int(i)**2)
+    #
+    # output.sort()
+    # return output
+
 
 print(sortedSquares([-4,-1,0,3,10]))
+print(sortedSquares([-7,-3,2,3,11]))
