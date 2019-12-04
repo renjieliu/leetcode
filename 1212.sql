7810cda132ac8f4195a08b77c8e2655c98fb63d4
renjieliu <anlrj@qq.com>
2019-12-04 13:49:24 -0500

    Add files via upload

---
+++ 1212.sql
@@ -0,0 +1,110 @@
+/*
+
+
+Table: Teams
+
++---------------+----------+
+| Column Name   | Type     |
++---------------+----------+
+| team_id       | int      |
+| team_name     | varchar  |
++---------------+----------+
+team_id is the primary key of this table.
+Each row of this table represents a single football team.
+Table: Matches
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| match_id      | int     |
+| host_team     | int     |
+| guest_team    | int     | 
+| host_goals    | int     |
+| guest_goals   | int     |
++---------------+---------+
+match_id is the primary key of this table.
+Each row is a record of a finished match between two different teams. 
+Teams host_team and guest_team are represented by their IDs in the teams table (team_id) and they scored host_goals and guest_goals goals respectively.
+ 
+
+You would like to compute the scores of all teams after all matches. Points are awarded as follows:
+A team receives three points if they win a match (Score strictly more goals than the opponent team).
+A team receives one point if they draw a match (Same number of goals as the opponent team).
+A team receives no points if they lose a match (Score less goals than the opponent team).
+Write an SQL query that selects the team_id, team_name and num_points of each team in the tournament after all described matches. Result table should be ordered by num_points (decreasing order). In case of a tie, order the records by team_id (increasing order).
+
+The query result format is in the following example:
+
+Teams table:
++-----------+--------------+
+| team_id   | team_name    |
++-----------+--------------+
+| 10        | Leetcode FC  |
+| 20        | NewYork FC   |
+| 30        | Atlanta FC   |
+| 40        | Chicago FC   |
+| 50        | Toronto FC   |
++-----------+--------------+
+
+Matches table:
++------------+--------------+---------------+-------------+--------------+
+| match_id   | host_team    | guest_team    | host_goals  | guest_goals  |
++------------+--------------+---------------+-------------+--------------+
+| 1          | 10           | 20            | 3           | 0            |
+| 2          | 30           | 10            | 2           | 2            |
+| 3          | 10           | 50            | 5           | 1            |
+| 4          | 20           | 30            | 1           | 0            |
+| 5          | 50           | 30            | 1           | 0            |
++------------+--------------+---------------+-------------+--------------+
+
+Result table:
++------------+--------------+---------------+
+| team_id    | team_name    | num_points    |
++------------+--------------+---------------+
+| 10         | Leetcode FC  | 7             |
+| 20         | NewYork FC   | 3             |
+| 50         | Toronto FC   | 3             |
+| 30         | Atlanta FC   | 1             |
+| 40         | Chicago FC   | 0             |
++------------+--------------+---------------+
+
+*/
+
+
+Create table  Teams (team_id int, team_name varchar(30))
+Create table  Matches (match_id int, host_team int, guest_team int, host_goals int, guest_goals int)
+Truncate table Teams
+insert into Teams (team_id, team_name) values ('10', 'Leetcode FC')
+insert into Teams (team_id, team_name) values ('20', 'NewYork FC')
+insert into Teams (team_id, team_name) values ('30', 'Atlanta FC')
+insert into Teams (team_id, team_name) values ('40', 'Chicago FC')
+insert into Teams (team_id, team_name) values ('50', 'Toronto FC')
+Truncate table Matches
+insert into Matches (match_id, host_team, guest_team, host_goals, guest_goals) values ('1', '10', '20', '30', '0')
+insert into Matches (match_id, host_team, guest_team, host_goals, guest_goals) values ('2', '30', '10', '2', '2')
+insert into Matches (match_id, host_team, guest_team, host_goals, guest_goals) values ('3', '10', '50', '5', '1')
+insert into Matches (match_id, host_team, guest_team, host_goals, guest_goals) values ('4', '20', '30', '1', '0')
+insert into Matches (match_id, host_team, guest_team, host_goals, guest_goals) values ('5', '50', '30', '1', '0')
+
+
+ 
+
+--select host.team_name, guest.team_name, M.* from 
+--Teams host left outer join Matches M
+--on host.team_id = m.host_team
+--left outer join Teams guest
+--on guest.team_id = M.guest_team 
+
+select T.team_id, team_name, num_points = isnull(sum(score), 0 ) from Teams T left outer join 
+(
+select team_id = host_team, score = case when host_goals > guest_goals then 3 when host_goals = guest_goals then 1 else 0 end from Matches
+union all
+select team_id = guest_team, scores = case when host_goals > guest_goals then 0 when host_goals = guest_goals then 1 else 3 end from Matches 
+) s on T.team_id = s.team_id
+group by T.team_id, team_name 
+order by 3 desc, 1 
+
+
+
+
+
