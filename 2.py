3552e4524f3b6d01ae158529c6cd191883d09782
renjieliu <anlrj@qq.com>
2019-12-18 10:37:23 -0500

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,95 +1,62 @@
-class Solution(object):
-	def addTwoNumbers(self, l1, l2):
-		"""
-		:type l1: ListNode
-		:type l2: ListNode
-		:rtype: ListNode
-		"""
-		input1  = ""
-		input2 = ""
-		
-		len1 = 0
-		len2 = 0
-		
-		#for x in l1:
-		for i in (l1[0], l1[-1]):
-			len1+=1
-		
-		for i in (l2[0], l2[-1]):
-			len2+=1
-		
-		
-		for i in range(len1,-1,-1):
-			input1 += str(l1[i])
-		#print (input1)
-		for j in range(len2,-1, -1):
-			input2 += str(l2[j])		
-		#print (input2)
-		sum = str(int(input1) + int(input2))
-		#print(sum)
-		r = []
-		for i in range(len(sum)-1, -1, -1):
-			r.append(int(sum[i]))
-		return r
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-a = [2,4,3]
-b = [5,6,4]
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode: 
+        #calculate to an array, and build the ListNode from the array
+        #In order to build the ListNode, need to initialize the head, then add the body
+        #Do not initiate the next node for the last node.
+        carry  = 0
+        stk_1 = [] 
+        stk_2 = []
+        while l1:
+            stk_1.append(l1.val)
+            l1=l1.next
+        while l2:
+            stk_2.append(l2.val)
+            l2=l2.next
+        short = stk_1 if len(stk_1) <= len(stk_2) else stk_2
+        long = stk_2 if short == stk_1 else stk_1
+        res = []
+        while short:
+            t = short.pop(0)+long.pop(0)+ carry 
+            carry = 1 if t >= 10 else 0
+            res.append( t- (10 if carry ==1 else 0))
+        while long: #add the remaining to the res stk
+            t = long.pop(0)+ carry 
+            carry = 1 if t >= 10 else 0
+            res.append( t- (10 if carry ==1 else 0))
+            
+        if carry ==1:
+            res.append(1)
 
-# c = Solution()
-print(a[::-1]+b[::-1])
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Solution(object):
-	# def addTwoNumbers(self, l1, l2):
-		# """
-		# :type l1: ListNode
-		# :type l2: ListNode
-		# :rtype: ListNode
-		# """
-		# input1  = ""
-		# input2 = ""
-		
-		# len1 = 0
-		# len2 = 0
-		
-		# #for x in l1:
-		# for i in (l1[0], l1[-1]):
-			# len1+=1
-		
-		# for i in (l2[0], l2[-1]):
-			# len2+=1
-		
-		
-		# for i in range(len1,-1,-1):
-			# input1 += str(l1[i])
-		# #print (input1)
-		# for j in range(len2,-1, -1):
-			# input2 += str(l2[j])		
-		# #print (input2)
-		# sum = str(int(input1) + int(input2))
-		# #print(sum)
-		# r = []
-		# for i in range(len(sum)-1, -1, -1):
-			# r.append(int(sum[i]))
-		# return r
-
-# a = [2,4,3]
-# b = [5,6,4]
-
-# c = Solution()
-# print(c.addTwoNumbers(a,b))
- 
- 
+        output = ListNode(0) #Head
+        tmp = output # body
+        for i in range(len(res)):            
+            tmp.val = res[i]
+            if i < len(res)-1 :
+                tmp.next = ListNode(0)                 
+                tmp = tmp.next
+        return output
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
