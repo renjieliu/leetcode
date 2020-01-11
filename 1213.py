ef50d793f84241f66f0750640ea66b2c085f00da
renjieliu <anlrj@qq.com>
2020-01-11 11:33:54 -0500

    Add files via upload

--- 1213.py
+++ 1213.py
@@ -1,22 +1,15 @@
 class Solution:
-    def arraysIntersection(self, arr1: 'List[int]', arr2: 'List[int]', arr3: 'List[int]') -> 'List[int]':
-        t = [] #common number in arr1 and arr2
-        output = [] #common for t and arr3
-        while arr1 and arr2:
-            if arr1[0] < arr2[0]:
-                arr1.pop(0)
-            elif arr1[0] == arr2[0]:
-                t.append(arr1.pop(0))
-                arr2.pop(0)
+    def decompressRLElist(self, nums: 'List[int]') -> 'List[int]':
+        i = 0
+        output = []
+        while i < len(nums):
+            if i%2 ==0:
+                cnt = nums[i]
             else:
-                arr2.pop(0)
-
-        while t and arr3:
-            if arr3[0] < t[0]:
-                arr3.pop(0)
-            elif t[0] < arr3[0]:
-                t.pop(0)
-            else:
-                output.append(t.pop(0))
-                arr3.pop(0)
+                j = 0
+                while j < cnt:
+                    output.append(nums[i])
+                    j+=1
+                cnt = 0
+            i+=1
         return output
