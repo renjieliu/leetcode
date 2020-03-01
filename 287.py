c78eeee9741d84e71f17728b875c7768632e7360
renjieliu <anlrj@qq.com>
2020-03-01 10:38:19 -0500

    Add files via upload

--- 287.py
+++ 287.py
@@ -1,11 +1,13 @@
-def findDuplicate(nums: 'List[int]'):
-    map = {}
-    for i in nums:
-        if i not in map:
-            map[i] = 1
-        else:
-            return i
-
-
-print(findDuplicate([1,3,4,2,2]))
-print(findDuplicate([1]))
+class Solution:
+    def findDuplicate(self, nums: 'List[int]') -> int:
+        slow, fast = 0,0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        p = 0
+        while p!=fast:
+            fast = nums[fast]
+            p = nums[p]
+        return p
