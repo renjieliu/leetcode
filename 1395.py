6b863a7f7ed1d9ef0ee91e724db8c937f29f368a
renjieliu <anlrj@qq.com>
2020-03-29 21:38:46 -0400

    Add files via upload

--- 1395.py
+++ 1395.py
@@ -1,18 +1,43 @@
 class Solution:
     def numTeams(self, rating: 'List[int]') -> int:
         cnt = 0
+        dp = []
+        for i in range(len(rating)):
+            dp.append(0)
+            for j in range(i + 1, len(rating)):
+                if rating[j] > rating[i]:
+                    dp[-1] += 1
         for i in range(len(rating)):
             for j in range(i + 1, len(rating)):
                 if rating[j] > rating[i]:
-                    for k in range(j + 1, len(rating)):
-                        if rating[k] > rating[j]:
-                            cnt += 1
+                    cnt += dp[j]  # how many number after it is larger
+        dp = []
+        for i in range(len(rating)):
+            dp.append(0)
+            for j in range(i + 1, len(rating)):
+                if rating[j] < rating[i]:
+                    dp[-1] += 1
 
-        rating = rating[::-1].copy()
         for i in range(len(rating)):
             for j in range(i + 1, len(rating)):
-                if rating[j] > rating[i]:
-                    for k in range(j + 1, len(rating)):
-                        if rating[k] > rating[j]:
-                            cnt += 1
+                if rating[j] < rating[i]:
+                    cnt += dp[j]  # how many number after it is larger
         return cnt
+
+    # O(n3)
+#         cnt = 0
+#         for i in range(len(rating)):
+#             for j in range(i+1, len(rating)):
+#                 if rating[j] > rating[i] :
+#                     for k in range(j+1,len(rating)):
+#                         if rating[k] > rating[j]:
+#                             cnt +=1
+
+#         rating  = rating[::-1].copy()
+#         for i in range(len(rating)):
+#             for j in range(i+1, len(rating)):
+#                 if rating[j] > rating[i] :
+#                     for k in range(j+1,len(rating)):
+#                         if rating[k] > rating[j]:
+#                             cnt +=1
+#         return cnt
