7b78a8f76a824122bac92998140a05c0943917fc
renjieliu <anlrj@qq.com>
2020-04-01 11:38:26 -0400

    Add files via upload

--- 136.py
+++ 136.py
@@ -1,17 +1,30 @@
-def singleNumber(nums):
-    """
-    :type nums: List[int]
-    :rtype: int
-    """
-    dict = {}
-
-    for i in nums:
-        if i not in dict:
-            dict[i] = 1
+class Solution:
+    def singleNumber(self, nums: 'List[int]') -> int:
+        if len(nums) < 2: return nums[0]
         else:
-            dict[i] += 1
+            curr = nums[0]
+            for i in range(1, len(nums)):
+                curr =curr^nums[i]
+            return curr
 
-    for k, v in dict.items():
-        if v != 2:
-            return k
 
+
+
+
+# def singleNumber(nums):
+#     """
+#     :type nums: List[int]
+#     :rtype: int
+#     """
+#     dict = {}
+#
+#     for i in nums:
+#         if i not in dict:
+#             dict[i] = 1
+#         else:
+#             dict[i] += 1
+#
+#     for k, v in dict.items():
+#         if v != 2:
+#             return k
+#
