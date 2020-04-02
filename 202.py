79e6c5537306a589305d1ccf7519de0956d99c13
renjieliu <anlrj@qq.com>
2020-04-02 09:27:05 -0400

    Add files via upload

--- 202.py
+++ 202.py
@@ -1,27 +1,16 @@
-def calc(n):
-    x=list(str(n))
-    sum = 0
-    for i in range(0, len(x)):
-        sum += int(x[i])**2
-    return sum
-
-
-def isHappy(n):
-    """
-    :type n: int
-    :rtype: bool
-    """
-    h = calc (n)
-    dict = list()
-    while h != 1:
-        if h not in dict:
-            dict.append(h)
-            h=calc(h)
-        else:
-            return False
-    else:
-        return True
-
-print(isHappy(68))
-
-
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def calc(n) :
+            output = 0
+            for c in str(n):
+                output += int(c)**2
+            return output
+        seen = []
+        curr = calc(n)
+        if curr ==1 : return True
+        while curr != 1 and curr not in seen:
+            seen.append(curr)
+            curr = calc(curr)
+            if curr==1:
+                return True
+        return False
