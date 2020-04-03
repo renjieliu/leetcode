8e4d0db2c9dfbead4d7b7436a48abb5f31ff1be1
renjieliu <anlrj@qq.com>
2020-04-03 09:28:51 -0400

    Add files via upload

--- 53.py
+++ 53.py
@@ -1,26 +1,15 @@
-def maxSubArray(nums: 'List[int]'):
-    max = nums[0]
-    for i in range(len(nums)):
-        sum = nums[i]
-        if sum > max:
-            max = sum
-
-        for j in range(i+1, len(nums)):
-            sum+= nums[j]
-            if sum > max:
-                max = sum
+def maxSubArray(self, nums: 'List[int]') -> int:
+    dp =[nums[0]]
+    for i in range(1, len(nums)):
+        dp.append(max(dp[-1]+nums[i], nums[i]))
+    return max(dp)
 
 
-
-    return max
-
-
-
-print(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
-print(maxSubArray([-2, 1]))
-print(maxSubArray([-1,0,-2]))
-
-print(maxSubArray(
-    [-2, 1, -3, 4, -1, 2, 1, -5, 4, 6, 7, 8, 9, 0, -1, -1, 2, 4, 5, -4, 4, 32, 532, 5, -325, 23, 52, 45, 2]))
+# print(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+# print(maxSubArray([-2, 1]))
+# print(maxSubArray([-1,0,-2]))
+#
+# print(maxSubArray(
+#     [-2, 1, -3, 4, -1, 2, 1, -5, 4, 6, 7, 8, 9, 0, -1, -1, 2, 4, 5, -4, 4, 32, 532, 5, -325, 23, 52, 45, 2]))
 
 
