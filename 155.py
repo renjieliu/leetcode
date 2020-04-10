4ea0937256ed753b994b441246180e0563d7aefe
renjieliu <anlrj@qq.com>
2020-04-10 12:02:52 -0400

    Add files via upload

--- 155.py
+++ 155.py
@@ -5,38 +5,27 @@ class MinStack:
         initialize your data structure here.
         """
         self.stk = []
-        self.minTrack = []
         self.minn = float('inf')
+        self.minStk = []
 
     def push(self, x: int) -> None:
         self.stk.append(x)
-        if x == self.minn:
-            self.minTrack[-1][x] += 1
-        elif x < self.minn:
-            self.minTrack.append({x: 1})
-        self.minn = min(self.minn, x)
+        self.minn = min(self.minn, x)  # compare with current minumum when push to the stack
+        self.minStk.append(self.minn)
 
     def pop(self) -> None:
-        x = self.stk[-1]
-        if x == list(self.minTrack[-1].keys())[0]:  # same as the last value of the track stack
-            self.minTrack[-1][x] -= 1
-            if self.minTrack[-1][x] == 0:
-                self.minTrack.pop(-1)
-                if len(self.minTrack) > 0:
-                    self.minn = list(self.minTrack[-1].keys())[0]
-
-        self.stk.pop(-1)
-
-        if len(self.stk) == 0 or len(self.minTrack) == 0:
-            self.stk = []
-            self.minTrack = []
+        self.stk.pop()
+        self.minStk.pop()
+        if self.minStk == []:
             self.minn = float('inf')
+        else:
+            self.minn = self.minStk[-1]
 
     def top(self) -> int:
         return self.stk[-1]
 
     def getMin(self) -> int:
-        return list(self.minTrack[-1].keys())[0]
+        return self.minStk[-1]
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
