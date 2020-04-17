da49c155b58ff257b2f0ee96bf453b6143d5a3b9
renjieliu <anlrj@qq.com>
2020-04-17 09:08:01 -0400

    Add files via upload

--- 200.py
+++ 200.py
@@ -1,48 +1,28 @@
-def numIslands_dfs(done, grid, sr, sc, output):
-    if grid[sr][sc]  == "1":
-        grid[sr][sc] = "0"
-        done[sr][sc] = 1
+class Solution:
+    def numIslands(self, grid: 'List[List[str]]') -> int:
+        def dfs(checked, grid, sr, sc):
+            if checked[sr][sc] == 1 or grid[sr][sc] == "0":
+                return None
+            else:
+                checked[sr][sc] = 1
+                for r in range(sr - 1, -1, -1):
+                    if dfs(checked, grid, r, sc) == None:
+                        break
+                for c in range(sc - 1, -1, -1):
+                    if dfs(checked, grid, sr, c) == None:
+                        break
+                for r in range(sr + 1, len(grid)):
+                    if dfs(checked, grid, r, sc) == None:
+                        break
+                for c in range(sc + 1, len(grid[0])):
+                    if dfs(checked, grid, sr, c) == None:
+                        break
 
-        #right
-        for c in range(sc+1, len(grid[0])):
-            if numIslands_dfs(done, grid, sr, c,output) ==-1:
-                break
-
-        # left
-        for c in range(sc-1, -1 , -1):
-            if numIslands_dfs(done, grid, sr, c,output) == -1:
-                break
-        # up
-        for r in range(sr-1, -1 , -1):
-            if numIslands_dfs(done, grid, r, sc,output) == -1:
-                break
-
-        # down
-        for r in range(sr+1, len(grid)):
-            if numIslands_dfs(done, grid, r, sc,output) == -1:
-                break
-
-    else:
-        return -1
-
-
-def numIslands(grid: 'List[List[str]]'):
-    done = [ [None for _ in range(len(grid[0]))] for _ in range(len(grid)) ]
-    output = grid.copy()
-    cnt = 0
-    for r in range(len(grid)):
-        for c in range(len(grid[0])):
-            if done[r][c] == None and grid[r][c] == "1":
-                cnt +=1
-                numIslands_dfs(done, grid, r, c, output)
-
-    return cnt
-
-print(numIslands([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]))
-print(numIslands([["1","1","0","0","0"],
-["1","1","0","0","0"],
-["0","0","1","0","0"],
-["0","0","0","1","1"]]))
-print(numIslands([[]]))
-
-
+        checked = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        cnt = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == "1" and checked[r][c] == 0:
+                    cnt += 1
+                    dfs(checked, grid, r, c)
+        return cnt
