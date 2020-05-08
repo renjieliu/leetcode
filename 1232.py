f1562544ee57eea3a378f338e1dc34d2e1c2b89d
renjieliu <anlrj@qq.com>
2020-05-08 09:07:52 -0400

    Add files via upload

--- 1232.py
+++ 1232.py
@@ -1,26 +1,12 @@
-def checkStraightLine(coordinates: 'List[List[int]]'):
-    p1 = coordinates[0]
-    p2 = coordinates[1]
-
-    if p2[0] == p1[0]:
-        for c in coordinates:
-            if c[0] != p1[0]:
-                return False
-
-    else:
-        k = (p2[1] - p1[1]) / (p2[0] - p1[0])
-        b = p1[1] - k* p1[0]
-        for i in coordinates:
-            if i[1] != i[0]*k + b:
-                return False
-
-
-    return True
-
-
-
-print(checkStraightLine( [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]))
-print(checkStraightLine( [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]))
-print(checkStraightLine( [[2,2], [2,3], [2,5]]))
-
-
+class Solution:
+    def checkStraightLine(self, coordinates: 'List[List[int]]') -> bool:
+        if len(coordinates) == 2: return True
+        else:
+            x = coordinates[0][0]-coordinates[1][0]
+            y = coordinates[0][1] - coordinates[1][1]
+            k = 0 if x ==0 else y/x
+            b = coordinates[0][1] if k == 0 else coordinates[0][1] - k*coordinates[0][0]
+            for c in coordinates :
+                if c[1] != k*c[0] + b:
+                    return False
+            return True
