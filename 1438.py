73e627442da3fd469de0d205cec6e2490d08954a
renjieliu <anlrj@qq.com>
2020-05-08 13:02:24 -0400

    Update 1438.py

--- 1438.py
+++ 1438.py
@@ -1,26 +1,24 @@
 class Solution:
-    def longestSubarray(self, nums: 'List[int]', limit: int) -> int:
-        minStk = []  # store the minValue loc, increasing
-        maxStk = []  # store the maxValue loc, decreasing
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        minStk = [] #store the minValue loc, increasing
+        maxStk = [] #store the maxValue loc, decreasing 
         left = 0
-        curr = 0
+        curr = 0 
         output = 0
-        while curr < len(nums):
-            while minStk and nums[curr] <= nums[minStk[-1]]:
+        for curr in range(len(nums)):
+            while minStk and nums[curr] < minStk[-1]:
                 minStk.pop()
-            while maxStk and nums[curr] >= nums[maxStk[-1]]:
+            while maxStk and nums[curr] > maxStk[-1]:
                 maxStk.pop()
-            minStk.append(curr)
-            maxStk.append(curr)
-
-            if nums[maxStk[0]] - nums[minStk[0]] > limit:  # just need to compare the max and min value in the current window
-                left += 1  # max(maxStk[0]+1,minStk[0]+1)
-                while left > maxStk[0]:
+            minStk.append(nums[curr])
+            maxStk.append(nums[curr])
+            
+            while maxStk[0] - minStk[0]> limit: # just need to compare the max and min value in the current window
+                if nums[left] == maxStk[0]:
                     maxStk.pop(0)
-                while left > minStk[0]:
+                if nums[left] == minStk[0]:
                     minStk.pop(0)
-
-            output = max(output, curr - left + 1)
-            curr += 1
-            print(left, curr)
+                left +=1 
+                
+            output = max(output, curr-left+1)
         return output
