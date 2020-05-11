811c5b33292f97fc495be0c82f37702230a9b31f
renjieliu <anlrj@qq.com>
2020-05-11 09:48:09 -0400

    Add files via upload

--- 733.py
+++ 733.py
@@ -1,53 +1,16 @@
-# def dfs_find(k, graph, output=None):
-#     if output ==None:
-#         output = []
-#     output.append(k)
-#     for i in graph[k]:
-#         dfs_find(i, graph, output)
-#
-#     return  output
-#
-#
-
-def floodFill_dfs(done: 'List[List[int]]', image: 'List[List[int]]', sr: int,  sc: int, newColor: int, originalColor:int):
-    if image[sr][sc] == originalColor and done[sr][sc] == None:
-        image[sr][sc]= newColor
-        done[sr][sc] = 1
-        for c in range(sc+1, len(image[0])): #right
-            if floodFill_dfs(done,image, sr, c, newColor, originalColor) ==-1:
-                break
-            # else:
-            #     floodFill_dfs(done, image, sr, c, newColor, originalColor)
-
-        for c in range(sc-1,-1,-1): #left
-            if floodFill_dfs(done,image, sr, c, newColor, originalColor) ==-1:
-                break
-            # else:
-            #     floodFill_dfs(done, image, sr, c, newColor, originalColor)
-
-        for r in range(sr+1, len(image)): #down
-            if floodFill_dfs(done,image, r, sc, newColor, originalColor) ==-1:
-                break
-            # else:
-            #     floodFill_dfs(done, image, r, sc, newColor, originalColor)
-
-        for r in range(sr-1, -1, -1): #up
-            if floodFill_dfs(done,image, r, sc, newColor, originalColor) ==-1:
-                break
-            # else:
-            #     floodFill_dfs(done, image, r, sc, newColor, originalColor)
-
+class Solution:
+    def floodFill(self, image: 'List[List[int]]', sr: int, sc: int, newColor: int) -> 'List[List[int]]':
+        def fill(check, image, sr, sc, newColor, target):
+            image[sr][sc] = newColor
+            check[sr][sc] = 1
+            if sr-1 >-1 and image[sr-1][sc] == target and check[sr-1][sc] == 0:
+                fill(check, image, sr-1, sc, newColor, target)
+            if sr+1<len(image) and image[sr+1][sc] == target  and check[sr+1][sc] == 0:
+                fill(check, image, sr+1, sc, newColor, target)
+            if sc-1>-1 and image[sr][sc-1] == target  and check[sr][sc-1] == 0 :
+                fill(check, image, sr, sc-1, newColor, target)
+            if sc+1 < len(image[0]) and image[sr][sc+1] == target and check[sr][sc+1] ==0:
+                fill(check, image, sr, sc+1, newColor, target)
+        check = [[0 for _ in range(len(image[0]))] for _ in range(len(image))]
+        fill(check, image, sr, sc, newColor, image[sr][sc])
         return image
-
-    else:
-        return -1
-
-
-def floodFill(image: 'List[List[int]]', sr: int, sc: int, newColor: int):
-    done = [ [ None for _ in range(len(image[0]))] for _ in range(len(image))]
-    return floodFill_dfs( done,image, sr, sc, newColor, image[sr][sc])
-
-
-
-print(floodFill([[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, newColor = 2))
-print(floodFill([[0,0,0],[0,1,1]], sr = 1, sc = 1, newColor = 1))
