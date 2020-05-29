dae23b67a58793abf131ea9a2d954b60d819b173
renjieliu <anlrj@qq.com>
2020-05-29 09:15:47 -0400

    Add files via upload

--- 207.py
+++ 207.py
@@ -1,36 +1,59 @@
-def dfs(graph, s, done):
-    if done[s] == 0:
-        done[s] = 1
-        for x in graph[s]:
-            if dfs(graph, x, done) == -1:
-                return -1
-        done[s] = 2
-        return 0
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        pre = {}
+        for c, r in prerequisites:
+            if c not in pre:
+                pre[c] = []
+            pre[c].append(r)
 
-    elif done[s] == 1:  # it is being visited
-        return -1
+        def dfs(path, pre, c, checked):
+            if c in path:
+                return False
+            else:
+                if c in pre:
+                    for x in pre[c]:
+                        if dfs(path + [c], pre, x, checked) == False:
+                            return False
+                checked[c] = 1
+                return True
 
-    elif done[s] == 2:  # it has been done before
-        return 0
+        checked = [-1 for _ in range(numCourses)]
 
+        for i in range(numCourses):
+            if checked[i] == -1 and dfs([], pre, i, checked) == False:
+                return False
+        return True
 
-def canFinish(numCourses: int, prerequisites: 'List[List[int]]') -> bool:
-    courses = {_: [] for _ in range(numCourses)}
-
-    for i, j in prerequisites:
-        courses[i].append(j)
-
-    done = [0] * numCourses
-
-    for i in courses:
-        if dfs(courses, i, done) == -1:
-            return False
-
-    return True
-
-
-print(canFinish(2, [[1,0]] ))
-print(canFinish(2, [[1,0],[0,1]] ))
-print(canFinish(3, [[0,1],[0,2],[1,2]] ))
-
-
+#OLD Approach
+# def dfs(graph, s, status):
+#     if status[s] == 1: #cycle
+#         return -1
+#     elif status[s] == 2: #visited, and no cycle
+#         return 1
+#     else:
+#         status[s] = 1
+#         for x in graph[s]:
+#             if dfs(graph, x, status) == -1:
+#                 return -1
+#
+#         status[s] = 2
+#
+#     return 1
+#
+#
+# def canFinish(numCourses: int, prerequisites: 'List[List[int]]'):
+#     courses = {_:[] for _ in range(numCourses)}
+#     for i, j in prerequisites:
+#         courses[i].append(j)
+#
+#
+#     status = [0] * numCourses
+#
+#     for i in courses:
+#         if dfs(courses, i, status) == -1:
+#             return False
+#
+#     return True
+#
+# print(canFinish(2, [[1,0],[0,1]]))
+# print(canFinish(2, [[1,0]]))
