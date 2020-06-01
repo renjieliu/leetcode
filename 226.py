7dc0e74b7b51f4f49091fad9aa8814c8af65a873
renjieliu <anlrj@qq.com>
2020-06-01 09:08:34 -0400

    Add files via upload

--- 226.py
+++ 226.py
@@ -1,21 +1,49 @@
 # Definition for a binary tree node.
 class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
-        def invert(node):
-            if node:
-                if node.left or node.right:
-                    t = node.left
-                    node.left = node.right
-                    node.right = t
-                    invert(node.left)
-                    invert(node.right)
+        def helper(node):
+            if node.left == node.right == None:
+                return None
+            else:
+                t = node.left
+                node.left = node.right
+                node.right = t
+                if node.left:
+                    helper(node.left)
+                if node.right:
+                    helper(node.right)
+        if root == None : return None
+        else:
+            helper(root)
+            return root
 
-        invert(root)
-        return root
 
+
+# Previous Solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#
+# class Solution:
+#     def invertTree(self, root: TreeNode) -> TreeNode:
+#         def invert(node):
+#             if node:
+#                 if node.left or node.right:
+#                     t = node.left
+#                     node.left = node.right
+#                     node.right = t
+#                     invert(node.left)
+#                     invert(node.right)
+#
+#         invert(root)
+#         return root
+#
