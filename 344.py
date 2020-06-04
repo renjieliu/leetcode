88edc70f08237e84203d19ce5fd69384d399b4f6
renjieliu <anlrj@qq.com>
2020-06-04 09:01:20 -0400

    Add files via upload

--- 344.py
+++ 344.py
@@ -1,7 +1,18 @@
-def reverseString(s):
-    output = ""
-    for i in range(len(s)-1, -1,-1):
-        output += s[i]
-    return  output
+class Solution:
+    def reverseString(self, s: 'List[str]') -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s) // 2):  # in-place swap
+            t = s[i]
+            s[i] = s[-(i + 1)]
+            s[-(i + 1)] = t
 
-print(reverseString("Hello"))
+# OLD solution, not optimal
+# def reverseString(s):
+#     output = ""
+#     for i in range(len(s)-1, -1,-1):
+#         output += s[i]
+#     return  output
+#
+# print(reverseString("Hello"))
