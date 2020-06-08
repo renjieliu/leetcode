1d5f931679d58d57ddb471e94b68673bee8f7a4a
renjieliu <anlrj@qq.com>
2020-06-08 09:05:06 -0400

    Add files via upload

--- 231.py
+++ 231.py
@@ -1,11 +1,33 @@
-def isPowerOfTwo(n):
-    """
-    :type n: int
-    :rtype: bool
-    """
-    if n==0:
-        return False
-    else:
-        return True if n&(n-1) == 0 else False
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return True if n > 0 and n & n - 1 == 0 else False
 
-print(isPowerOfTwo(1))
+#approach 3
+
+        # return True if n>0 and str(bin(n)).count('1') ==1 else False
+
+
+# approach 2
+#         e = 1000
+#         s = 0
+#         while s<=e:
+#             mid  = (s+e)//2
+#             if 2**mid == n :
+#                 return True
+#             else:
+#                 if 2**mid>n:
+#                     e = mid - 1
+#                 else:
+#                     s = mid +1
+#         return False
+
+# approach 1
+#         i = 0
+#         while True:
+#             if 2**i < n :
+#                 i+=1
+#             elif 2**i == n:
+#                 return True
+#             else:
+#                 return False
+
