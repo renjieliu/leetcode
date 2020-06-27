b4487fbec269860dc2d599021b136610d3ac5e04
renjieliu <anlrj@qq.com>
2020-06-27 12:58:36 -0400

    Add files via upload

--- 279.py
+++ 279.py
@@ -1,18 +1,6 @@
-def numSquares(n: int) -> int:
-    arr = [float('inf')] * (n + 1)
-    arr[0] = 0
-
-    for i in range(1, n+1):
-        arr[i] = min(arr[i-j*j] for j in range(1, int(i**0.5)+1)) + 1
-
-    return arr[n]
-
-
-print(numSquares(12))
-print(numSquares(4781))
-print(numSquares(47814))
-print(numSquares(7927))
-
-
-
-
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [0] + [float('inf')]*n
+        for i in range(1, n+1):
+            dp[i] = 1+ min( dp[i-j**2] for j in range(1, int(i**0.5)+1) )
+        return dp[n]
