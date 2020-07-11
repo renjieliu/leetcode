9f885dee83f839c62a2447441c27fafc4bbb9984
renjieliu <anlrj@qq.com>
2020-07-11 11:28:45 -0400

    Add files via upload

--- 78.py
+++ 78.py
@@ -1,30 +1,13 @@
-def combo(n, k):
-    if len(n)<=k:
-        return [n]
-
-    else:
-        output = []
-        for i in range(len(n)):
-            rest = n[:i]+n[i+1:]
-
-            for x in combo(rest,k):
-                output.append(x)
-
-    result = []
-    for i in output:
-        if i not in result:
-            result.append(i)
-
-    return result
-
-
-def subsets(nums: 'List[int]') :
-    output = []
-    for i in range(1, len(nums)+1):
-        for x in combo(nums, i):
-            output.append(x)
-    output.append([])
-    return output
-
-
-print(subsets([1,2,3]))
+class Solution:
+    def subsets(self, nums: 'List[int]') -> 'List[List[int]]':
+        def combo(output, curr, target, arr):
+            if len(curr) == target:
+                output.append(curr)
+            else:
+                for i in range(len(arr)):
+                    combo(output, curr+[arr[i]], target, arr[i+1:])
+        output = [[]]
+        for k in range(1, len(nums)+1):
+            for i, n in enumerate(nums):
+                combo(output, [n], k, nums[i+1:])
+        return output
