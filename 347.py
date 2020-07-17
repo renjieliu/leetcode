00d20d48dab9248c82359f128c39a24169d310a5
renjieliu <anlrj@qq.com>
2020-07-17 09:29:52 -0400

    Add files via upload

--- 347.py
+++ 347.py
@@ -1,47 +1,31 @@
-def findKey(map: 'dict', value):
-    output = []
-    for k, v in map.items():
-        if v == value:
-            output.append(k)
-    return output
+class Solution:
+    def topKFrequent(self, nums: 'List[int]', k: int) -> 'List[int]': #O(N)
+        hmp = {}
+        for n in nums:
+            if n not in hmp:
+                hmp[n] = 0
+            hmp[n] += 1
+        total = max(hmp.values()) + 1
+        arr = [[] for _ in range(total)]
+        for c, v in hmp.items():
+            arr[v].append(c)
+        output = []
+        for a in arr[::-1]:
+            if a:
+                output += a
+                if len(output) == k:
+                    return output
+        return output
 
 
-def topKFrequent(nums: 'List[int]', k: int):
-    map = {}
-    output = []
-    for i in nums:
-        if i not in map.keys():
-            map[i] = 0
-        map[i] += 1
+#previous approach
 
-    max = 0
-    min = float('inf')
-    for i in map.values():
-        if i > max:
-            max = i
-        if i < min:
-            min = i
-    #bucket sort
-    bucket = [None] * (max + 1)
-
-    for i in map.values():
-        bucket[i] = i
-
-    sortedlist = []
-    for i in range(len(bucket)-1,-1,-1):
-        if bucket[i] != None:
-            sortedlist.append(bucket[i])
-
-    for i in range(k):
-        for x in findKey(map, sortedlist[i]):
-            output.append(x)
-        if len(output) >=k:
-            return output
-
-    return output
-
-
-print(topKFrequent([1, 1, 1, 2, 2, 3], 2))
-print(topKFrequent([1, 1, 1, 2, 2, 3,3], 3))
-print(topKFrequent([1], 1))
-print(topKFrequent([5,3,1,1,1,3,73,1],2))
+# class Solution:
+#     def topKFrequent(self, nums: 'List[int]', k: int) -> 'List[int]': #O(nlogn)
+#         hmp = {}
+#         for n in nums:
+#             if n not in hmp:
+#                 hmp[n] = 0
+#             hmp[n]+=1
+#         t = list(hmp.keys())
+#         return sorted(t, key = lambda x : hmp[x], reverse = True)[:k]
