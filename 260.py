cc20ef09fc21525e4c14b45b65f2111636f88c4e
renjieliu <anlrj@qq.com>
2020-07-23 15:35:23 -0400

    Add files via upload

--- 260.py
+++ 260.py
@@ -1,13 +1,29 @@
-def singleNumber(nums: 'List[int]'):
-    map = {}
-    for i in nums:
-        if i not in map.keys():
-            map[i] = 0
-        map[i]+=1
+class Solution:
+    def singleNumber(self, nums: 'List[int]') -> 'List[int]': #O(n) and T(1)
+        rem = nums[0]
+        for n in nums[1:]:  # the result is the ^ of the 2 unique number
+            rem ^= n
+        diff = rem & (-rem)  # to find the which rightmote bit is different, and divide the numbers to 2 groups.
+        output = [0, 0]
+        for n in nums:
+            if n & diff == 0:  # curr bit diff from the first num
+                output[0] ^= n
+            else:  # curr bit same as the second number
+                output[1] ^= n
+        return output
 
-        if map[i] ==2:
-            del map[i]
 
-    return list(map.keys())
 
-print(singleNumber([1,2,1,3,2,5]))
+# previous appoache, O(n) and T(n)
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> List[int]:
+#         hmp = {}
+#         for n in nums:
+#             if n not in hmp:
+#                 hmp[n] = 0
+#             hmp[n] += 1
+#         output = []
+#         for k, v in hmp.items():
+#             if v == 1:
+#                 output.append(k)
+#         return output
