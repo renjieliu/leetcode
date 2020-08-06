5384c65ae6f2db8d75edc004a5be1a4d4fe9554e
renjieliu <anlrj@qq.com>
2020-08-06 09:18:56 -0400

    Add files via upload

--- 442.py
+++ 442.py
@@ -1,15 +1,27 @@
-def findDuplicates(nums):
-    """
-    :type nums: List[int]
-    :rtype: List[int]
-    """
-    dict  = {}
-    output = list()
-    for i in nums:
-        if i in dict.keys():
-            output.append(i)
-        else:
-            dict[i] = 0
-    return output
+class Solution:
+    def findDuplicates(self, nums: 'List[int]') -> 'List[int]':
+        output = []
+        for n in nums: #use n as the index to check if the same index has been marked
+            if nums[abs(n)-1] < 0 : #current number is seen before
+                output.append(abs(n))
+            else:
+                nums[abs(n)-1] *= -1
+        return output
 
-print(findDuplicates([4,3,2,7,8,2,3,1]))
+
+# previous approach
+# def findDuplicates(nums):
+#     """
+#     :type nums: List[int]
+#     :rtype: List[int]
+#     """
+#     dict  = {}
+#     output = list()
+#     for i in nums:
+#         if i in dict.keys():
+#             output.append(i)
+#         else:
+#             dict[i] = 0
+#     return output
+#
+# print(findDuplicates([4,3,2,7,8,2,3,1]))
