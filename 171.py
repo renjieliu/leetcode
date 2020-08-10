06ac6e34239e4b16f47e8608a187f51872614b4e
renjieliu <anlrj@qq.com>
2020-08-10 09:19:01 -0400

    Add files via upload

--- 171.py
+++ 171.py
@@ -1,22 +1,31 @@
-def titleToNumber(s):
-    """
-    :type s: str
-    :rtype: int
-    """
-    map = dict()
-    for i in range(65, 91):
-        map[chr(i)] = i-64
-    # if len(s) == 1:
-    #     return map[s]
-    # else:
-    pos = 0
-    sum = 0
-    for i in range(len(s)-1,-1, -1):
-        sum += map[s[i]] * (26**pos)
-        pos+=1
-    return sum
-
-print(titleToNumber("AZZ"))
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        num = lambda x: ord(x) - 64
+        output = 0
+        i = 0
+        for c in s[::-1]:
+            output += num(c) * (26**i)
+            i+=1
+        return output
 
 
-
+# Previous approach
+# def titleToNumber(s):
+#     """
+#     :type s: str
+#     :rtype: int
+#     """
+#     map = dict()
+#     for i in range(65, 91):
+#         map[chr(i)] = i-64
+#     if len(s) == 1:
+#         return map[s]
+#     else:
+#         pos = 1
+#         sum = map[s[-1]]
+#         for i in range(len(s)-1,0, -1):
+#             sum += map[s[i]] * (26 ** pos)
+#             pos+=1
+#         return sum
+#
+# print(titleToNumber("AZZ"))
