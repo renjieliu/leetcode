f77df50dcb3421b06cdf15cd4eb0cf2d61841c2b
renjieliu <anlrj@qq.com>
2020-08-15 11:27:16 -0400

    Add files via upload

--- 435.py
+++ 435.py
@@ -1,11 +1,18 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: 'List[List[int]]') -> int:
-        if intervals ==  []: return 0
+        if intervals == [] :return 0
+        arr = []
         intervals.sort(key = lambda x: x[1])
-        cnt = 1
-        curr = intervals[0]
-        for s, e in intervals:
-            if curr[1] <= s: #curr no overlapping
-                cnt += 1
-                curr = [s,e] #check the max can be grouped together
-        return len(intervals)-cnt
+        #print(intervals)
+        arr.append([intervals[0]])
+        for s, e in intervals[1:]:
+            placed = 0
+            for a in arr:
+                if s >= a[-1][1]:
+                    a.append([s,e]) #no overlap here
+                    placed = 1
+                    break
+            if placed == 0:
+                arr.append([[s,e]])
+        #print(arr)
+        return len(intervals) - max( map( lambda x:len(x), arr) ) #the most can be grouped together
