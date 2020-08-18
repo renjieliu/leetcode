f09f46a89e8fbee33e780d0732c5398184fd1a5c
renjieliu <anlrj@qq.com>
2020-08-18 09:25:52 -0400

    Add files via upload

--- 967.py
+++ 967.py
@@ -1,36 +1,19 @@
-def numsSameConsecDiff(N, K):
-    #N - digits
-    #K - diff
-    temp = [str(_) for _ in range(10)]
-    output = []
-    width = 1
-    iter = 0
-    while width < N:
-        length = len(temp)
-        while iter < length:
-            curr = temp[iter]
-            cal_add = int(str(curr)[-1])+K
-            if cal_add <10:
-                temp.append ( str(curr) + str(cal_add))
-            cal_minus = int(str(curr)[-1])-K
-            if cal_minus >=0 and cal_minus!=cal_add:
-                temp.append( str(curr) + str(cal_minus) )
+class Solution:
+    def numsSameConsecDiff(self, N: int, K: int) -> 'List[int]':
+        output = []
+        for i in range(1, 10):
+            stk = [str(i)]
+            while stk and len(stk[0]) < N:
+                curr = stk.pop(0)
+                if 0 <= int(curr[-1]) - K <= 9:
+                    stk.append(curr + str(int(curr[-1]) - K))
+                if 0 <= int(curr[-1]) + K <= 9:
+                    stk.append(curr + str(int(curr[-1]) + K))
+            for s in stk:
+                if len(s) == N:
+                    output.append(int(s))
 
-            iter+=1
+        if N == 1:
+            output.append(0)
 
-       # print(temp)
-
-        width+=1
-
-    for i in temp:
-        o = int(i)
-        if len(str(o))== N:
-            output.append(o)
-
-    return output
-
-print( numsSameConsecDiff(3,3))
-print( numsSameConsecDiff(9,1))
-print( numsSameConsecDiff(3,7))
-print( numsSameConsecDiff(3,0))
-print( numsSameConsecDiff(1,0))
+        return sorted(list(set(output)))
