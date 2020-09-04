86ae3462e8d7bea889c51265acf2f917e71225eb
renjieliu <anlrj@qq.com>
2020-09-04 09:09:30 -0400

    Add files via upload

--- 763.py
+++ 763.py
@@ -1,35 +1,25 @@
-def partitionLabels(S: str):
-    map = {}
-    temp = [0]
+class Solution:
+    def partitionLabels(self, S: str) -> 'List[int]':
+        hmp = {}
+        for i, c in enumerate(S):
+            if c not in hmp:
+                hmp[c] = []
 
-    for i in range(len(S)):
-        if S[i] not in map.keys() :
-            map[S[i]] = []
-            map[S[i]].append(i)
-        else:
-            map[S[i]].append(i)
-    #print(map)
-    splitEnd = map[S[0]][-1]
-
-    for i in range(len(S)):
-        if map[S[i]][-1]>splitEnd :
-            if map[S[i]][0] <= splitEnd: #or len(map[S[i]]) == 1:
-                splitEnd = map[S[i]][-1]
+            if len(hmp[c]) == 2:
+                hmp[c][-1] = i
             else:
-                temp.append(splitEnd+1) #+1 for the next start, makes it easier to compute the size of the array
-                splitEnd = map[S[i]][-1]
+                hmp[c].append(i)
+        output = []
+        ranges = sorted(hmp.values())
+        prev = ranges[0]
+        for curr in ranges[1:]:
+            if curr[0] > prev[-1]:  # r > prevend, then it can be cut here.
+                output.append(prev[-1] - prev[0] + 1)
+                prev = curr
+            else:
+                if curr[0] < prev[-1] and curr[-1] > prev[-1]:  # curr range is extending th prevend
+                    prev[-1] = curr[1]
 
-    temp.append(len(S)) # the last split point
-    # print(temp)
-    output = []
+        output.append(prev[-1] - prev[0] + 1)  # add the last section
+        return output
 
-    for i in range(1, len(temp)):
-        output.append(temp[i]- temp[i-1])
-
-    return output
-
-
-print(partitionLabels("ababcbacadefegdehijhklij"))
-print(partitionLabels("a"))
-print(partitionLabels("aaaabab"))
-print(partitionLabels("eaaaabaaec"))
