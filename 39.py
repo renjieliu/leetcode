54c784c4bac5556e7d7780331cf5819c00b351ff
renjieliu <anlrj@qq.com>
2020-10-02 09:30:25 -0400

    Add files via upload

--- 39.py
+++ 39.py
@@ -1,27 +1,47 @@
-def combinationSum(candidates: 'List[int]', target: int):
-    output = []
-    curr = []
-    candidates.sort()
+class Solution:
+    def combinationSum(self, candidates: 'List[int]', target: int) -> 'List[List[int]]':
+        candidates.sort()
 
-    if candidates == []:
-        return []
+        def helper(output, curr, target, arr):
+            if target == 0:
+                output.append(curr)
+            for i in range(len(arr)):
+                if arr[i] <= target:
+                    helper(output, curr + [arr[i]], target - arr[i], arr[i:])
+                else:
+                    break
 
-    def dfs(output, curr, candidates, target, currIndex = 0 ):
-        if sum(curr) == target:
-            output.append(curr.copy())
-            return 1
+        output = []
+        helper(output, [], target, candidates)
+        return output
 
-        for i in range(currIndex, len(candidates)):
-            if sum(curr) > target:
-                break
-            curr.append(candidates[i])
-            x = dfs(output, curr, candidates, target, i)
-            curr.pop()
-
-    dummy = dfs(output, curr, candidates, target, 0)
-
-
-    return output
-
-print(combinationSum(candidates = [2,3,6,7], target = 7))
+#previous solution
+# def combinationSum(candidates: 'List[int]', target: int):
+#     def dfs(output, curr, candidates, target, currIndex = 0 ):
+#         if sum(curr) == target:
+#             output.append(curr.copy())
+#             return 1
+#
+#         for i in range(currIndex, len(candidates)):
+#             if sum(curr) > target:
+#                 break
+#             curr.append(candidates[i])
+#             x = dfs(output, curr, candidates, target, i)
+#             curr.pop()
+#
+#
+#     output = []
+#     curr = []
+#     candidates.sort()
+#
+#     if candidates == []:
+#         return []
+#
+#     dummy = dfs(output, curr, candidates, target, 0) #main, calling the function.
+#
+#
+#     return output
+#
+# print(combinationSum(candidates = [2,3,6,7], target = 7))
+# print(combinationSum(candidates = [2,3,5], target = 8))
 
