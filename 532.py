19befdaddb431ffd8e439ac8ce54caeb76f69efe
renjieliu <anlrj@qq.com>
2020-10-03 10:14:16 -0400

    Add files via upload

--- 532.py
+++ 532.py
@@ -1,33 +1,54 @@
-def findPairs(nums, k):
-    """
-    :type nums: List[int]
-    :type k: int
-    :rtype: int
-    """
-    t = list()
-    g = list(nums)
-
-    if k == 0:
-        check = {}
-        count = 0
+class Solution:
+    def findPairs(self, nums: 'List[int]', k: int) -> int:
+        output = 0
+        hmp = {}
         for n in nums:
-            if n in check.keys():
-                check[n] += 1
+            if n not in hmp:
+                hmp[n] = 0
+            hmp[n] += 1
+        for v in hmp.keys():
+            if k == 0:
+                if hmp[v] > 1:
+                    output += 1
             else:
-                check[n] = 1
-        for v in check.values():
-            if v > 1:
-                count+=1
-        return count
+                if v + k in hmp:  # only check v+k, to elimindate double counting
+                    output += 1
+        return output
 
 
-    elif k < 0:
-        return 0
-    else:
-        for x in nums:
-            t.append(x + k)
-        return len(set(t) & set(nums))
 
 
-print(findPairs([1,3,1,5,4], 0 ))
-print(findPairs([1,1,1,1,1], 0 ))
+    # previous solution
+# def findPairs(nums, k):
+#     """
+#     :type nums: List[int]
+#     :type k: int
+#     :rtype: int
+#     """
+#     t = list()
+#     g = list(nums)
+#
+#     if k == 0:
+#         check = {}
+#         count = 0
+#         for n in nums:
+#             if n in check.keys():
+#                 check[n] += 1
+#             else:
+#                 check[n] = 1
+#         for v in check.values():
+#             if v > 1:
+#                 count+=1
+#         return count
+#
+#
+#     elif k < 0:
+#         return 0
+#     else:
+#         for x in nums:
+#             t.append(x + k)
+#         return len(set(t) & set(nums))
+#
+#
+# print(findPairs([1,3,1,5,4], 0 ))
+# print(findPairs([1,1,1,1,1], 0 ))
