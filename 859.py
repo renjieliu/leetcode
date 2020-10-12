b6890ab68c9ab81f38e750b704fa11191f06ea76
renjieliu <anlrj@qq.com>
2020-10-12 09:19:59 -0400

    Add files via upload

--- 859.py
+++ 859.py
@@ -1,42 +1,18 @@
-def buddyStrings(A: 'str', B: 'str'):
-    change = 0
-    a_swap_1 = ""
-    b_swap_1 = ""
-    find = 0
-    dups = 0
-    c = []
-    if len(A)!=len(B):
-        return False
-
-    else:
-        for i in A:  # check if string has all unique character.
-            if i in c:
-                dups = 1
-                break
-            else:
-                c.append(i)
-
-        for i in range(0, len(A)):
-            if A[i] !=  B[i] and a_swap_1 == "":
-                change += 1
-                a_swap_1 = A[i]
-                b_swap_1 = B[i]
-            elif A[i] != B[i] and a_swap_1 != "":
-                change +=1
-                if A[i] == b_swap_1 and B[i] ==a_swap_1:
-                    find += 1
-            if change > 2:
-                return False
-
-        if (A==B and len(A)>0 and dups  ==1 ) or (change == 2 and find==1):
-            return True
-        else:
+class Solution:
+    def buddyStrings(self, A: str, B: str) -> bool:
+        if len(A) != len(B) or len(A) < 2 or len(B) < 2 or sorted(list(A)) != sorted(list(B)):  # character unmatched
             return False
+        else:
+            cnt = 0
+            for i in range(len(A)):
+                cnt += 1 if A[i] != B[i] else 0
+            if cnt == 2:
+                return True
+            elif cnt > 2:
+                return False
+            elif cnt == 0:
+                if len(set(A)) < len(A):  # the character appears more than once in A, swap it to make it same
+                    return True
+                else:
+                    return False
 
-print(buddyStrings("ab", "ba"))
-print(buddyStrings("aa", "aa"))
-print(buddyStrings("ab", "ab"))
-print(buddyStrings("", "ab"))
-print(buddyStrings("aaaaaaabc", "aaaaaaacb"))
-print(buddyStrings("", ""))
-print(buddyStrings("abab", "abab"))
