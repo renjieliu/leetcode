db1198f60d9e09fc41be5a5deebbdb1020ab4eef
renjieliu <anlrj@qq.com>
2020-10-14 09:27:30 -0400

    Add files via upload

--- 213.py
+++ 213.py
@@ -1,31 +1,27 @@
-def rob_try(nums: 'List[int]'):
-    if len(nums) ==0:
-        return 0
-    elif len(nums) in [1,2]:
-        return max(nums)
-    else:
-        opt = [0]*3
-        opt[0] = nums[0]
-        opt[1] = max(nums[0], nums[1])
-        for i in range(2, len(nums)):
-            opt[2] = max(opt[1], opt[0] + nums[i])
-            opt[0] = opt[1]
-            opt[1] = opt[2]
+class Solution:
+    def rob(self, nums: 'List[int]') -> int:
+        def helper(arr, i, dp):
+            if dp[i] != -1:
+                return dp[i]
+            if len(arr[i:]) == 0:
+                dp[i] = 0
+                return dp[i]
+            elif len(arr[i:]) <= 2:
+                dp[i] = max(arr[i:])
+                return dp[i]
+            else:
+                a = arr[i] + helper(arr, i + 2, dp)
+                b = arr[i + 1] + helper(arr, i + 3, dp)
+                dp[i] = max(a, b)
+                return dp[i]
 
-    return opt[-1]
-
-
-def rob(nums: 'List[int]'):
-    if len(nums) ==0:
-        return 0
-    elif len(nums) in [1,2]:
-        return max(nums)
-    else:
-        return max( rob_try(nums[1:]), rob_try(nums[:-1]), rob_try(nums[1:-1]) )
-
-print(rob([2,3,2]))
-print(rob([1,2,3,1]))
-print(rob([1]))
-print(rob([4,1,2,7,5,3,1]))
-
+        if len(nums) <= 2:
+            return max(nums)
+        dp = [-1] * len(nums)
+        helper(nums[:-1], 0, dp)  # pick the first one, and the last one cannot be picked
+        output_1 = max(dp)
+        dp = [-1] * len(nums)
+        helper(nums[1:], 0, dp)  # pick the last one, and the first one cannot be picked
+        output_2 = max(dp)
+        return max(output_1, output_2)
 
