954475fa17cd91fbc5643c902564370b1200e7dd
renjieliu <anlrj@qq.com>
2020-10-23 09:07:08 -0400

    Add files via upload

--- 456.py
+++ 456.py
@@ -2,19 +2,40 @@ class Solution:
     def find132pattern(self, nums: 'List[int]') -> bool:
         if len(nums) < 3:
             return False
-        minStk = [float('inf')]  # first item does not have a number on the left
-        minn = float('inf')
-        for i in range(1, len(nums)):
-            minStk.append(min(minn, nums[i - 1]))
+        else:
+            minStk = [float('inf')]
+            for i in range(1, len(nums)):  # smallest on the left
+                minStk.append(min(nums[i - 1], minStk[-1]))
+            stk = []
+            for i in range(len(nums) - 1, -1, -1):
+                curr = -float('inf')
+                while stk and stk[-1] < nums[
+                    i]:  # find the one smaller than the current one, but larger than the one in the minStk
+                    curr = stk.pop()
+                if curr > minStk[i]:
+                    return True
+                stk.append(nums[i])
+            return False
 
-        stk = []
-        t = -float('inf')
-        for i in range(len(nums) - 1, -1, -1):
-            while stk and stk[-1] < nums[i]:  # find second largest on the right
-                t = stk.pop()
 
-            if t > minStk[i]:  # this is the last popped one
-                return True
-            stk.append(nums[i])
-
-        return False
+# previous approach
+# class Solution:
+#     def find132pattern(self, nums: 'List[int]') -> bool:
+#         if len(nums) < 3:
+#             return False
+#         minStk = [float('inf')]  # first item does not have a number on the left
+#         minn = float('inf')
+#         for i in range(1, len(nums)):
+#             minStk.append(min(minn, nums[i - 1]))
+#
+#         stk = []
+#         t = -float('inf')
+#         for i in range(len(nums) - 1, -1, -1):
+#             while stk and stk[-1] < nums[i]:  # find second largest on the right
+#                 t = stk.pop()
+#
+#             if t > minStk[i]:  # this is the last popped one
+#                 return True
+#             stk.append(nums[i])
+#
+#         return False
