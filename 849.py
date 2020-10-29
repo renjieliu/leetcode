c16fa37f38981f271104d4ba60f15291a24e8cdd
renjieliu <anlrj@qq.com>
2020-10-29 09:11:59 -0400

    Add files via upload

--- 849.py
+++ 849.py
@@ -1,34 +1,64 @@
-def maxDistToClosest(seats: 'List[int]'):
-    cnt = 0
-    max = 0
+class Solution:
+    def maxDistToClosest(self, seats: 'List[int]') -> int:
+        left_arr = []
+        right_arr = [0] * len(seats)
+        left = 0
+        for i in range(len(seats)):
+            if seats[i] == 1:
+                left = i
+            left_arr.append(i - left)
 
-    if seats[0] == 0:
-        for i in seats:
-            if i == 0:
-                max += 1
-            else:
-                break
+        right = len(seats) - 1
+        for i in range(len(seats) - 1, -1, -1):
+            if seats[i] == 1:
+                right = i
+            right_arr[i] = (right - i)
 
-    for i in seats:
-        if i == 0:
-            cnt += 1
-        else:
+        if seats[0] == 0:
+            left_arr[0] = float('inf')
+        if seats[-1] == 0:
+            right_arr[-1] = float('inf')
 
-            distance = cnt // 2 if cnt % 2 == 0 else cnt // 2 + 1
+        output = 0
+        # print(left_arr)
+        # print(right_arr)
+        for i in range(len(seats)):
+            if seats[i] != 1:
+                output = max(output, min(left_arr[i], right_arr[i]))
+        return output
 
-            if max < distance:
-                max = distance
-
-            cnt = 0
-    if i == 0 and max < cnt:  # the last seat is 0
-        max = cnt
-
-    return max
-
-
-print(maxDistToClosest( [1,0,0,0,1,0,1]))
-print(maxDistToClosest(  [1,0,0,0]))
-print(maxDistToClosest(  [0,0,0,1]))
-print(maxDistToClosest(  [0,0,0,0,1,0,0,1,1,1,0]))
-
-
+# Previous approach
+# def maxDistToClosest(seats: 'List[int]'):
+#     cnt = 0
+#     max = 0
+#
+#     if seats[0] == 0:
+#         for i in seats:
+#             if i == 0:
+#                 max += 1
+#             else:
+#                 break
+#
+#     for i in seats:
+#         if i == 0:
+#             cnt += 1
+#         else:
+#
+#             distance = cnt // 2 if cnt % 2 == 0 else cnt // 2 + 1
+#
+#             if max < distance:
+#                 max = distance
+#
+#             cnt = 0
+#     if i == 0 and max < cnt:  # the last seat is 0
+#         max = cnt
+#
+#     return max
+#
+#
+# print(maxDistToClosest( [1,0,0,0,1,0,1]))
+# print(maxDistToClosest(  [1,0,0,0]))
+# print(maxDistToClosest(  [0,0,0,1]))
+# print(maxDistToClosest(  [0,0,0,0,1,0,0,1,1,1,0]))
+#
+#
