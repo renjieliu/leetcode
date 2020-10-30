5b6154bfb695bddba0fd519ac80340f7a03f9cce
renjieliu <anlrj@qq.com>
2020-10-30 10:24:56 -0400

    Add files via upload

--- 673.py
+++ 673.py
@@ -3,34 +3,20 @@ class Solution:
         if nums == []:
             return 0
         else:
-            dp = [[1, 1]]
+            dp = [[1, 1]]  # [longest, combinations]
             for i in range(1, len(nums)):
-                m = 1  # current length
-                repeating = 0  # how many combinations
-                for j in range(i):
+                currLength = 1
+                combinations = 1
+                for j in range(i - 1, -1, -1):
                     if nums[j] < nums[i]:
-                        if dp[j][0] + 1 == m:
-                            repeating += dp[j][1]
-                        elif dp[j][0] + 1 > m:
-                            repeating = dp[j][1]
-                            m = dp[j][0] + 1
-                dp.append([m, max(1, repeating)])
-        hmp = {}
-        for i in dp:
-            if i[0] not in hmp:
-                hmp[i[0]] = 0
-            hmp[i[0]] += i[1]
+                        if dp[j][0] + 1 == currLength:
+                            combinations += dp[j][1]
+                        if dp[j][0] + 1 > currLength:
+                            combinations = dp[j][1]
+                            currLength = dp[j][0] + 1
+                dp.append([currLength, combinations])
 
-        # print(dp, hmp)
-        k = sorted(list(hmp.keys()), reverse=True)[0]
-        return hmp[k]
-
-
-
-
-
-
-
-
-
+            l = max([l for l, c in dp])  # find the max length
+            output = sum([b for a, b in dp if a == l])  # sum how many combinations
+            return output
 
