13ef7eb0ab5d35b88325c9c03a095aa4231a1454
renjieliu <anlrj@qq.com>
2020-11-03 09:00:22 -0500

    Add files via upload

--- 1446.py
+++ 1446.py
@@ -1,12 +1,15 @@
 class Solution:
     def maxPower(self, s: str) -> int:
-        currStart = 0
+        if s == "":
+            return 0
         output = 1
-        i = 1
-        while i < len(s):
-            if s[i] != s[i-1]:
-                output = max(output, i-currStart)
-                currStart = i
-            i+=1
-        output = max(output, i-currStart)
+        curr = 1
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                curr += 1
+            else:
+                curr = 1
+
+            output = max(curr, output)
+
         return output
