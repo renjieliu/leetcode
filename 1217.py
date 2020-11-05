45808b2aa0b78b3c5c671895c51dd46325aef8c0
renjieliu <anlrj@qq.com>
2020-11-05 10:15:54 -0500

    Add files via upload

--- 1217.py
+++ 1217.py
@@ -1,13 +1,9 @@
-def minCostToMoveChips(chips: 'List[int]'):
-    s1, s2  = 0, 0
-    for i in chips:
-        s1+= 0 if abs((i-1))%2 ==0 else 1  # move everything to position 1
-        s2+= 0 if abs((i - 2)) % 2 == 0 else 1 #move everything to position 2
-    return min(s1, s2)
-
-
-
-print(minCostToMoveChips(chips = [1,2,3]))
-print(minCostToMoveChips(chips = [2,2,2,3,3]))
-print(minCostToMoveChips(chips = [2,2,2,3,100000003]))
-print(minCostToMoveChips(chips = [2,2,2,3,100000003,3,100000003,3,100000003,3,100000003,3,100000003,3,100000003,3,100000003,3,100000003,3,100000003,3,100000003,3,100000003,3,100000003,3,100000003,3,100000003,3,100000003,3,100000003]))
+class Solution:
+    def minCostToMoveChips(self, position: 'List[int]') -> int:
+        odd = even = 0
+        for p in position:
+            if p%2==0:
+                even +=1
+            else:
+                odd+=1
+        return 0 if even*odd == 0 else min(even, odd)
