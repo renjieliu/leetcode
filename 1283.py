0a79f2d8ca60d2df7c3fe88282f049a9469ec8ce
renjieliu <anlrj@qq.com>
2020-11-06 09:13:43 -0500

    Add files via upload

--- 1283.py
+++ 1283.py
@@ -1,26 +1,23 @@
 class Solution:
     def smallestDivisor(self, nums: 'List[int]', threshold: int) -> int:
-        def f(arr, d):
+        def calc(arr, n):
             output = 0
             for a in arr:
-                output += a//d + (1 if a%d!=0 else 0)
+                A = a / n
+                B = a // n
+                output += B + (1 if A > B else 0)
             return output
+
+        output = float('inf')
         s = 1
         e = max(nums)
-        output = float('inf')
-        while s<=e: #binary search to find the target number
-            mid = (s+e)//2
-            t = f(nums,mid)
-            if t==threshold:
-                output= min(output,mid)
-                e = mid -1
-            elif t > threshold: #curr mid should be shifted to the right
-                s=mid+1
-            elif t<= threshold:
-                e=mid-1
-            
-        if output ==float('inf'):
-            return s
-        else:
-            return output
-        
+        while s <= e:
+            mid = s - (s - e) // 2
+
+            if calc(nums, mid) > threshold:
+                s = mid + 1
+            else:
+                output = min(mid, output)
+                e = mid - 1
+                # print(mid, calc(nums, mid))
+        return output
