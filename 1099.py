25563129b8e11c8c077323b9c19784bde883d240
renjieliu <anlrj@qq.com>
2020-11-08 10:00:21 -0500

    Add files via upload

--- 1099.py
+++ 1099.py
@@ -1,15 +1,8 @@
 class Solution:
     def twoSumLessThanK(self, A: 'List[int]', K: int) -> int:
-        i = 0
-        m = -float('inf')
-        while i < len(A):
-            if A[i] > K:
-                i+=1
-                continue
-            j = i+1
-            while j < len(A):
+        output = -1
+        for i in range(len(A)):
+            for j in range(i+1, len(A)) :
                 if A[i] + A[j] < K:
-                    m = max(m, A[i] + A[j])
-                j+=1
-            i+=1
-        return -1 if m==-float('inf') else m
+                    output = max(output,A[i] + A[j])
+        return output
