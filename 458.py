f584392b3afcd088a9f0819dc5ef2a5b4fe4d7af
renjieliu <anlrj@qq.com>
2020-11-14 10:24:06 -0500

    Add files via upload

---
+++ 458.py
@@ -0,0 +1,6 @@
+import math;
+
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        states = minutesToTest // minutesToDie + 1
+        return math.ceil(math.log(buckets) / math.log(states))
