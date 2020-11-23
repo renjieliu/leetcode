3ebbfd8ed42da81aacee7736019059c3a5314e7e
renjieliu <anlrj@qq.com>
2020-11-23 17:11:17 -0500

    Add files via upload

--- 954.py
+++ 954.py
@@ -18,13 +18,13 @@ class Solution:
                         del hmp[k]
                     if 2 * k in hmp and hmp[2 * k] == 0:
                         del hmp[2 * k]
-                elif k / 2 in hmp:  # or if k/2 is there, then we find a pair as well
-                    hmp[k] -= 1
-                    hmp[k / 2] -= 1
-                    if hmp[k] == 0:
-                        del hmp[k]
-                    if k / 2 in hmp and hmp[k / 2] == 0:
-                        del hmp[k / 2]
+                # elif k/2 in hmp: #or if k/2 is there, then we find a pair as well
+                #     hmp[k]-=1
+                #     hmp[k/2]-=1
+                #     if hmp[k] == 0:
+                #         del hmp[k]
+                #     if k/2 in hmp and hmp[k/2] == 0:
+                #         del hmp[k/2]
                 else:  # cannot find a match, then it cannot be arranged
                     return False
 
