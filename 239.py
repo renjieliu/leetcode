2946e3aee40d9a64ac06b9694d8129fb15071166
renjieliu <anlrj@qq.com>
2020-11-28 22:47:57 -0500

    Add files via upload

--- 239.py
+++ 239.py
@@ -1,12 +1,36 @@
-def maxSlidingWindow(nums: 'List[int]', k: int):
-    if k==0 and len(nums) ==0: return []
-    output = []
-    i = 0
-    for i in range(len(nums) - (k - 1)):
-        output.append(max(nums[i:i + k]))
-    return output
+class Solution:
+    def maxSlidingWindow(self, nums: 'List[int]', k: 'int') -> 'List[int]':
+        # base cases
+        n = len(nums)
+        if n * k == 0:
+            return []
+        if k == 1:
+            return nums
 
+        def clean_deque(i):
+            # remove indexes of elements not from sliding window
+            if deq and deq[0] == i - k:
+                deq.popleft()
 
-print(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3))
-print(maxSlidingWindow([], 0))
+            # remove from deq indexes of all elements
+            # which are smaller than current element nums[i]
+            while deq and nums[i] > nums[deq[-1]]:
+                deq.pop()
 
+        # init deque and output
+        deq = deque()
+        max_idx = 0
+        for i in range(k):
+            clean_deque(i)
+            deq.append(i)
+            # compute max in nums[:k]
+            if nums[i] > nums[max_idx]:
+                max_idx = i
+        output = [nums[max_idx]]
+
+        # build output
+        for i in range(k, n):
+            clean_deque(i)
+            deq.append(i)
+            output.append(nums[deq[0]])
+        return output
