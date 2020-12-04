7968991ce3f172e758ef15273c4a0f3b664067d5
renjieliu <anlrj@qq.com>
2020-12-04 08:58:43 -0500

    Add files via upload

--- 1492.py
+++ 1492.py
@@ -1,4 +1,10 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
-        arr = [i for i in range(1, n+1) if n % i == 0]
-        return -1 if k > len(arr) else arr[k-1]
+        cnt = 0
+        for i in range(1, n+1):
+            if n % i == 0:
+                cnt += 1
+                if cnt == k:
+                    return i
+        return -1
+
