7b94b7f6d39774af805435a53c2106d434fae638
renjieliu <anlrj@qq.com>
2020-12-15 09:17:00 -0500

    Add files via upload

--- 977.py
+++ 977.py
@@ -1,25 +1,6 @@
-def sortedSquares(A: 'List[int]'):
-    output = []
-    i, j = 0, len(A) - 1
-    while i <= j:
-        if abs(A[i]) < abs(A[j]):
-            output.append(A[j] ** 2)
-            j -= 1
-        else:
-            output.append(A[i] ** 2)
-            i += 1
-
-    return output[::-1]
-
-    # previous codes
-
-    # output = []
-    # for i in A:
-    #     output.append(int(i)**2)
-    #
-    # output.sort()
-    # return output
+class Solution:
+    def sortedSquares(self, nums: 'List[int]') -> 'List[int]':
+        return sorted([n**2 for n in nums])
 
 
-print(sortedSquares([-4,-1,0,3,10]))
-print(sortedSquares([-7,-3,2,3,11]))
+
