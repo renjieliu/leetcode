140b47894065af1bc2e4a7e6eef688b82530cddf
renjieliu <anlrj@qq.com>
2020-12-31 10:57:56 -0500

    Add files via upload

--- 84.py
+++ 84.py
@@ -2,11 +2,14 @@ class Solution:
     def largestRectangleArea(self, heights: 'List[int]') -> int:
         if heights == []:  return 0
         pos = [0]  # mono increasing stk. keep popping if current number is smaller than than the prev one.
+        #assuming current bar is the threshold, and check what is the max area.
+        #if there's smaller bar before it, keep the smaller bar as is
+        #if there's smaller bar after it, this bar will be removed when meet the smaller bar, as the smaller bar will become the new threshold
         output = 0
         r = 1
         while r < len(heights):
             curr = heights[r]
-            while pos and heights[pos[-1]] >= curr:
+            while pos and heights[pos[-1]] > curr:
                 P = pos.pop()  # previous max position
                 W = r if pos == [] else r - (pos[-1] + 1)
                 H = heights[P]
@@ -20,4 +23,5 @@ class Solution:
             H = heights[P]
             output = max(output, W * H)
         return output
+
 
