096486db234b5e87829822c144bbbe4d341539e3
renjieliu <anlrj@qq.com>
2021-01-01 10:36:57 -0500

    Add files via upload

--- 1640.py
+++ 1640.py
@@ -1,5 +1,5 @@
 class Solution:
-    def canFormArray(self, arr: 'List[int]', pieces:' List[List[int]]') -> bool:
+    def canFormArray(self, arr: 'List[int]', pieces: 'List[List[int]]') -> bool:
         for p in pieces:
             i = 0
             while i < len(arr) + 1 - len(p):
@@ -10,3 +10,17 @@ class Solution:
             return True
         else:
             return False
+
+#previous approach
+# class Solution:
+#     def canFormArray(self, arr: 'List[int]', pieces:' List[List[int]]') -> bool:
+#         for p in pieces:
+#             i = 0
+#             while i < len(arr) + 1 - len(p):
+#                 if arr[i: i + len(p)] == p:
+#                     arr = arr[:i] + arr[i + len(p):]
+#                 i += 1
+#         if arr == []:
+#             return True
+#         else:
+#             return False
--- 266.py
+++ 266.py
@@ -1,18 +1,11 @@
 class Solution:
     def canPermutePalindrome(self, s: str) -> bool:
-        hmp = {}
+        hmp  ={}
         for c in s:
             if c not in hmp:
                 hmp[c] = 0
             hmp[c]+=1
-        even = 0
         odd = 0
-        for v in hmp.values():
-            if v %2 ==0:
-                even +=1
-            else:
-                odd +=1
-        if odd > 1:
-            return False
-        else: # regardless of how many even are there... even 0.
-            return True
+        for k, v in hmp.items():
+            odd+= v%2
+        return odd <=1
