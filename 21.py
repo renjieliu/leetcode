c527072f22ca362c4d5fbdd46e4f23da438ae00b
renjieliu <anlrj@qq.com>
2021-01-04 09:09:16 -0500

    Add files via upload

--- 21.py
+++ 21.py
@@ -1,38 +1,46 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if l1 == None:
-            return l2
-        elif l2 == None:
-            return l1
+        if l1 == None: return l2
+        if l2 == None: return l1
+
+        if l1.val <= l2.val:
+            start = ListNode(l1.val)
+            l1 = l1.next
         else:
-            output = ListNode(l1.val if l1.val <= l2.val else l2.val)
-            if output.val == l1.val:
+            start = ListNode(l2.val)
+            l2 = l2.next
+
+        node = start
+        while l1 and l2:
+            if l1.val<=l2.val:
+                node.next = ListNode(l1.val)
+                node = node.next
                 l1 = l1.next
             else:
-                l2 = l2.next
-            node = output
-            while l1 or l2:
-                if l1 == None:
-                    node.next = ListNode(l2.val)
-                    l2 = l2.next
-                elif l2 == None:
-                    node.next = ListNode(l1.val)
-                    l1 = l1.next
-                else:
-                    if l1.val <= l2.val:
-                        node.next = ListNode(l1.val)
-                        l1 = l1.next
-                    else:
-                        node.next = ListNode(l2.val)
-                        l2 = l2.next
+                node.next = ListNode(l2.val)
                 node = node.next
-            return output
+                l2 = l2.next
+
+        node.next = l1 if l1 else l2
+        return start
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
