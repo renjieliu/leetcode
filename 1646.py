61650792d6730d29a1e5b9e293263e337d0411b8
renjieliu <anlrj@qq.com>
2021-01-15 09:35:56 -0500

    Add files via upload

--- 1646.py
+++ 1646.py
@@ -1,16 +1,16 @@
 class Solution:
     def getMaximumGenerated(self, n: int) -> int:
-        if n == 0:
+        if n == 0 :
             return 0
         elif n == 1:
             return 1
         else:
-            arr = [-1] * (n + 1)
-            arr[0] = 0
-            arr[1] = 1
-            for i in range(2, n + 1):
+            nums = [0, 1] + [0] * (n-1)
+            for i in range(2, n+1):
                 if i % 2 == 0:
-                    arr[i] = arr[i // 2]
+                    nums[i]= nums[i//2]
                 else:
-                    arr[i] = arr[i // 2 + 1] + arr[i // 2]
-            return max(arr)
+                    nums[i] = nums[i//2] + nums[i//2+1]
+            return max(nums)
+
+
