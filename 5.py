7a85e2a5a224c05bb93860ba860b40eee8711bbb
renjieliu <anlrj@qq.com>
2021-01-19 09:08:15 -0500

    Add files via upload

--- 5.py
+++ 5.py
@@ -1,30 +1,13 @@
-def isPalindrome(s:'str'):
-    if len(s) == 1 :
-        return 1
-    else:
-        if len(s)%2==0:
-            return 1 if s[:len(s)//2] == s[len(s)//2:][::-1] else 0
-        else:
-            return 1 if s[:len(s)//2] == s[len(s)//2+1:][::-1] else 0
-
-
-def longestPalindrome(s: 'str'):
-    if len(s) == 1:
-        return s
-    else:
-        temp = ""
-        max = 0
-        for i in range(len(s)):
-            for j in range(len(s) - 1, i - 1, -1):
-                if isPalindrome(s[i:j+1]) == 1:
-                    if len(s[i:j+1]) > max:
-                        temp = s[i:j+1]
-                        max = len(s[i:j+1])
-                    break  # since this is from whole to single approach, there will not be any longer Palindrome for current i
-
-        return temp
-print(longestPalindrome("babad"))
-print(longestPalindrome("cbbd"))
-print(longestPalindrome("aa"))
-
-
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) == 1: return s
+        output = ""
+        for i in range(len(s)) :
+            for j in range(len(s), i, -1):
+                curr = s[i:j]
+                if len(curr) <= len(output):
+                    break
+                elif curr == curr[::-1]:
+                    if len(curr) > len(output):
+                        output = curr
+        return output
