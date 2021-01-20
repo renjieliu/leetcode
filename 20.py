512c8be3a2ada91fa9b7d954729c6bd014ae888e
renjieliu <anlrj@qq.com>
2021-01-20 08:51:40 -0500

    Add files via upload

--- 20.py
+++ 20.py
@@ -1,58 +1,16 @@
-def isValid(s: str):
-    if len(s) ==0:
-        return True
-    map  = {}
-    c = 0
-    for i in s:
-        if i =="{":
-            if "{" not in map:
-                map["{"] = []
-            map["{"].append(c)
-
-        elif i == "[":
-            if "[" not in map:
-                map["["] = []
-            map["["].append(c)
-
-        elif i == "(":
-            if "(" not in map:
-                map["("] = []
-            map["("].append(c)
-
-        elif i == "}":
-            if "{" not in map or len(map["{"])<1 or c - map["{"][-1] >1 :
-                return False
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+        left = ['(', '[', '{']
+        for c in s:
+            if c in left:
+                stk.append(c)
             else:
-                map["{"].pop()
-                c -= 2 #once found, backtrack the current pointer by 2 position.
+                if stk:
+                    if  stk.pop() + c not in ['()', '[]', '{}']:
+                        return False
+                else:
+                    return False
+            #print(c, 'stk' ,stk)
+        return stk == []
 
-        elif i == "]":
-            if "[" not in map or len(map["["])<1 or c - map["["][-1] >1 :
-                return False
-            else:
-                map["["].pop()
-                c -= 2
-
-        elif i == ")":
-            if "(" not in map or len(map["("]) < 1 or c - map["("][-1] >1 :
-                return False
-            else:
-                map["("].pop()
-                c-=2
-
-        c+=1
-
-    summ = 0
-    for v in map.values():
-        summ+=len(v)
-    if summ !=0 :
-        return False
-    else:
-        return True
-
-
-print(isValid( "()"))
-print(isValid( "()[]{}"))
-print(isValid( "(]"))
-print(isValid( "([)]"))
-print(isValid( "{[]}"))
