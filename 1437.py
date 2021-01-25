990c85254b6858fd4a2196d86829eb133bcb2a67
renjieliu <anlrj@qq.com>
2021-01-25 08:43:40 -0500

    Add files via upload

--- 1437.py
+++ 1437.py
@@ -1,10 +1,23 @@
 class Solution:
     def kLengthApart(self, nums: 'List[int]', k: int) -> bool:
-        prev = -float('inf')
-        for i, c in enumerate(nums):
+        pre = -k-1
+        for i , c in enumerate(nums):
             if c == 1:
-                if i - prev > k:
-                    prev = i
-                else:
+                if i - pre -1 < k:
                     return False
+                else:
+                    pre = i
         return True
+
+
+#previous approach
+# class Solution:
+#     def kLengthApart(self, nums: 'List[int]', k: int) -> bool:
+#         prev = -float('inf')
+#         for i, c in enumerate(nums):
+#             if c == 1:
+#                 if i - prev > k:
+#                     prev = i
+#                 else:
+#                     return False
+#         return True
