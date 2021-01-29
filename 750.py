91a2ac8e895504d724fa49dde19ff6ccc3dfa7ed
renjieliu <anlrj@qq.com>
2021-01-29 09:46:11 -0500

    Add files via upload

--- 750.py
+++ 750.py
@@ -1,32 +1,17 @@
 class Solution:
     def countCornerRectangles(self, grid: 'List[List[int]]') -> int:
-        def one_loc(arr):  # to find all the combination of the 1 loc of the curr row
-            output = []
-            for i in range(len(arr)):
-                for j in range(i + 1, len(arr)):
-                    output.append((arr[i], arr[j]))
-            return output
-
-        pool = {}
-        if len(grid) < 2:
-            return 0
-        else:
-            for r in range(len(grid)):
-                arr = []  # the location of 1
-                for c in range(len(grid[0])):
-                    if grid[r][c] == 1:
-                        arr.append(c)
-                if len(arr) > 1:
-                    for loc in one_loc(arr):
-                        if loc not in pool:
-                            pool[loc] = 0
-                        pool[loc] += 1
-            fact = lambda x: 1 if x <= 1 else x * fact(x - 1)
-            output = 0
-            for v in pool.values():
-                if v >= 2:
-                    output += fact(v) // (fact(v - 2) * fact(2))
-            return output
-
-
-
+        arr = []
+        for r in range(len(grid)): #find the column of 1 in each row
+            arr.append(set())
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    arr[-1].add(c)
+        cnt = 0
+        fact= lambda x: 1 if x in (0,1) else fact(x-1) * x
+        combo = lambda n, r: fact(n)//(fact(r) * fact(n-r))
+        #print(arr)
+        for i in range(len(arr)): #if more than 1 column pair find a match, then it can form a rectangle
+            for j in range(i+1, len(arr)):
+                curr = len(arr[i] & arr[j])
+                cnt += combo(curr, 2) if curr > 1 else 0
+        return cnt
