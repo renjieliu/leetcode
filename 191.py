8da92bd1929c8d1f38ed1b5cbd66c7ec952d0537
renjieliu <anlrj@qq.com>
2021-02-01 09:38:54 -0500

    Add files via upload

--- 191.py
+++ 191.py
@@ -1,13 +1,23 @@
-def hammingWeight(n):
-    """
-    :type n: int
-    :rtype: int
-    """
-    sum = 0
-    for i in bin(n).replace("0b",""):
-        sum += int(i)
-    return sum
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        cnt = 0
+        while n != 0:
+            if n%2 ==1:
+                cnt +=1
+            n = n >> 1
+        return cnt
 
-print(hammingWeight(11))
-print(hammingWeight(128))
-
+# previous approach
+# def hammingWeight(n):
+#     """
+#     :type n: int
+#     :rtype: int
+#     """
+#     sum = 0
+#     for i in bin(n).replace("0b",""):
+#         sum += int(i)
+#     return sum
+#
+# print(hammingWeight(11))
+# print(hammingWeight(128))
+#
