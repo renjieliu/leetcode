8422f5fd290f9986f38c864636c2bbd0b09900e7
renjieliu <anlrj@qq.com>
2021-02-03 08:45:57 -0500

    Add files via upload

--- 141.py
+++ 141.py
@@ -6,11 +6,31 @@ class ListNode:
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        lkp = set()
-        while head:
-            if head not in lkp:
-                lkp.add(head)
-                head = head.next
-            else:
-                return True
-        return False
+        if head == None:
+            return False
+        else:
+            slow = fast = head
+            while fast.next and fast.next.next:
+                fast = fast.next.next
+                slow = slow.next
+                if fast == slow: #fast meets slow
+                    return True
+            return False
+
+# previous approach
+# # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+#
+# class Solution:
+#     def hasCycle(self, head: ListNode) -> bool:
+#         lkp = set()
+#         while head:
+#             if head not in lkp:
+#                 lkp.add(head)
+#                 head = head.next
+#             else:
+#                 return True
+#         return False
