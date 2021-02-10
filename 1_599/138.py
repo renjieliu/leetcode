8a0fecdd47589c658dda5dafb1dcc157a69e47f5
renjieliu <anlrj@qq.com>
2021-02-10 09:19:57 -0500

    Add files via upload

--- 1_599/138.py
+++ 1_599/138.py
@@ -1,11 +1,10 @@
-"""
 # Definition for a Node.
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
-"""
+
 
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
