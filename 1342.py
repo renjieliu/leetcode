a92dec548c5a4aa8ada682c841ae7fbae9f567c9
renjieliu <anlrj@qq.com>
2021-02-12 08:31:16 -0500

    Add files via upload

--- 1342.py
+++ 1342.py
@@ -1,19 +1,15 @@
 class Solution:
     def numberOfSteps (self, num: int) -> int:
-        if num ==0 : return 0
-        else:
-            cnt = 0
-            while num > 1:
-                cnt += 1+ (num%2) #if odd, then add 2, if even, add 1
-                num = num>>1
-            return cnt +1 #cnt==1, and need one more step to turn to 0
+        cnt = 0
+        while num!=0:
+            if num %2 != 0 :
+                num -= 1
+                cnt += 1
+                if num == 0 :
+                    return cnt
+            num = num >> 1
+            cnt += 1
+        return cnt
 
-#Trivial solution
-# cnt = 0
-# while num != 0 :
-#     if num%2==0:
-#         num/=2
-#     else:
-#         num-=1
-#     cnt +=1
-# return cnt
+
+
