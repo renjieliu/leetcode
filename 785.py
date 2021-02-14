c45dd7974e1240ed97a9ec53da78c18fd8a24920
renjieliu <anlrj@qq.com>
2021-02-14 09:12:58 -0500

    Add files via upload

---
+++ 785.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isBipartite(self, graph):
+        color = {}
+        for node in range(len(graph)):
+            if node not in color:
+                stack = [node]
+                color[node] = 0
+                while stack:
+                    node = stack.pop()
+                    for nei in graph[node]:
+                        if nei not in color:
+                            stack.append(nei)
+                            color[nei] = color[node] ^ 1
+                        elif color[nei] == color[node]:
+                            return False
+        return True
