a426cf5a0c6d303b4f7d2d831393b12cd4ce0c5b
renjieliu <anlrj@qq.com>
2021-02-16 08:36:30 -0500

    Add files via upload

--- 784.py
+++ 784.py
@@ -1,43 +1,26 @@
-def cartesian(a, b):
-    output = []
-    if len(a)==0: return b
-    if len(b)==0: return a
+class Solution:
+    def letterCasePermutation(self, S: str) -> 'List[str]':
+        s = S.lower()
+        arr = [] #all the letters
+        for i, c in enumerate(s):
+            if c >='a':
+                arr.append(i)
 
-    else:
-        for i in a:
-            for j in b:
-                output.append(str(i) + str(j))
-    return output
+        change = [] #the combination of letter positions, to be upper'ed
+        def combo(change, arr, curr, n):
+            if len(curr) == n:
+                change.append(curr)
+            else:
+                for i in range(len(arr)):
+                    combo(change, arr[i+1:], curr+[arr[i]], n)
 
+        for i in range(1, len(arr)+1):
+            combo(change, arr, [], i)
 
-def letterCasePermutation(S: str):
-    t =  []
-    for i in range(len(S)):
-        if 65 <=ord(S[i]) <=90:
-            t.append([S[i], chr(ord(S[i])+32)])
-
-        elif 97<=ord(S[i]) <=122:
-            t.append([S[i], chr(ord(S[i]) -32)])
-
-        else:
-            t.append([S[i]])
-    print(t)
-    if len(t) ==0: return [""]
-    elif len(t) == 1:return cartesian(t[0], '')
-    elif len(t) == 2:return cartesian(t[0], t[1])
-
-    else:
-        temp = cartesian(t[0], t[1])
-        for i in range(2, len(t)):
-            temp = cartesian(temp, t[i])
-
-    return temp
-
-
-print(letterCasePermutation(""))
-print(letterCasePermutation("a"))
-print(letterCasePermutation("C"))
-print(letterCasePermutation("8"))
-print(letterCasePermutation("a1"))
-print(letterCasePermutation("a1b"))
-print(letterCasePermutation("a1b2"))
+        res = [s] # initialize the res with all lowercase'd string
+        for curr in change:
+            temp = list(s)
+            for pos in curr:
+                temp[pos]=temp[pos].upper()
+            res.append(''.join(temp))
+        return res
