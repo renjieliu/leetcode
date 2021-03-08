d9e8a8262f40ec7b4a83c909daf5fc555d9d3744
renjieliu <anlrj@qq.com>
2021-03-08 08:21:42 -0500

    Add files via upload

--- 1_599/246.py
+++ 1_599/246.py
@@ -1,13 +1,25 @@
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
-        temp = ""
-        for n in num:
-            if n in ['0','1','8']:
-                temp+=n
-            elif n == '6':
-                temp+='9'
-            elif n == '9':
-                temp+='6'
-            else:
+        lkp = {"0":"0", "1":"1", "6":"9", "8":"8", "9":"6"}
+        curr = ""
+        for n in num[::-1]:
+            if n not in lkp:
                 return False
-        return True if temp[::-1] == num else False
+            curr+=lkp[n]
+        return curr == num
+
+
+# previous approach
+# class Solution:
+#     def isStrobogrammatic(self, num: str) -> bool:
+#         temp = ""
+#         for n in num:
+#             if n in ['0','1','8']:
+#                 temp+=n
+#             elif n == '6':
+#                 temp+='9'
+#             elif n == '9':
+#                 temp+='6'
+#             else:
+#                 return False
+#         return True if temp[::-1] == num else False
