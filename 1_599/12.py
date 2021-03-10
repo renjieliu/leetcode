1749334b5470b8aff42f1fa1150d58dfb914ba28
renjieliu <anlrj@qq.com>
2021-03-10 08:23:22 -0500

    Add files via upload

--- 1_599/12.py
+++ 1_599/12.py
@@ -1,42 +1,9 @@
-def intToRoman(num: 'int'):
-    map = { 1: "I",
-            4: "IV",
-            5: "V",
-            9: "IX",
-            10: "X",
-            40: "XL",
-            50: "L",
-            90: "XC",
-            100: "C",
-            400: "CD",
-            500: "D",
-            900: "CM",
-            1000: "M"
-           }
-    map_key =[1000,900,500,400,100,90,50,40,10,9,5,4,1]
-    output = ""
-    while num >0:
-        for i in map_key:
-            if num//i != 0:
-                output+= map[i]*(num//i)
-                num-= i * (num//i)
-
-    return output
-
-print(intToRoman(10))
-print(intToRoman(99))
-print(intToRoman(3999))
-print(intToRoman(2019))
-print(intToRoman(1231))
-
-
-
-
-
-
-
-
-
-
-
-
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        output = ""
+        lkp = [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"],[100, "C"],[90, "XC"],[50, "L"],[40, "XL"],[10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]]
+        for a, b in lkp:
+            if num >= a:
+                output += b*(num//a)
+                num %= a
+        return output
