4192a6029671c4a16073382cb531c386b0847a07
renjieliu <anlrj@qq.com>
2021-03-11 08:11:14 -0500

    Add files via upload

--- 1_599/322.py
+++ 1_599/322.py
@@ -1,19 +1,31 @@
-def coinChange(coins: 'List[int]', amount: int):
-    if amount == 0:
-        return 0
-    elif amount < min(coins):
-        return -1
-    arr = [float('inf') for _ in range(amount + 1)]
-    arr[0] = 0
-    for curr in range(len(arr)):  # this is to populate the array.
-        for c in coins:
-            if c <= curr and arr[curr - c] != float('inf'):  # the place has been calculated before
-                arr[curr] = min(arr[curr - c] + 1, arr[curr])
-
-    if arr[amount] == float('inf'):
-        return -1
-    else:
-        return arr[amount]
+class Solution:
+    def coinChange(self, coins: 'List[int]', amount: int) -> int:
+        dp = [float('inf') for _ in range(amount+1)]
+        dp[0] = 0
+        for a in range(amount+1):
+            for c in coins:
+                if a-c >= 0:
+                    dp[a] = min(1+ dp[a-c], dp[a])
+        return -1 if dp[-1] == float('inf') else dp[-1]
 
 
-print(coinChange([1, 2, 5], 11))
+# previous approach
+# def coinChange(coins: 'List[int]', amount: int):
+#     if amount ==0 :
+#         return 0
+#     elif amount < min(coins):
+#         return -1
+#
+#     arr = [float('inf') for _  in range(amount+1)]
+#     arr[0] = 0
+#     for curr in range(1, len(arr)):
+#         for c in coins:
+#             if c<=arr[curr] and arr[curr-c] != float('inf'):
+#                 arr[curr] = min(arr[curr-c]+1, arr[curr])
+#
+#     return -1 if arr[amount] == float('inf') else arr[amount]
+#
+# print(coinChange([1,3,5], 11))
+
+
+
