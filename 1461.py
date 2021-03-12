80124b6afe6f6a83acf54e6849f560028e36bc32
renjieliu <anlrj@qq.com>
2021-03-12 08:13:41 -0500

    Add files via upload

--- 1461.py
+++ 1461.py
@@ -1,7 +1,6 @@
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
-        hmp =  {}
+        lkp = set()
         for i in range(len(s)+1-k):
-            if s[i:i+k] not in hmp:
-                hmp[s[i:i+k]] = None
-        return False if len(hmp) < 2**k else True
+            lkp.add(s[i:i+k])
+        return len(lkp) == 2**k
