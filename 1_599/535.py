ee5cbc21ccba2ddc758200cecd7a1aecc57174dc
renjieliu <anlrj@qq.com>
2021-03-15 08:48:51 -0400

    Add files via upload

--- 1_599/535.py
+++ 1_599/535.py
@@ -1,33 +1,23 @@
 class Codec:
 
     def __init__(self):
-        self.decodeURL = {}
-        self.encodeURL = {}
-        self.maxID = 0
+        self.hmp_decoding = {}
+        self.cnt = 0
 
-    def encode(self, longUrl):
+    def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
-
-        :type longUrl: str
-        :rtype: str
         """
-        shortURL = "http://a.com/" + str(self.maxID)
-        self.encodeURL[longUrl] = shortURL
-        self.decodeURL[shortURL] = longUrl
-        self.maxID += 1
-        return shortURL
+        self.cnt += 1
+        self.hmp_decoding[str(self.cnt)] = longUrl
+        return str(self.cnt)
 
-    def decode(self, shortUrl):
+
+    def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
-
-        :type shortUrl: str
-        :rtype: str
         """
-        return self.decodeURL[shortUrl]
-
-codec = Codec()
-print(codec.encode("https://leetcode.com/problems/design-tinyurl"))
-print(codec.decode(codec.encode("https://leetcode.com/problems/design-tinyurl")))
+        return self.hmp_decoding[shortUrl]
 
 
-
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
