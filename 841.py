b39bd41dfe56cb73337dd8d2448ac06d805368bd
renjieliu <anlrj@qq.com>
2021-03-19 08:53:33 -0400

    Add files via upload

--- 841.py
+++ 841.py
@@ -1,42 +1,11 @@
-def dfs(k, graph, output=None):
-    if output ==None:
-        output = []
-    output.append(k)
-    for i in graph[k]:
-        if i not in output:
-           dfs(i, graph, output)
-
-    return output
-
-
-
-def canVisitAllRooms(rooms: 'List[List[int]]'):
-    if len(rooms[0]) == 0 and len(rooms) == 1:
-        return True
-    if len(rooms[0]) ==0 :
-        return False
-
-    graph = {}
-    for i in range(len(rooms)):
-        graph[i]= []
-        for j in range(len(rooms[i])):
-            graph[i].append(rooms[i][j])
-
-    total = sum([i for i in range(len(rooms))])
-
-    if sum(dfs(0, graph)) != total:
-
-        return False
-    else:
-        return True
-
-
-
-
-
-print(canVisitAllRooms( [[1],[2],[3],[]]))
-print(canVisitAllRooms( [[]]))
-print(canVisitAllRooms([[1,3],[3,0,1],[2],[0]]))
-
-
-
+class Solution:
+    def canVisitAllRooms(self, rooms: 'List[List[int]]') -> bool:
+        def dfs(seen, arr, curr):
+            for c in arr[curr]:
+                if c not in seen:
+                    seen.add(c)
+                    dfs(seen, arr, c)
+        seen = set([0])
+        dfs(seen, rooms,0)
+        #print(seen)
+        return len(seen) == len(rooms)
