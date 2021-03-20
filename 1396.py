ecb6d3aa10fb3fb6427e9eca6252145a073bc378
renjieliu <anlrj@qq.com>
2021-03-20 09:36:56 -0400

    Add files via upload

--- 1396.py
+++ 1396.py
@@ -1,22 +1,22 @@
 class UndergroundSystem:
 
     def __init__(self):
-        self.cust = {}
-        self.route = {}
+        self.customer_in = {}
+        self.station = {}
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        if id not in self.cust:  # add current customer to the stk
-            self.cust[id] = [stationName, t]
+        self.customer_in[id] = [stationName, t]  # customer in stamp
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        combo = (self.cust[id][0], stationName)  # current cust start station and current station
-        if combo not in self.route:
-            self.route[combo] = []
-        self.route[combo].append(t - self.cust[id][1])
-        del self.cust[id]
+        self.customer_in[id] += [stationName, t]  # [inStation, t, outStation, t] #record the customer's trip
+        arr = self.customer_in[id]
+        if (arr[0], arr[2]) not in self.station:
+            self.station[(arr[0], arr[2])] = []
+        self.station[(arr[0], arr[2])].append(arr[3] - arr[1])  # add the timestamp to the station combination
+        del self.customer_in[id]
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        return sum(self.route[(startStation, endStation)]) / len(self.route[(startStation, endStation)])
+        return sum(self.station[(startStation, endStation)]) / len(self.station[(startStation, endStation)])
 
 # Your UndergroundSystem object will be instantiated and called as such:
 # obj = UndergroundSystem()
