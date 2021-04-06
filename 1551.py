515bf2cc03820c7bd9594de45ad1b8dbce06649c
renjieliu <anlrj@qq.com>
2021-04-06 08:59:54 -0400

    Add files via upload

--- 1551.py
+++ 1551.py
@@ -1,10 +1,5 @@
 class Solution:
     def minOperations(self, n: int) -> int:
-        total = [2 * i + 1 for i in range(n)]
-        avg = sum(total) // n
-        output = 0
-        for t in total:
-            if t < avg:
-                output += avg - t
-            else:
-                return output
+        target = (1 + (n//2)*2 ) if n % 2 == 1 else ((1 + (n//2) * 2) + (1 + (n//2-1) * 2))//2 #to find the middle number
+        cnt = n//2
+        return (2*cnt + cnt*(cnt-1)) if target % 2 == 1 else (cnt + (cnt*(cnt-1))) #it's 1.3.5... cnt or 2.4.6..cnt
