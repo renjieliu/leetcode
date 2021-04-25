ca15e8b9cace28291e54135f2f708cbb441f0153
renjieliu <anlrj@qq.com>
2021-04-25 10:32:11 -0400

    Add files via upload

--- 1_599/48.py
+++ 1_599/48.py
@@ -3,25 +3,18 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        r = 0
-        n = len(matrix)
-        cnt = 0
-        while r <= n // 2:
-            c = r
-            while c < n - 1 - r:  # only need to pivot to the particular column for the target row.
-                curr_r = r
-                curr_c = c
-                cnt = 0
-                curr = matrix[r][c]
-                while cnt < 4:  # make 4 changes each time.
-                    nxt_r = curr_c
-                    nxt_c = n - 1 - curr_r
-                    prev = matrix[nxt_r][nxt_c]
-                    matrix[nxt_r][nxt_c] = curr
-                    curr_r = nxt_r
-                    curr_c = nxt_c
-                    curr = prev
-                    cnt += 1
-                c += 1
-
-            r += 1
+        totalRow = len(matrix) // 2
+        for r in range(totalRow):
+            for c in range(r, len(matrix) - 1 - r):  # no need to fill the last cell of the row.
+                prev_r = r
+                prev_c = c
+                prev_v = matrix[r][c]
+                for _ in range(4):
+                    rr = prev_c  # the cell to be
+                    rc = (len(matrix) - 1) - prev_r
+                    curr_v = matrix[rr][rc]
+                    matrix[rr][rc] = prev_v
+                    prev_v = curr_v
+                    prev_r = rr
+                    prev_c = rc
+                # matrix[r][c] = prev_v
