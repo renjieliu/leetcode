1bb91498c7db3e256ccb887fa3b89eaf648d9e1b
renjieliu <anlrj@qq.com>
2021-04-27 08:51:33 -0400

    Add files via upload

--- 1_599/326.py
+++ 1_599/326.py
@@ -1,6 +1,12 @@
-def isPowerOfThree(self, n):
-    """
-    :type n: int
-    :rtype: bool
-    """
-    return n > 0 and 1162261467 % n == 0  # power of 3 should be able to be divided by the largest power3 which is < Int32.
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        return n in {3**_ for _ in range(32) if 3**_ < 2**32}
+
+# previous approach
+# class Solution:
+#     def isPowerOfThree(self, n):
+#         """
+#         :type n: int
+#         :rtype: bool
+#         """
+#         return  n>0 and 1162261467%n == 0
