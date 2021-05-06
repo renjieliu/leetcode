13b212a62e582ea260fd1a0ea4eb13335e7cac2c
renjieliu <anlrj@qq.com>
2021-05-06 08:59:08 -0400

    Add files via upload

--- 630.py
+++ 630.py
@@ -1,13 +1,27 @@
 class Solution:
     def scheduleCourse(self, courses: 'List[List[int]]') -> int:
-        heap, currFinish = [], 0
-        for t, end in sorted(courses, key=lambda x: x[1]):
-            currFinish += t
-            heapq.heappush(heap, -t)
-            while currFinish > end:
-                saved = heapq.heappop(heap)
-                currFinish += saved
-        return len(heap)
+        courses.sort(key = lambda x: x[1])
+        currEnd = 0
+        arr = []
+        heapq.heapify(arr)
+        for duration, end in courses: #take the shortest duration, and end as early as possible
+            currEnd += duration
+            heapq.heappush(arr, -duration)
+            while currEnd > end and len(arr)>0:
+                currEnd += heapq.heappop(arr)
+        return len(arr)
+
+# previous approach
+# class Solution:
+#     def scheduleCourse(self, courses: 'List[List[int]]') -> int:
+#         heap, currFinish = [], 0
+#         for t, end in sorted(courses, key=lambda x: x[1]):
+#             currFinish += t
+#             heapq.heappush(heap, -t)
+#             while currFinish > end:
+#                 saved = heapq.heappop(heap)
+#                 currFinish += saved
+#         return len(heap)
 
 # previous approach
 # class Solution:
