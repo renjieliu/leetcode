15ff11982552ae6acf79b21600ad3844794cf5f7
renjieliu <anlrj@qq.com>
2021-05-16 10:07:04 -0400

    Add files via upload

---
+++ 968.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def minCameraCover(self, root: TreeNode) -> int:
+        def solve(node):
+            # 0: Strict ST; All nodes below this are covered, but not this one
+            # 1: Normal ST; All nodes below and incl this are covered - no camera
+            # 2: Placed camera; All nodes below this are covered, plus camera here
+
+            if not node: return [0, 0, float('inf')]
+            L = solve(node.left)
+            R = solve(node.right)
+
+            dp0 = L[1] + R[1]
+            dp1 = min(L[2] + min(R[1:]), R[2] + min(L[1:]))
+            dp2 = 1 + min(L) + min(R)
+
+            return [dp0, dp1, dp2]
+
+        return min(solve(root)[1:])
+
