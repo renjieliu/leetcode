a414965e2a13383659fb93430bb5130a2dc23823
renjieliu <anlrj@qq.com>
2021-05-21 08:59:49 -0400

    Add files via upload

--- 890.py
+++ 890.py
@@ -1,36 +1,19 @@
-def findKey(input: 'dict', find: 'str'):
-    for k, v in input.items():
-        if v==find:
-            return k
-    return '-1'
-
-
-
-def findAndReplacePattern(words: 'List[str]', pattern: 'str'):
-    output = []
-    for w in words:
-        if len(w) != len(pattern):
-            continue
-        else:
-            map = {}
-            cnt = 0
-            for i in range(len(pattern)):
-                if pattern[i] not in map.keys():
-                    if findKey(map, w[i]) == '-1':  # pattern and target should be 1 to 1 match.
-                        map[pattern[i]] = w[i]
-                        cnt += 1
-                else:
-                    if w[i]==map[pattern[i]]:
-                        cnt+=1
-
-            if cnt == len(pattern):
+class Solution:
+    def findAndReplacePattern(self, words: 'List[str]', pattern: str) -> 'List[str]':
+        output = []
+        for w in words:  #if str a --> pattern b, pattern b --> str a
+            hmp_a = {} #from string, map to pattern
+            hmp_b = {} #from pattern, map to string
+            good = 1
+            for i in range(len(w)):
+                if pattern[i] not in hmp_a and w[i] not in hmp_b:
+                    hmp_a[pattern[i]] = w[i]
+                    hmp_b[w[i]] = hmp_a[pattern[i]]
+                elif pattern[i] not in hmp_a or w[i] not in hmp_b or hmp_a[pattern[i]] != w[i] or hmp_b[w[i]] != hmp_a[pattern[i]]:
+                    good = 0
+                    break
+            if good == 1:
                 output.append(w)
+        return output
 
-
-
-    return output
-
-
-print(findAndReplacePattern(["abc","deq","mee","aqq","dkd","ccc"],"abb"))
-print(findAndReplacePattern(["ab","cd","fe","gg"],"ab"))
 
