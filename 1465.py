774c405c54bea57ed246fa100618dda89908fcec
renjieliu <anlrj@qq.com>
2021-06-03 08:45:51 -0400

    Add files via upload

--- 1465.py
+++ 1465.py
@@ -1,8 +1,17 @@
 class Solution:
     def maxArea(self, h: int, w: int, horizontalCuts: 'List[int]', verticalCuts: 'List[int]') -> int:
-        horizontalCuts = sorted([0] + horizontalCuts + [h])
-        verticalCuts = sorted([0] + verticalCuts + [w])
-        h_gap = [horizontalCuts[i] - horizontalCuts[i - 1] for i in range(1, len(horizontalCuts))]
-        v_gap = [verticalCuts[i] - verticalCuts[i - 1] for i in range(1, len(verticalCuts))]
-        mod = 10 ** 9 + 7
-        return max(h_gap) * max(v_gap) % mod  # adjacent max(h_gap)  * ajacent v_gap
+        mod = 10**9 + 7
+        horizontalCuts.sort()
+        horizontalCuts.insert(0, 0)
+        horizontalCuts.append(h)
+        verticalCuts.sort()
+        verticalCuts.insert(0, 0)
+        verticalCuts.append(w)
+        arr_1 = []
+        for i in range(1, len(horizontalCuts)):
+            arr_1.append(horizontalCuts[i] - horizontalCuts[i-1])
+        arr_2 = []
+        for i in range(1, len(verticalCuts)):
+            arr_2.append(verticalCuts[i] - verticalCuts[i-1])
+        return max(arr_1) * max(arr_2) % mod
+
