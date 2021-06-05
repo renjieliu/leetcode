ce7821c60b9511cace41428e682bc763bc96a83e
renjieliu <anlrj@qq.com>
2021-06-05 12:23:25 -0400

    Add files via upload

---
+++ 1383.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxPerformance(self, n: int, speed: 'List[int]', efficiency: 'List[int]', k: int) -> int:
+        eng = sorted(list(zip(speed, efficiency)), reverse=True, key=lambda x: x[1])
+        output = 0
+        totalSpeed = 0
+        pq_speed = []
+        heapq.heapify(pq_speed)
+        for s, eff in eng:  # assuming current has the lowest eff
+            if len(pq_speed) > k - 1:
+                totalSpeed -= heapq.heappop(pq_speed)
+            heapq.heappush(pq_speed, s)
+            totalSpeed += s
+            output = max(output, totalSpeed * eff)
+        return output % (10 ** 9 + 7)
+
+
