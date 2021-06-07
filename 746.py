6cd04d7d70e0fdc97b9c6c21e39b9f3bb32786fe
renjieliu <anlrj@qq.com>
2021-06-07 09:51:25 -0400

    Add files via upload

--- 746.py
+++ 746.py
@@ -1,18 +1,33 @@
-def minCostClimbingStairs(cost: 'List[int]'):
-    if len(cost) == 0:
-        return 0
-    elif len(cost) in [1, 2]:
-        return min(cost)
-
-    a0 = cost[0]
-    a1 = cost[1]
-    for i in range(2, len(cost)):
-        a2 = cost[i] + min(a0, a1)
-        a0 = a1
-        a1 = a2
-
-    return min(a1, a0)
+class Solution:
+    def minCostClimbingStairs(self, cost: 'List[int]') -> int:
+        if len(cost) <= 2 :
+            return min(cost[0], cost[-1])
+        else:
+            a = cost[0] #from 0
+            b = cost[1] #from 1
+            for i in range(2, len(cost)):
+                curr = cost[i] + min(a, b) # current is from a or b + currCost
+                a = b
+                b = curr
+            return min(a, b) # min(curr-1, curr-2) + curr
 
 
-print(minCostClimbingStairs([10, 15, 20]))
-print(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
+# previous approach
+# class Solution:
+#     def minCostClimbingStairs(self, cost: 'List[int]') -> int:
+#         def dfs(dp, pos, cost):
+#             if pos in dp:
+#                 return dp[pos]
+#             elif pos >= len(cost):
+#                 return 0
+#             else:
+#                 dp[pos] = cost[pos]
+#                 A = dfs(dp, pos+1, cost)
+#                 B = dfs(dp, pos+2, cost)
+#                 dp[pos] += min(A, B)
+#                 return dp[pos]
+#
+#         dp = {}
+#         dfs(dp, 0, cost)
+#         return min(dp[0], dp[1])
+
