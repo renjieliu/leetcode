7deb350b56fd0e9db982d5f6f888a2f285d8e330
renjieliu <anlrj@qq.com>
2021-07-03 18:10:27 -0400

    Add files via upload

--- 1_599/53.py
+++ 1_599/53.py
@@ -1,15 +1,9 @@
-def maxSubArray(self, nums: 'List[int]') -> int:
-    dp =[nums[0]]
-    for i in range(1, len(nums)):
-        dp.append(max(dp[-1]+nums[i], nums[i]))
-    return max(dp)
-
-
-# print(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
-# print(maxSubArray([-2, 1]))
-# print(maxSubArray([-1,0,-2]))
-#
-# print(maxSubArray(
-#     [-2, 1, -3, 4, -1, 2, 1, -5, 4, 6, 7, 8, 9, 0, -1, -1, 2, 4, 5, -4, 4, 32, 532, 5, -325, 23, 52, 45, 2]))
-
+class Solution:
+    def maxSubArray(self, nums: 'List[int]') -> int:
+        output = nums[0]
+        curr = nums[0]
+        for n in nums[1:]:
+            curr = max(n, curr + n)
+            output = max(output, curr)
+        return output
 
