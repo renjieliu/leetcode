31fea0e511fdac3fa96406cd1bc330258ad17af2
renjieliu <anlrj@qq.com>
2021-07-09 09:59:54 -0400

    Add files via upload

--- 1_599/300.py
+++ 1_599/300.py
@@ -1,17 +1,13 @@
-def lengthOfLIS(nums: 'List[int]'):
-    if len(nums) in [0,1]:
-        return len(nums)
-    long = [1] * len(nums)
-    for i in range(len(nums)):
-        for j in range(i):
-            if nums[j]<nums[i]:
-                long[i] = max( long[j] +1, long[i] )
+class Solution:
+    def lengthOfLIS(self, nums: 'List[int]') -> int:
+        dp = [1] * len(nums)
+        ans = 1
+        for i in range(1, len(nums)):
+            for j in range(i-1, -1, -1):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+                    if nums[j] == nums[i]-1:
+                        break
+            ans = max(ans, dp[i])
+        return ans
 
-    return max(long)
-
-print(lengthOfLIS([10,9,2,5,3,7,101,18]))
-print(lengthOfLIS([1,1]))
-print(lengthOfLIS([]))
-print(lengthOfLIS([-2, -1]))
-print(lengthOfLIS([-2, -2]))
-print(lengthOfLIS([1,3,6,7,9,4,10,5,6])) #6
