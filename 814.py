600d2d636526213373104cb1ff89d0518b4fa8f4
renjieliu <anlrj@qq.com>
2021-07-23 08:57:44 -0400

    Add files via upload

--- 814.py
+++ 814.py
@@ -1,16 +1,13 @@
 # Definition for a binary tree node.
-class TreeNode(object):
+
+class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-class Solution(object):
-    def pruneTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
+class Solution:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
         def helper(node): #to get the sum of current node and children
             if node.left == node.right == None:
                 return node.val
@@ -31,6 +28,4 @@ class Solution(object):
         helper(p)
 
         return p.left
-
-
 
