b1acda4622693b67414a188e6481d005b6d918a1
renjieliu <anlrj@qq.com>
2021-08-02 15:18:48 -0400

    Add files via upload

--- 1953.py
+++ 1953.py
@@ -1,5 +1,8 @@
 class Solution:
     def numberOfWeeks(self, milestones: 'List[int]') -> int:
+        #take one from the smallest, and take one from the current maximum
+        #if the maximum cannot be exhausted, then stop
+        #else, all the milestones can be finished
         total = sum(milestones)
         largest = max(milestones)
         rest = total - largest
@@ -7,4 +10,5 @@ class Solution:
             return total
         else: #it cannot be split, and +1 for the unfinished
             return 2*rest + 1
+
 
