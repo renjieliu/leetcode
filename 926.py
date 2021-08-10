254273ca983e7a44fab4ecfc9c0209937d980526
renjieliu <anlrj@qq.com>
2021-08-10 12:31:05 -0400

    Add files via upload

--- 926.py
+++ 926.py
@@ -1,24 +1,40 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        one = 0
-        zero = 0
-        dp = []
-        for i in s:
-            dp.append([one, 0]) #how many 1 before curr
-            if i == "1":
-                one += 1
-            else:
-                zero += 1
-        output = min(one, zero)
-        zero = 0
-        for i in range(len(s)-1, -1, -1): # how many 0's after curr
-            dp[i][1] = zero
-            zero += 1 if s[i] == "0" else 0
-
+        one = [0] * len(s) # one count before
+        zero = [0] * len(s) # zero count after
+        for i in range(1, len(s)):
+            one[i] = one[i-1] + (1 if s[i-1] == "1" else 0)
+        for i in range(len(s)-2,-1, -1 ):
+            zero[i] = zero[i+1] + (1 if s[i+1] == "0" else 0)
+        output = min(zero[0], one[-1])
         for i, c in enumerate(s):
             if c == "1":
-                output = min(output, dp[i][0]+dp[i][1]) #change prev 1's to 0 and after 0's to 1
+                output = min(output, zero[i]+one[i]) #change prev 1 to 0 or change remaining 0 to 1
         return output
+
+
+# previous approach
+# class Solution:
+#     def minFlipsMonoIncr(self, s: str) -> int:
+#         one = 0
+#         zero = 0
+#         dp = []
+#         for i in s:
+#             dp.append([one, 0]) #how many 1 before curr
+#             if i == "1":
+#                 one += 1
+#             else:
+#                 zero += 1
+#         output = min(one, zero)
+#         zero = 0
+#         for i in range(len(s)-1, -1, -1): # how many 0's after curr
+#             dp[i][1] = zero
+#             zero += 1 if s[i] == "0" else 0
+#
+#         for i, c in enumerate(s):
+#             if c == "1":
+#                 output = min(output, dp[i][0]+dp[i][1]) #change prev 1's to 0 and after 0's to 1
+#         return output
 
 
 # previous approach
