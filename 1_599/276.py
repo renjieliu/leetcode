6b09fc2c3ee63b74dc82710370d741796993b5ba
renjieliu <anlrj@qq.com>
2021-08-11 16:56:09 -0400

    Add files via upload

--- 1_599/276.py
+++ 1_599/276.py
@@ -1,17 +1,37 @@
 class Solution:
     def numWays(self, n: int, k: int) -> int:
-        if n == 1 :
+        if n == 1:
             return k
-        elif n == 2:
-            return k*k
+        elif n == 2 :
+            return k ** 2
         else:
-            prevprev = k
-            prev = k*k
-            for i in range(3, n+1):
-                curr = (k-1) * prev + (k-1) * prevprev # different from the previous + different from prevprev
+            prevprev = k #first fence
+            prev = k*k #second fence
+            for i in range(3, n+1): #up to curr, how many possible ways to paint
+                curr = (k-1) * prevprev + (k-1) * prev #same as prevprev, then k-1. same as prev then k-1
                 prevprev = prev
                 prev = curr
             return curr
+
+
+
+
+
+# previous approach
+# class Solution:
+#     def numWays(self, n: int, k: int) -> int:
+#         if n == 1 :
+#             return k
+#         elif n == 2:
+#             return k*k
+#         else:
+#             prevprev = k
+#             prev = k*k
+#             for i in range(3, n+1):
+#                 curr = (k-1) * prev + (k-1) * prevprev # different from the previous + different from prevprev
+#                 prevprev = prev
+#                 prev = curr
+#             return curr
 
 # previous approach
 # class Solution:
