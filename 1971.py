8587ad2d64ed02859c4f5bc89ba5d2dced30c17b
renjieliu <anlrj@qq.com>
2021-08-26 12:54:17 -0400

    Add files via upload

--- 1971.py
+++ 1971.py
@@ -1,18 +1,40 @@
-class Solution:  # union find approach
+class Solution:
     def validPath(self, n: int, edges: 'List[List[int]]', start: int, end: int) -> bool:
         def find(root, v):
-            if root[v] != v:
-                root[v] = find(root, root[v])
+            if root[v]!=v:
+                root[v] = find(root,  root[v])
             return root[v]
 
-        root = list(range(n))
-        for n1, n2 in edges:
-            p_a = find(root, n1)
-            p_b = find(root, n2)
-            if p_a != p_b:
+        def union(root, a, b):
+            p_a = find(root, a)
+            p_b = find(root, b)
+            if p_a!=p_b:
                 root[p_b] = p_a
 
+        root = [_ for _ in range(n)]
+
+        for a, b in edges:
+            union(root, a, b)
+
         return find(root, start) == find(root, end)
+
+
+# previous approach
+# class Solution:  # union find approach
+#     def validPath(self, n: int, edges: 'List[List[int]]', start: int, end: int) -> bool:
+#         def find(root, v):
+#             if root[v] != v:
+#                 root[v] = find(root, root[v])
+#             return root[v]
+#
+#         root = list(range(n))
+#         for n1, n2 in edges:
+#             p_a = find(root, n1)
+#             p_b = find(root, n2)
+#             if p_a != p_b:
+#                 root[p_b] = p_a
+#
+#         return find(root, start) == find(root, end)
 
 # class Solution: #BFS approach
 #     def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
@@ -41,11 +63,5 @@ class Solution:  # union find approach
 #                             nxt.append(x)
 #             stk = nxt
 #         return False
-
-
-
-
-
-
 
 
