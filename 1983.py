0c1aa738faa6bc1c1aef9e30581bdf110a8826a9
renjieliu <anlrj@qq.com>
2021-08-28 19:07:49 -0400

    Add files via upload

---
+++ 1983.py
@@ -0,0 +1,13 @@
+class Solution:
+    def widestPairOfIndices(self, nums1: 'List[int]', nums2: 'List[int]') -> int:
+        diff = {0: -1}
+        output = s1 = s2 = 0
+        for loc, (a, b) in enumerate(zip(nums1, nums2)):
+            s1 += a  # prefix of nums1
+            s2 += b  # prefix of nums2
+            if s2 - s1 not in diff:  # total of nums2 - total of nums1
+                diff[s2 - s1] = loc
+            else:
+                output = max(output, loc - diff[s2 - s1])  # if s2-s1 is met before, current gap is equal.
+        return output
+
