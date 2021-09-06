363f643e0761d22299169d34d9c7ce0d431e985e
renjieliu <anlrj@qq.com>
2021-09-06 17:59:08 -0400

    Add files via upload

--- 1629.py
+++ 1629.py
@@ -1,14 +1,32 @@
 class Solution:
     def slowestKey(self, releaseTimes: 'List[int]', keysPressed: str) -> str:
-        curr = releaseTimes[0]
+        prev = 0
         output = keysPressed[0]
-        for i in range(1, len(keysPressed)):
-            diff = releaseTimes[i] - releaseTimes[i - 1]
-            if diff > curr:
-                curr = diff
+        currMax = releaseTimes[0]
+        for i in range(len(releaseTimes)):
+            if releaseTimes[i] - prev > currMax:
+                currMax = max(currMax, releaseTimes[i] - prev)
                 output = keysPressed[i]
-            elif diff == curr:
-                output = max(keysPressed[i], output)
+            elif releaseTimes[i] - prev == currMax:
+                output = max(output, keysPressed[i])
+            prev = releaseTimes[i]
         return output
 
+
+
+
+# previous approach
+# class Solution:
+#     def slowestKey(self, releaseTimes: 'List[int]', keysPressed: str) -> str:
+#         curr = releaseTimes[0]
+#         output = keysPressed[0]
+#         for i in range(1, len(keysPressed)):
+#             diff = releaseTimes[i] - releaseTimes[i - 1]
+#             if diff > curr:
+#                 curr = diff
+#                 output = keysPressed[i]
+#             elif diff == curr:
+#                 output = max(keysPressed[i], output)
+#         return output
+#
 
