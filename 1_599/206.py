499a0dfbff7715b47d8492d6836ac68145bddb1e
renjieliu <anlrj@qq.com>
2021-09-07 08:54:09 -0400

    Add files via upload

--- 1_599/206.py
+++ 1_599/206.py
@@ -1,35 +1,38 @@
 # Definition for singly-linked list.
 class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        def flat(node, lst):
-            while node:
-                lst.append(node.val)
-                node = node.next
+    def reverseList(self, head: 'Optional[ListNode]') -> 'Optional[ListNode]':
+        def helper(node):
+            if node.next == None:
+                return node
+            res = helper(node.next)
+            node.next.next = node
+            node.next = None
+            return res
+        return helper(head) if head else None
 
-        if head == None:
-            return None
-        else:
-            lst = []
-            flat(head, lst)
-            output = ListNode(lst.pop())
-            node = output
-            while lst:
-                node.next = ListNode(lst.pop())
-                node = node.next
-            return output
 
-# Below is the recursive approach
 
-#         def flat(node,lst):
-#             if node:
+# previous approach
+# # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+#
+# class Solution:
+#     def reverseList(self, head: ListNode) -> ListNode:
+#         def flat(node, lst):
+#             while node:
 #                 lst.append(node.val)
-#                 flat(node.next, lst)
-#         if head== None :return None
+#                 node = node.next
+#
+#         if head == None:
+#             return None
 #         else:
 #             lst = []
 #             flat(head, lst)
@@ -39,3 +42,20 @@ class Solution:
 #                 node.next = ListNode(lst.pop())
 #                 node = node.next
 #             return output
+#
+# # Below is the recursive approach
+#
+# #         def flat(node,lst):
+# #             if node:
+# #                 lst.append(node.val)
+# #                 flat(node.next, lst)
+# #         if head== None :return None
+# #         else:
+# #             lst = []
+# #             flat(head, lst)
+# #             output = ListNode(lst.pop())
+# #             node = output
+# #             while lst:
+# #                 node.next = ListNode(lst.pop())
+# #                 node = node.next
+# #             return output
