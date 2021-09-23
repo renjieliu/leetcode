d7614dcb248b43ce98f2d679f808b9d9cd6226ef
renjieliu <anlrj@qq.com>
2021-09-23 09:19:18 -0400

    Add files via upload

--- 1328.py
+++ 1328.py
@@ -1,13 +1,36 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
-        if len(palindrome) == 1:
-            return ""
-        i = 0
-        while i < len(palindrome):
-            t = palindrome[:i] + 'a' + palindrome[i + 1:]  # replace current to 'a'
-            if t != t[::-1]:  # still palindrome, need to continue
-                return t
-            i += 1
-        return palindrome[:-1] + 'b'
+        s = palindrome
+        ifNotPalin = lambda x: True if x != x[::-1] else False
+        for i in range(len(s)): #replace from left to right - with ascii lower than curr
+            c = s[i]
+            for j in range(ord('a'), ord(c)):
+                tmp = s[:i] + chr(j) + s[i+1:]
+                if ifNotPalin(tmp):
+                    return tmp
+
+        for i in range(len(s)-1, -1, -1): #replace from right to left, with ascii higher than curr
+            c = s[i]
+            for j in range(ord(c), ord('z')+1):
+                tmp = s[:i] + chr(j) + s[i+1:]
+                if ifNotPalin(tmp):
+                    return tmp
+
+        return ""
 
 
+
+# previous approach
+# class Solution:
+#     def breakPalindrome(self, palindrome: str) -> str:
+#         if len(palindrome) == 1:
+#             return ""
+#         i = 0
+#         while i < len(palindrome):
+#             t = palindrome[:i] + 'a' + palindrome[i + 1:]  # replace current to 'a'
+#             if t != t[::-1]:  # still palindrome, need to continue
+#                 return t
+#             i += 1
+#         return palindrome[:-1] + 'b'
+#
+#
