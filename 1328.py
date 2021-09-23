db1086d3e1abbfecd53e1308c8be7af8f38735d1
renjieliu <anlrj@qq.com>
2021-09-23 09:29:58 -0400

    Add files via upload

--- 1328.py
+++ 1328.py
@@ -1,22 +1,33 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
         s = palindrome
-        ifNotPalin = lambda x: True if x != x[::-1] else False
-        for i in range(len(s)): #replace from left to right - with ascii lower than curr
-            c = s[i]
-            for j in range(ord('a'), ord(c)):
-                tmp = s[:i] + chr(j) + s[i+1:]
-                if ifNotPalin(tmp):
-                    return tmp
+        for i, c in enumerate(s):
+            t = s[:i] + 'a' + s[i+1:]
+            if t != t[::-1]:
+                return t
+        return "" if len(s) <= 1 else s[:-1]+'b'
 
-        for i in range(len(s)-1, -1, -1): #replace from right to left, with ascii higher than curr
-            c = s[i]
-            for j in range(ord(c), ord('z')+1):
-                tmp = s[:i] + chr(j) + s[i+1:]
-                if ifNotPalin(tmp):
-                    return tmp
 
-        return ""
+# previous approach
+# class Solution:
+#     def breakPalindrome(self, palindrome: str) -> str:
+#         s = palindrome
+#         ifNotPalin = lambda x: True if x != x[::-1] else False
+#         for i in range(len(s)): #replace from left to right - with ascii lower than curr
+#             c = s[i]
+#             for j in range(ord('a'), ord(c)):
+#                 tmp = s[:i] + chr(j) + s[i+1:]
+#                 if ifNotPalin(tmp):
+#                     return tmp
+#
+#         for i in range(len(s)-1, -1, -1): #replace from right to left, with ascii higher than curr
+#             c = s[i]
+#             for j in range(ord(c), ord('z')+1):
+#                 tmp = s[:i] + chr(j) + s[i+1:]
+#                 if ifNotPalin(tmp):
+#                     return tmp
+#
+#         return ""
 
 
 
