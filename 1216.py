eb3572df00a34c73ea072a94e7ae3758d4590135
renjieliu <anlrj@qq.com>
2021-10-08 09:39:08 -0400

    Add files via upload

---
+++ 1216.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isValidPalindrome(self, s: str, k: int) -> bool:
+        def lcs(a, b): #to find the longest common sequence of a and b
+            dp = [[0 for _ in range(len(b)+1)] for _ in range(len(a)+1)]
+            for r in range(1, len(a)+1):
+                for c in range(1, len(b)+1):
+                    dp[r][c] = max(dp[r-1][c-1] + (a[r-1] == b[c-1]), dp[r][c-1], dp[r-1][c])
+            return dp[-1][-1]
+        return len(s) - lcs(s, s[::-1]) <= k
+
