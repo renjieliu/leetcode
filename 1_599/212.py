5bbbe5cdc32dc55a70f0db8c8db00a42f40a4e31
renjieliu <anlrj@qq.com>
2021-10-09 16:33:52 -0400

    Add files via upload

--- 1_599/212.py
+++ 1_599/212.py
@@ -1,6 +1,5 @@
 class Solution:
     def findWords(self, board: 'List[List[str]]', words: 'List[str]') -> 'List[str]':
-        end = '$'
         trie = {}
         for w in words:
             curr = trie
@@ -8,44 +7,81 @@ class Solution:
                 if c not in curr:
                     curr[c] = {}
                 curr = curr[c]
-            curr[end] = w  # use the word as the value for the last $
+            curr['#'] = None  # flag the end of one word
 
-        def dfs(row, col, parent, matchedWords):
+        def dfs(output, trie, path, board, r, c, seen):
+            curr = trie[board[r][c]]
+            if '#' in curr:  # it has reach the end of one word
+                output.append(path)
+                del curr['#']  # take out the word from the trie
 
-            letter = board[row][col]
-            currNode = parent[letter]
+            direction = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+            for a, b in direction:
+                if -1 < r + a < len(board) and -1 < c + b < len(board[0]) and ((r + a), (c + b)) not in seen and \
+                        board[r + a][c + b] in curr:
+                    seen.add((r + a, c + b))
+                    dfs(output, curr, path + board[r + a][c + b], board, r + a, c + b, seen)
+                    seen.remove((r + a, c + b))
 
-            # check if we find a match of word
-            word_match = currNode.pop('$', False)
-            if word_match:
-                # also we removed the matched word to avoid duplicates,
-                #   as well as avoiding using set() for results.
-                matchedWords.append(word_match)
-
-            # Before the EXPLORATION, mark the cell as visited
-            board[row][col] = '`seen`'
-
-            # Explore the neighbors in 4 directions, i.e. up, right, down, left
-            for (rowOffset, colOffset) in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-                r, c = row + rowOffset, col + colOffset
-                if r < 0 or r >= len(board) or c < 0 or c >= len(board[0]) or board[r][c] not in currNode:
-                    continue
-                else:
-                    dfs(r, c, currNode, matchedWords)
-            # restore
-            board[row][col] = letter
-
-            # # Optimization: incrementally remove the matched leaf node in Trie.
-            # if not currNode:
-            #     parent.pop(letter)
-
-        matchedWords = []
-        for r in range(len(board)):
+        output = []
+        for r in range(
+                len(board)):  # if current is in the trie, search the neighbour, check if they are in the trie as well.
             for c in range(len(board[0])):
                 if board[r][c] in trie:
-                    dfs(r, c, trie, matchedWords)
+                    dfs(output, trie, board[r][c], board, r, c, {(r, c)})
+        return output
 
-        return matchedWords
+
+
+# previous approach
+# class Solution:
+#     def findWords(self, board: 'List[List[str]]', words: 'List[str]') -> 'List[str]':
+#         end = '$'
+#         trie = {}
+#         for w in words:
+#             curr = trie
+#             for c in w:
+#                 if c not in curr:
+#                     curr[c] = {}
+#                 curr = curr[c]
+#             curr[end] = w  # use the word as the value for the last $
+#
+#         def dfs(row, col, parent, matchedWords):
+#
+#             letter = board[row][col]
+#             currNode = parent[letter]
+#
+#             # check if we find a match of word
+#             word_match = currNode.pop('$', False)
+#             if word_match:
+#                 # also we removed the matched word to avoid duplicates,
+#                 #   as well as avoiding using set() for results.
+#                 matchedWords.append(word_match)
+#
+#             # Before the EXPLORATION, mark the cell as visited
+#             board[row][col] = '`seen`'
+#
+#             # Explore the neighbors in 4 directions, i.e. up, right, down, left
+#             for (rowOffset, colOffset) in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+#                 r, c = row + rowOffset, col + colOffset
+#                 if r < 0 or r >= len(board) or c < 0 or c >= len(board[0]) or board[r][c] not in currNode:
+#                     continue
+#                 else:
+#                     dfs(r, c, currNode, matchedWords)
+#             # restore
+#             board[row][col] = letter
+#
+#             # # Optimization: incrementally remove the matched leaf node in Trie.
+#             # if not currNode:
+#             #     parent.pop(letter)
+#
+#         matchedWords = []
+#         for r in range(len(board)):
+#             for c in range(len(board[0])):
+#                 if board[r][c] in trie:
+#                     dfs(r, c, trie, matchedWords)
+#
+#         return matchedWords
 
 
 
