0a630ac9b73cce178101cfda89790bd381048cb2
Renjie Liu <anlrj@qq.com>
2021-10-21 09:02:45 -0400

    380

--- 0001_0599/380.py
+++ 0001_0599/380.py
@@ -1,41 +1,86 @@
-import random;
-
-
 class RandomizedSet:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.hmp = {}
+        self.arr = []
+        self.hmp = {} #to store the location of the value
+        
 
     def insert(self, val: int) -> bool:
-        """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        """
-        if val not in self.hmp:
-            self.hmp[val] = None
+        if val in self.hmp:
+            return False
+        else:
+            self.hmp[val] = len(self.arr)
+            self.arr.append(val)
             return True
-        return False
+        
 
     def remove(self, val: int) -> bool:
-        """
-        Removes a value from the set. Returns true if the set contained the specified element.
-        """
-        if val in self.hmp:
+        if val not in self.hmp:
+            return False
+        else:
+            last = self.arr[-1] #swap the val and last item in the list, and pop the last item
+            loc = self.hmp[val]
+            self.arr[loc] = last 
+            self.arr[-1] = val
+            self.hmp[last] = loc #update the location of the previously last item
             del self.hmp[val]
+            self.arr.pop()
             return True
-        return False
+            
+            
+        
 
     def getRandom(self) -> int:
-        """
-        Get a random element from the set.
-        """
-        k = list(self.hmp.keys())
-        return k[random.randint(0, len(k) - 1)]
+        return random.choice(self.arr)
+
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
+
+
+
+# previous approach
+# import random;
+
+
+# class RandomizedSet:
+
+#     def __init__(self):
+#         """
+#         Initialize your data structure here.
+#         """
+#         self.hmp = {}
+
+#     def insert(self, val: int) -> bool:
+#         """
+#         Inserts a value to the set. Returns true if the set did not already contain the specified element.
+#         """
+#         if val not in self.hmp:
+#             self.hmp[val] = None
+#             return True
+#         return False
+
+#     def remove(self, val: int) -> bool:
+#         """
+#         Removes a value from the set. Returns true if the set contained the specified element.
+#         """
+#         if val in self.hmp:
+#             del self.hmp[val]
+#             return True
+#         return False
+
+#     def getRandom(self) -> int:
+#         """
+#         Get a random element from the set.
+#         """
+#         k = list(self.hmp.keys())
+#         return k[random.randint(0, len(k) - 1)]
+
+# # Your RandomizedSet object will be instantiated and called as such:
+# # obj = RandomizedSet()
+# # param_1 = obj.insert(val)
+# # param_2 = obj.remove(val)
+# # param_3 = obj.getRandom()
