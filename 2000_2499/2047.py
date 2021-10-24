4ae2712b216942c4d8a134cabc36a16cc3deb553
Renjie Liu <anlrj@qq.com>
2021-10-24 11:51:57 -0400

    2047

---
+++ 2000_2499/2047.py
@@ -0,0 +1,35 @@
+class Solution:
+    def countValidWords(self, sentence: str) -> int:
+        arr = sentence.split(' ') 
+        cnt = 0 
+        for a in arr:
+            if a:
+                valid = 1
+                for c in a: #It only contains lowercase letters, hyphens, and/or punctuation (no digits)
+                    if 48 <= ord(c) <=57:
+                        valid = 0 
+                        break 
+                
+                if a.count('-') > 1:#There is at most one hyphen '-'. 
+                    valid = 0
+
+                if a.count('-') == 1: #If present, it should be surrounded by lowercase characters ("a-b" is valid, but "-ab" and "ab-" are not valid).
+                    loc = a.find('-')
+                    if not (loc > 0 and loc < len(a)-1 and 97<=ord(a[loc-1]) <=122 and 97<=ord(a[loc+1])<=122 ):
+                        valid = 0                 
+                
+                t = a.replace('.', '').replace(',','').replace('!', '')
+                if len(a) - len(t) > 1: # at most one punctuation mark
+                    valid = 0
+                else: #If present, it should be at the end of the token
+                    s = ".,!"
+                    for c in s:
+                        if c in a and a.find(c)!=len(a) -1:
+                            valid = 0 
+                            break
+                
+                # if valid: print(a)
+                cnt += valid
+        return cnt 
+    
+    
