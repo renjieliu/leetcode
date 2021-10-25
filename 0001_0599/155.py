f96c72648bedb7a3bceea1ed94f877aea54f5161
Renjie Liu <anlrj@qq.com>
2021-10-25 09:00:58 -0400

    155

--- 0001_0599/155.py
+++ 0001_0599/155.py
@@ -1,35 +1,67 @@
 class MinStack:
 
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
         self.stk = []
         self.minn = float('inf')
-        self.minStk = []
+        
 
-    def push(self, x: int) -> None:
-        self.stk.append(x)
-        self.minn = min(self.minn, x)  # compare with current minumum when push to the stack
-        self.minStk.append(self.minn)
+    def push(self, val: int) -> None:
+        self.stk.append(val)
+        self.minn = min(self.minn, val)
 
     def pop(self) -> None:
         self.stk.pop()
-        self.minStk.pop()
-        if self.minStk == []:
-            self.minn = float('inf')
-        else:
-            self.minn = self.minStk[-1]
+        self.minn = min(self.stk) if self.stk else float('inf')
 
     def top(self) -> int:
         return self.stk[-1]
 
     def getMin(self) -> int:
-        return self.minStk[-1]
+        return self.minn
+
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
-# obj.push(x)
+# obj.push(val)
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+
+
+# previous approach
+# class MinStack:
+
+#     def __init__(self):
+#         """
+#         initialize your data structure here.
+#         """
+#         self.stk = []
+#         self.minn = float('inf')
+#         self.minStk = []
+
+#     def push(self, x: int) -> None:
+#         self.stk.append(x)
+#         self.minn = min(self.minn, x)  # compare with current minumum when push to the stack
+#         self.minStk.append(self.minn)
+
+#     def pop(self) -> None:
+#         self.stk.pop()
+#         self.minStk.pop()
+#         if self.minStk == []:
+#             self.minn = float('inf')
+#         else:
+#             self.minn = self.minStk[-1]
+
+#     def top(self) -> int:
+#         return self.stk[-1]
+
+#     def getMin(self) -> int:
+#         return self.minStk[-1]
+
+# # Your MinStack object will be instantiated and called as such:
+# # obj = MinStack()
+# # obj.push(x)
+# # obj.pop()
+# # param_3 = obj.top()
+# # param_4 = obj.getMin()
