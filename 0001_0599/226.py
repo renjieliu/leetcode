c8ec102fe8ceca59b10f2790d3f1936b0bab4b55
Renjie Liu <anlrj@qq.com>
2021-10-26 08:51:22 -0400

    226

--- 0001_0599/226.py
+++ 0001_0599/226.py
@@ -1,27 +1,45 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def invertTree(self, root: TreeNode) -> TreeNode:
-        def helper(node):
-            if node.left == node.right == None:
-                return None
-            else:
-                t = node.left
-                node.left = node.right
-                node.right = t
-                if node.left:
-                    helper(node.left)
-                if node.right:
-                    helper(node.right)
-        if root == None : return None
-        else:
-            helper(root)
-            return root
+    def invertTree(self, root: 'Optional[TreeNode]') -> 'Optional[TreeNode]':
+        def invert(node):
+            if node:
+                node.left, node.right = node.right, node.left
+                invert(node.left)
+                invert(node.right)
+        invert(root) 
+        return root
+
+
+# previous approach
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class Solution:
+#     def invertTree(self, root: TreeNode) -> TreeNode:
+#         def helper(node):
+#             if node.left == node.right == None:
+#                 return None
+#             else:
+#                 t = node.left
+#                 node.left = node.right
+#                 node.right = t
+#                 if node.left:
+#                     helper(node.left)
+#                 if node.right:
+#                     helper(node.right)
+#         if root == None : return None
+#         else:
+#             helper(root)
+#             return root
 
 
 
