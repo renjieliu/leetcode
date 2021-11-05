a86f454050c474d5258a901db42d9ee59887da3d
Renjie Liu <anlrj@qq.com>
2021-11-05 09:10:28 -0400

    441

--- 0001_0599/441.py
+++ 0001_0599/441.py
@@ -1,24 +1,36 @@
-def arrangeCoins(n):
-    """
-    :type n: int
-    :rtype: int
-    """
-    # cnt = 0
-    #
-    # while cnt*(cnt+1) <= 2*n:
-    #     cnt+= 1
-
-    # return cnt-1
-
-    return int(((1+8*n)**0.5-1)/2) #https://zh.wikipedia.org/wiki/%E4%B8%80%E5%85%83%E4%BA%8C%E6%AC%A1%E6%96%B9%E7%A8%8B
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        curr = 0 
+        row = 0 
+        while curr < n:
+            row += 1
+            curr += row
+        return row if curr == n else row - 1
+    
 
 
+# previous approach
+# def arrangeCoins(n):
+#     """
+#     :type n: int
+#     :rtype: int
+#     """
+#     # cnt = 0
+#     #
+#     # while cnt*(cnt+1) <= 2*n:
+#     #     cnt+= 1
 
-print(arrangeCoins(8)) #3
-print(arrangeCoins(5)) #2
-print(arrangeCoins(10009)) #140
-print(arrangeCoins(1)) #1
-print(arrangeCoins(0)) #0
+#     # return cnt-1
+
+#     return int(((1+8*n)**0.5-1)/2) #https://zh.wikipedia.org/wiki/%E4%B8%80%E5%85%83%E4%BA%8C%E6%AC%A1%E6%96%B9%E7%A8%8B
+
+
+
+# print(arrangeCoins(8)) #3
+# print(arrangeCoins(5)) #2
+# print(arrangeCoins(10009)) #140
+# print(arrangeCoins(1)) #1
+# print(arrangeCoins(0)) #0
 
 
 
