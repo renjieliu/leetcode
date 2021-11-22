fa5a04298bf80ec9edd4e2114cf8a650e8a805a8
Renjie Liu <anlrj@qq.com>
2021-11-22 09:18:39 -0500

    1429

--- 1001_1499/1429.py
+++ 1001_1499/1429.py
@@ -2,32 +2,68 @@ class FirstUnique:
 
     def __init__(self, nums: 'List[int]'):
         self.hmp = {}
-        self.stk = []
+        self.stk = [] 
         for n in nums:
             if n not in self.hmp:
-                self.hmp[n] = 0
-            self.hmp[n] += 1
-            if self.hmp[n] == 1:
                 self.stk.append(n)
+                self.hmp[n] = 0 
+            self.hmp[n] += 1 
             while self.stk and self.hmp[self.stk[0]] > 1:
                 self.stk.pop(0)
+        
 
     def showFirstUnique(self) -> int:
-        if self.stk:
-            return self.stk[0]
-        return -1
+        return -1 if self.stk == [] else self.stk[0]
+       
 
     def add(self, value: int) -> None:
         if value not in self.hmp:
-            self.hmp[value] = 0
-        self.hmp[value] += 1
-        if self.hmp[value] == 1:
             self.stk.append(value)
-
+            self.hmp[value] = 0 
+        self.hmp[value] += 1 
         while self.stk and self.hmp[self.stk[0]] > 1:
-            self.stk.pop(0)
+            self.stk.pop(0) 
+
 
 # Your FirstUnique object will be instantiated and called as such:
 # obj = FirstUnique(nums)
 # param_1 = obj.showFirstUnique()
 # obj.add(value)
+
+
+
+
+# previous approach
+# class FirstUnique:
+
+#     def __init__(self, nums: 'List[int]'):
+#         self.hmp = {}
+#         self.stk = []
+#         for n in nums:
+#             if n not in self.hmp:
+#                 self.hmp[n] = 0
+#             self.hmp[n] += 1
+#             if self.hmp[n] == 1:
+#                 self.stk.append(n)
+#             while self.stk and self.hmp[self.stk[0]] > 1:
+#                 self.stk.pop(0)
+
+#     def showFirstUnique(self) -> int:
+#         if self.stk:
+#             return self.stk[0]
+#         return -1
+
+#     def add(self, value: int) -> None:
+#         if value not in self.hmp:
+#             self.hmp[value] = 0
+#         self.hmp[value] += 1
+#         if self.hmp[value] == 1:
+#             self.stk.append(value)
+
+#         while self.stk and self.hmp[self.stk[0]] > 1:
+#             self.stk.pop(0)
+
+# # Your FirstUnique object will be instantiated and called as such:
+# # obj = FirstUnique(nums)
+# # param_1 = obj.showFirstUnique()
+# # obj.add(value)
