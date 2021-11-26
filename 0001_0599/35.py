509a619d225eff63cafe459ccbbfcc59c1b07ca1
Renjie Liu <anlrj@qq.com>
2021-11-26 08:58:20 -0500

    35

--- 0001_0599/35.py
+++ 0001_0599/35.py
@@ -1,16 +1,31 @@
 class Solution:
-    def searchInsert(self, nums: 'List[int]', target: int) -> int:
+    def searchInsert(self, nums: 'List[int]', target: int) -> int: 
         s = 0
-        e = len(nums) - 1
-        while s <= e: #bin_search
-            mid = (s + e) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] > target:
-                e = mid - 1
+        e = len(nums)-1 
+        while s <= e: #binary search to find the first place >= than the target
+            mid = s - (s-e)//2
+            if nums[mid] >= target:
+                e = mid - 1 
             else:
-                s = mid + 1
-        return s
+                s = mid +1
+        return s 
+    
+
+
+# previous approach
+# class Solution:
+#     def searchInsert(self, nums: 'List[int]', target: int) -> int:
+#         s = 0
+#         e = len(nums) - 1
+#         while s <= e: #bin_search
+#             mid = (s + e) // 2
+#             if nums[mid] == target:
+#                 return mid
+#             elif nums[mid] > target:
+#                 e = mid - 1
+#             else:
+#                 s = mid + 1
+#         return s
 
 
 
