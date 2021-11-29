f5d600a9e5662a62ecc3086e20fa8523313b3655
Renjie Liu <anlrj@qq.com>
2021-11-29 09:06:56 -0500

    721

--- 0600_0999/721.py
+++ 0600_0999/721.py
@@ -1,25 +1,51 @@
 class Solution:
     def accountsMerge(self, accounts: 'List[List[str]]') -> 'List[List[str]]':
-        root = {}
-        acc = [[] for _ in range(len(accounts))]
-        for i in range(len(accounts)):
-            email = accounts[i][1:]
-            for e in email :
-                if e not in root: # the email has never been seen before
-                    root[e] = i
-                    acc[i].append(e)
-                elif root[e] != i: # the email is seen before, attach all the people in the previous group to current
-                    prev_root = root[e]
-                    acc[i] += acc[prev_root]
-                    for v in acc[prev_root]: # map all the email within the same group to current
-                        root[v] = i
-                    acc[prev_root] = [] # clear the previous
-
+        loc= {} # to record the location of email
+        groups = [[] for _ in accounts] 
+        for i, acc in enumerate(accounts):
+            for email in acc[1:]:
+                if email not in loc:
+                    loc[email] = i
+                    groups[i].append(email)
+                elif loc[email] != i: #this email was seen before 
+                    prev_loc = loc[email]
+                    for e in groups[loc[email]]: #for all the email in the same group, move to the current group
+                        loc[e] = i
+                        groups[i].append(e)
+                    groups[prev_loc] = [] #clear the previous group
+        
         output = []
-        for i in range(len(acc)): #find the name of the person, and attach the emails
-            if acc[i]:
-                name = accounts[i][0]
-                email = sorted(list(set(acc[i])))
-                output.append( [name] + email )
+        for i, emails in enumerate(groups):
+            if emails:
+                name = accounts[loc[emails[0]]][0]
+                output.append([name] + sorted(emails))
         return output
+    
+
+
+# previous approach
+# class Solution:
+#     def accountsMerge(self, accounts: 'List[List[str]]') -> 'List[List[str]]':
+#         root = {}
+#         acc = [[] for _ in range(len(accounts))]
+#         for i in range(len(accounts)):
+#             email = accounts[i][1:]
+#             for e in email :
+#                 if e not in root: # the email has never been seen before
+#                     root[e] = i
+#                     acc[i].append(e)
+#                 elif root[e] != i: # the email is seen before, attach all the people in the previous group to current
+#                     prev_root = root[e]
+#                     acc[i] += acc[prev_root]
+#                     for v in acc[prev_root]: # map all the email within the same group to current
+#                         root[v] = i
+#                     acc[prev_root] = [] # clear the previous
+
+#         output = []
+#         for i in range(len(acc)): #find the name of the person, and attach the emails
+#             if acc[i]:
+#                 name = accounts[i][0]
+#                 email = sorted(list(set(acc[i])))
+#                 output.append( [name] + email )
+#         return output
 
