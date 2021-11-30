436e9b74fef2ee45b09cacdba439d7fd14df060f
Renjie Liu <anlrj@qq.com>
2021-11-30 09:02:39 -0500

    85

--- 0001_0599/85.py
+++ 0001_0599/85.py
@@ -1,46 +1,79 @@
 class Solution:
     def maximalRectangle(self, matrix: 'List[List[str]]') -> int:
-        if matrix == []: return 0
         dp = []
-        output = -float('inf')
+        output = 0 
         for r in range(len(matrix)):
             dp.append([])
             for c in range(len(matrix[0])):
-                if r == 0:
-                    if c == 0:
-                        if matrix[r][c] == "1":
-                            dp[-1].append([[1, 1]])  # [[left, above]]
-                        else:
-                            dp[-1].append([[0, 0]])
-                    else:
-                        if matrix[r][c] == "1":
-                            dp[-1].append([[dp[-1][-1][-1][0] + 1, 1]])
-                        else:
-                            dp[-1].append([[0, 0]])
+                if matrix[r][c] == "0":
+                    dp[-1].append([[0,0]]) # [left, up]
                 else:
-                    if c == 0:
-                        if matrix[r][c] == "1":
-                            dp[-1].append([[1, dp[r - 1][0][-1][1] + 1]])
+                    if r == 0:
+                        if c == 0:
+                            dp[-1].append([[1,1]]) 
                         else:
-                            dp[-1].append([[0, 0]])
+                            dp[-1].append([[dp[r][c-1][0][0] + 1 , 1]])
                     else:
-                        if matrix[r][c] == "0":
-                            dp[-1].append([[0, 0]])
+                        if c == 0:
+                            dp[-1].append([[1, dp[r-1][c][0][1] + 1]])
                         else:
-                            curr_left = dp[-1][-1][-1][0] + 1
-                            if matrix[r - 1][c] == "0":
-                                dp[-1].append([[curr_left, 1]])  # how many 1 on the left, including itself
-                            else:
-                                curr = []
-                                for combo in dp[r - 1][c]:
-                                    curr.append([min(combo[0], curr_left),
-                                                 combo[1] + 1])  # follow the previous columns cnt,  but row cnt +1
-                                curr.append([curr_left, 1])  # start from the current line
-                                dp[-1].append(curr)
-
-                for i in dp[-1][-1]:
-                    output = max(output, i[0] * i[1])
-
+                            curr_left = dp[r][c-1][0][0]+1 # first one is the max left, as it was added first
+                            curr = []
+                            curr.append([curr_left,1]) #start from the current line
+                            for left, up in dp[r-1][c]:
+                                curr.append( [min(left, curr_left), up + 1] ) #follow the previous columns cnt,  but row cnt +1 
+                            dp[-1].append(curr)
+            
+                for left, up in dp[-1][-1]:
+                    output = max(output, left*up)
         return output
+        
+
+
+# previous approach
+# class Solution:
+#     def maximalRectangle(self, matrix: 'List[List[str]]') -> int:
+#         if matrix == []: return 0
+#         dp = []
+#         output = -float('inf')
+#         for r in range(len(matrix)):
+#             dp.append([])
+#             for c in range(len(matrix[0])):
+#                 if r == 0:
+#                     if c == 0:
+#                         if matrix[r][c] == "1":
+#                             dp[-1].append([[1, 1]])  # [[left, above]]
+#                         else:
+#                             dp[-1].append([[0, 0]])
+#                     else:
+#                         if matrix[r][c] == "1":
+#                             dp[-1].append([[dp[-1][-1][-1][0] + 1, 1]])
+#                         else:
+#                             dp[-1].append([[0, 0]])
+#                 else:
+#                     if c == 0:
+#                         if matrix[r][c] == "1":
+#                             dp[-1].append([[1, dp[r - 1][0][-1][1] + 1]])
+#                         else:
+#                             dp[-1].append([[0, 0]])
+#                     else:
+#                         if matrix[r][c] == "0":
+#                             dp[-1].append([[0, 0]])
+#                         else:
+#                             curr_left = dp[-1][-1][-1][0] + 1
+#                             if matrix[r - 1][c] == "0":
+#                                 dp[-1].append([[curr_left, 1]])  # how many 1 on the left, including itself
+#                             else:
+#                                 curr = []
+#                                 for combo in dp[r - 1][c]:
+#                                     curr.append([min(combo[0], curr_left),
+#                                                  combo[1] + 1])  # follow the previous columns cnt,  but row cnt +1
+#                                 curr.append([curr_left, 1])  # start from the current line
+#                                 dp[-1].append(curr)
+
+#                 for i in dp[-1][-1]:
+#                     output = max(output, i[0] * i[1])
+
+#         return output
 
 
