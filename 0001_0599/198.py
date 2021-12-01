45e2078c9457410c5c784b152fc799718b48796a
Renjie Liu <anlrj@qq.com>
2021-12-01 11:02:27 -0500

    198

--- 0001_0599/198.py
+++ 0001_0599/198.py
@@ -1,23 +1,40 @@
 class Solution:
-    def rob(self, nums: 'List[int]') -> int:
-        def helper(hmp, loc, arr):
-            if loc in hmp:
-                return hmp[loc]
-            elif loc >= len(arr):
-                hmp[loc] = 0 
-                return hmp[loc]
-            else:
-                hmp[loc] = 0 
-                for i in range(loc, len(arr)):
-                    A = arr[loc] + helper(hmp, i+2, arr) # take current and move on
-                    B = helper(hmp, i+1, arr) # just move on to the next one
-                    hmp[loc] = max(hmp[loc], A, B)
-                return hmp[loc]
+    def rob(self, nums: 'List[int]') -> int: #dp approach
+        if len(nums) <= 2:
+            return max(nums)
+        a = nums[0] 
+        b = nums[1]
+        output = max(a, b)
+        for n in nums[2:]:
+            c = max(n+a, b) #max at current, either take a or use b as is
+            output = max(output, c)
+            a = max(a, b) 
+            b = max(b, c)
+        return output
+
+
+
+# previous approach
+# class Solution:
+#     def rob(self, nums: 'List[int]') -> int:
+#         def helper(hmp, loc, arr):
+#             if loc in hmp:
+#                 return hmp[loc]
+#             elif loc >= len(arr):
+#                 hmp[loc] = 0 
+#                 return hmp[loc]
+#             else:
+#                 hmp[loc] = 0 
+#                 for i in range(loc, len(arr)):
+#                     A = arr[loc] + helper(hmp, i+2, arr) # take current and move on
+#                     B = helper(hmp, i+1, arr) # just move on to the next one
+#                     hmp[loc] = max(hmp[loc], A, B)
+#                 return hmp[loc]
         
-        hmp = {}
-        helper(hmp, 0, nums)
-        # print(hmp)
-        return hmp[0]
+#         hmp = {}
+#         helper(hmp, 0, nums)
+#         # print(hmp)
+#         return hmp[0]
 
 
 # previous approach
