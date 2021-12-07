b6b0c5f1318c50dc9bb783de0757fc21fcdeac6b
Renjie Liu <anlrj@qq.com>
2021-12-07 09:09:45 -0500

    1290

--- 1001_1499/1290.py
+++ 1001_1499/1290.py
@@ -1,12 +1,35 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def getDecimalValue(self, head: ListNode) -> int:
-        curr = ""
-        while head:
-            curr+=str(head.val)
-            head = head.next
-        return int(curr,2)
+    def getDecimalValue(self, head: 'ListNode') -> int:
+        def helper(output, node): #one pass DFS to calc the output
+            if node.next == None:
+                output[0] += node.val
+                return 0
+            else:
+                level = helper(output, node.next) + 1
+                output[0] += node.val * (2**level)
+                return level
+        
+        output = [0]
+        helper(output, head)
+        return output[0]
+
+    
+
+# previous approach
+# # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def getDecimalValue(self, head: ListNode) -> int:
+#         curr = ""
+#         while head:
+#             curr+=str(head.val)
+#             head = head.next
+#         return int(curr,2)
