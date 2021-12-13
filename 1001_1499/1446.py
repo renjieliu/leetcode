e867fc462658cc56298c83c6bfec13d225d00d54
Renjie Liu <anlrj@qq.com>
2021-12-13 09:12:58 -0500

    1446

--- 1001_1499/1446.py
+++ 1001_1499/1446.py
@@ -1,15 +1,47 @@
 class Solution:
     def maxPower(self, s: str) -> int:
-        if s == "":
-            return 0
-        output = 1
+        output = 1 
         curr = 1
         for i in range(1, len(s)):
-            if s[i] == s[i - 1]:
+            if s[i] == s[i-1]:
                 curr += 1
             else:
                 curr = 1
-
             output = max(curr, output)
-
         return output
+    
+
+
+# previous approach
+# class Solution:
+#     def maxPower(self, s: str) -> int:
+#         if s == "":
+#             return 0
+#         output = 1
+#         curr = 1
+#         for i in range(1, len(s)):
+#             if s[i] == s[i - 1]:
+#                 curr += 1
+#             else:
+#                 curr = 1
+
+#             output = max(curr, output)
+
+#         return output
+
+
+# previous approach
+# class Solution:
+#     def maxPower(self, s: str) -> int: 
+#         currStart = 0
+#         output = 1
+#         i = 1
+#         while i < len(s):
+#             if s[i] != s[i-1]:
+#                 output = max(output, i-currStart)
+#                 currStart = i
+#             i+=1
+#         output = max(output, i-currStart)
+#         return output
+
+
