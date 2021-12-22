dbe672b6a883e7c289bd962e462077b7ad2b9a3c
Renjie Liu <anlrj@qq.com>
2021-12-22 11:00:13 -0500

    624

--- 0600_0999/624.py
+++ 0600_0999/624.py
@@ -1,13 +1,37 @@
 class Solution:
     def maxDistance(self, arrays: 'List[List[int]]') -> int:
-        # min for each arr, and maxx for each arr, sort, and check if they are from the same array
-        minn = sorted([[i, a[0]] for i, a in enumerate(arrays)], key = lambda x: x[1]) 
-        maxx = sorted([[i, a[-1]] for i, a in enumerate(arrays)], key = lambda x: x[1])
-        # print(minn, maxx)
-        if minn[0][0]!=maxx[-1][0]: #check if they are from the same array
-            return maxx[-1][1] - minn[0][1]
+        minn = float('inf') 
+        maxx = -float('inf')
+        output = -float('inf')
+        loc = -1 
+        for i, arr in enumerate(arrays):
+            minn = min(minn, arr[0])
+            maxx = max(maxx, arr[-1]) 
+            if minn == arr[0] and maxx == arr[-1]:
+                loc = i
+
+        if loc == -1: #the maxx and minn belong to different groups
+            return maxx - minn
         else:
-            return max(maxx[-1][1] - minn[1][1], maxx[-2][1] - minn[0][1])
+            for i, arr in enumerate(arrays):
+                if i != loc:
+                    output=max(output, arr[-1]-minn)
+                    output=max(output, maxx-arr[0])
+            return output
+        
+
+
+# previous approach
+# class Solution:
+#     def maxDistance(self, arrays: 'List[List[int]]') -> int:
+#         # min for each arr, and maxx for each arr, sort, and check if they are from the same array
+#         minn = sorted([[i, a[0]] for i, a in enumerate(arrays)], key = lambda x: x[1]) 
+#         maxx = sorted([[i, a[-1]] for i, a in enumerate(arrays)], key = lambda x: x[1])
+#         # print(minn, maxx)
+#         if minn[0][0]!=maxx[-1][0]: #check if they are from the same array
+#             return maxx[-1][1] - minn[0][1]
+#         else:
+#             return max(maxx[-1][1] - minn[1][1], maxx[-2][1] - minn[0][1])
         
 
 
