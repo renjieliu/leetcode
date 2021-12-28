ae2a7dd90610f950b3388fe16b4edecbcea6c58c
Renjie Liu <anlrj@qq.com>
2021-12-28 09:17:32 -0500

    876

--- 0600_0999/876.py
+++ 0600_0999/876.py
@@ -1,17 +1,43 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def middleNode(self, head: ListNode) -> ListNode:
-        def flat(stk, node):
-            if node.next == None:
-                stk.append(node)
+    def middleNode(self, head: 'Optional[ListNode]') -> 'Optional[ListNode]':
+        output = [None] 
+        def helper(output, node, level): #check how many levels in total, and if current level is total//2+1 (the middle node)
+            if node.next:
+                total = helper(output, node.next, level+1) 
             else:
-                stk.append(node)
-                flat(stk, node.next)
-        stk = []
-        flat(stk, head)
-        return stk[len(stk)//2]
+                total = level
+            if level == total//2+1:
+                output[0] = node 
+            
+            return total
+        
+        helper(output, head, 1)
+        return output[0]
+            
+                
+
+
+
+# previous approach
+# # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# class Solution:
+#     def middleNode(self, head: ListNode) -> ListNode:
+#         def flat(stk, node):
+#             if node.next == None:
+#                 stk.append(node)
+#             else:
+#                 stk.append(node)
+#                 flat(stk, node.next)
+#         stk = []
+#         flat(stk, head)
+#         return stk[len(stk)//2]
