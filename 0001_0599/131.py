4dcd407cbfdf447af7df3f68ee7aea5072d2318a
Renjie Liu <anlrj@qq.com>
2022-01-05 09:08:39 -0500

    131

--- 0001_0599/131.py
+++ 0001_0599/131.py
@@ -1,18 +1,34 @@
 class Solution:
     def partition(self, s: str) -> 'List[List[str]]':
-        def isP(s):
-            return s == s[::-1] and s != ""  # return if the s is a palindrome
-
-        def helper(output, s, curr): #this is a simple DFS approach
-            if len(s) == 0:
+        def helper(output, s, curr, start):
+            if start == len(s): #at the end of the string, all prev partitons are palindrome
                 output.append(curr)
-            else:
-                for i in range(len(s) + 1):
-                    if isP(s[:i]):
-                        helper(output, s[i:], curr + [s[:i]])
-
+            for i in range(start,len(s)):
+                if s[start:i+1] == s[start:i+1][::-1]: # current partition is palindrome
+                    helper(output, s, curr + [s[start:i+1]], i+1) # keep checking from the next position
+        
         output = []
-        helper(output, s, [])
+        helper(output, s, [], 0)
         return output
+    
+    
+
+
+# class Solution:
+#     def partition(self, s: str) -> 'List[List[str]]':
+#         def isP(s):
+#             return s == s[::-1] and s != ""  # return if the s is a palindrome
+
+#         def helper(output, s, curr): #this is a simple DFS approach
+#             if len(s) == 0:
+#                 output.append(curr)
+#             else:
+#                 for i in range(len(s) + 1):
+#                     if isP(s[:i]):
+#                         helper(output, s[i:], curr + [s[:i]])
+
+#         output = []
+#         helper(output, s, [])
+#         return output
 
 
