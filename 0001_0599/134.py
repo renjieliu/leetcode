1cae7981e247a03645b3a99c35e623aa85fcd7a8
Renjie Liu <anlrj@qq.com>
2022-01-21 09:29:18 -0500

    134

--- 0001_0599/134.py
+++ 0001_0599/134.py
@@ -1,33 +1,49 @@
 class Solution:
-    def canCompleteCircuit(self, gas: 'List[int]', cost: 'List[int]') -> int: # Time: O(N), Space: O(N)
-        gas += gas 
-        cost += cost
-        curr = 0
-        arr = []
+    def canCompleteCircuit(self, gas: 'List[int]', cost: 'List[int]') -> int:
+        total, local = 0, 0 #total gas left, if to finish one cycle. local is the calculation for current segment
+        startStation = 0
         for i in range(len(gas)):
-            curr += gas[i] - cost[i] 
-            if curr < 0: #if current total is less than 0, just add -1
-                arr.append(-1) 
-                curr = 0 
-            else:
-                arr.append(curr)
+            total += gas[i]-cost[i]
+            local += gas[i]-cost[i] 
+            if local < 0: #curr segment 
+                startStation=i+1 #start from next station
+                local = 0
+        return startStation if total >= 0 else -1
+    
+
+
+# previous approach
+
+# class Solution:
+#     def canCompleteCircuit(self, gas: 'List[int]', cost: 'List[int]') -> int: # Time: O(N), Space: O(N)
+#         gas += gas 
+#         cost += cost
+#         curr = 0
+#         arr = []
+#         for i in range(len(gas)):
+#             curr += gas[i] - cost[i] 
+#             if curr < 0: #if current total is less than 0, just add -1
+#                 arr.append(-1) 
+#                 curr = 0 
+#             else:
+#                 arr.append(curr)
         
-        # print(arr)
-        negPos = [float('inf') for _ in gas] + [float('inf')] #to record the first -1 on the right or itself
+#         # print(arr)
+#         negPos = [float('inf') for _ in gas] + [float('inf')] #to record the first -1 on the right or itself
         
         
-        for i in range(len(arr)-1, -1, -1): # find the first negative on the right, or itself
-            if arr[i] < 0:
-                negPos[i] = i
-            else:
-                negPos[i] = negPos[i+1]
+#         for i in range(len(arr)-1, -1, -1): # find the first negative on the right, or itself
+#             if arr[i] < 0:
+#                 negPos[i] = i
+#             else:
+#                 negPos[i] = negPos[i+1]
         
-        # print(negPos)
-        for i in range(len(gas)//2):
-            if negPos[i]-i >= len(gas)//2 + 1: #current total non negative length
-                return i 
+#         # print(negPos)
+#         for i in range(len(gas)//2):
+#             if negPos[i]-i >= len(gas)//2 + 1: #current total non negative length
+#                 return i 
         
-        return -1
+#         return -1
     
     
 
