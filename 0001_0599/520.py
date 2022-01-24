d61b751ed17307d06c71fb36353bfd46d27e92ff
Renjie Liu <anlrj@qq.com>
2022-01-24 09:00:32 -0500

    520

--- 0001_0599/520.py
+++ 0001_0599/520.py
@@ -1,12 +1,24 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        upperLoc = []
-        for i, c in enumerate(word):
-            if 65<= ord(c) <= 90:
-                upperLoc.append(i)
-        if len(upperLoc) == len(word) or len(upperLoc) == 0 or (len(upperLoc) == 1 and upperLoc[0] == 0):
-            return True
-        return False
+        upper_cnt = 0
+        for i, c in enumerate(word): #just go through and check if the letter is in upper case
+            upper_cnt +=1 if 65 <= ord(c) <= 90 else 0
+        return upper_cnt==0 or upper_cnt == len(word) or (upper_cnt==1 and 65 <= ord(word[0]) <= 90) 
+        #return word == word.upper() or word == word[0].upper()+word[1:].lower() or word == word.lower()
+    
+
+
+# previous approach
+
+# class Solution:
+#     def detectCapitalUse(self, word: str) -> bool:
+#         upperLoc = []
+#         for i, c in enumerate(word):
+#             if 65<= ord(c) <= 90:
+#                 upperLoc.append(i)
+#         if len(upperLoc) == len(word) or len(upperLoc) == 0 or (len(upperLoc) == 1 and upperLoc[0] == 0):
+#             return True
+#         return False
 
 
 
