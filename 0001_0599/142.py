9e0eb7f2670bc91579d7bf6f122315d2c6657810
Renjie Liu <anlrj@qq.com>
2022-01-26 10:07:17 -0500

    142

--- 0001_0599/142.py
+++ 0001_0599/142.py
@@ -8,21 +8,58 @@ class Solution:
     def detectCycle(self, head: 'Optional[ListNode]') -> 'Optional[ListNode]':
         fast = head 
         slow = head
-        while fast: # Floyd's tortoise and hare algo
+        while fast: # Floyd's tortoise and hare's algorithm
             if fast.next:
                 fast = fast.next.next
-            else:
+                slow = slow.next
+            else: # no cycle found
                 return None
-            slow = slow.next
+            
             if fast == slow:
                 break
-        if fast == None: 
+            
+         
+        if fast == None:
             return None
-        slow = head #let fast finish current loop, and slow move from the start. They will meet at the cycle point
-        while fast != slow: 
-            fast = fast.next
+
+        slow = head  # move the slow back to the head. The meet point will be the start of the cycle when they meet again.
+        
+        while fast!=slow:
             slow = slow.next
-        return fast
+            fast = fast.next
+        
+        return slow
+            
+
+
+
+# previous approach
+
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.next = None
+
+# class Solution:
+#     def detectCycle(self, head: 'Optional[ListNode]') -> 'Optional[ListNode]':
+#         fast = head 
+#         slow = head
+#         while fast: # Floyd's tortoise and hare algo
+#             if fast.next:
+#                 fast = fast.next.next
+#             else:
+#                 return None
+#             slow = slow.next
+#             if fast == slow:
+#                 break
+#         if fast == None: 
+#             return None
+#         slow = head #let fast finish current loop, and slow move from the start. They will meet at the cycle point
+#         while fast != slow: 
+#             fast = fast.next
+#             slow = slow.next
+#         return fast
 
         
 
