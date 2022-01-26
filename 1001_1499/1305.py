ddf2f4ccf9175f24b3d427b0ce3e1b3f15114350
Renjie Liu <anlrj@qq.com>
2022-01-26 09:05:26 -0500

    1305

--- 1001_1499/1305.py
+++ 1001_1499/1305.py
@@ -1,21 +1,40 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> 'List[int]':
-        def dfs(output, node):
-            if node.left == node.right == None:
-                output.append(node.val)
-            else:
-                output.append(node.val)
-                if node.left: dfs(output, node.left)
-                if node.right: dfs(output, node.right)
+    def getAllElements(self, root1: 'TreeNode', root2: 'TreeNode') -> 'List[int]':
+        def dfs(node): #get all the node values from the tree and return
+            return ([node.val] + dfs(node.left) + dfs(node.right)) if node else []
+        return sorted(dfs(root1) + dfs(root2))
 
-        output = []
-        for r in (root1, root2):
-            if r: dfs(output, r)
+    
 
-        return sorted(list(output))
+# previous approach
+
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# class Solution:
+#     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> 'List[int]':
+#         def dfs(output, node):
+#             if node.left == node.right == None:
+#                 output.append(node.val)
+#             else:
+#                 output.append(node.val)
+#                 if node.left: dfs(output, node.left)
+#                 if node.right: dfs(output, node.right)
+
+#         output = []
+#         for r in (root1, root2):
+#             if r: dfs(output, r)
+
+#         return sorted(list(output))
+
+
+
