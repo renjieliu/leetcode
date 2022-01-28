6f890b55e66a1d7b31076fa6939fcfb8fb98fca4
Renjie Liu <anlrj@qq.com>
2022-01-28 13:16:38 -0500

    211

--- 0001_0599/211.py
+++ 0001_0599/211.py
@@ -1,60 +1,100 @@
 class WordDictionary:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.trie = {}
+        self.trie = {} 
 
-    def addWord(self, word: str) -> None:
-        """
-        Adds a word into the data structure.
-        """
-        if word[0] not in self.trie:
-            self.trie[word[0]] = {}
-        curr = self.trie[word[0]]    
-        for c in word[1:]:
+    def addWord(self, word: str) -> None: #build the trie
+        curr = self.trie
+        for c in word:
             if c not in curr:
-                curr[c] = {}
+                curr[c] = {} 
             curr = curr[c]
-        curr['$'] = None
+        curr['#'] = {}
+        
     
-    def helper(self, word, hmp):
-        if len(word) == 0:
-            return True if '$' in hmp else False
-       
-        i = 0 
-        while i < len(word):
-            if word[i] in hmp:
-                hmp = hmp[word[i]]
-                i+=1
-                if i == len(word):
-                    return True if '$' in hmp else False 
-                  
-            else:
-                if word[i] == ".":
-                    for h in hmp.values():
-                        if h != None and self.helper(word[i+1:], h) == True:
+    def search(self, word: str) -> bool:
+        def helper(trie, arr):
+            if arr:
+                if arr[0] in trie:
+                    return helper(trie[arr[0]], arr[1:])
+                elif arr[0] == '.':
+                    for k in trie.keys(): #need to check all the paths
+                        if helper(trie[k], arr[1:]) == True: #as long as find a match, return True
                             return True
-                    return False 
                 else:
                     return False
-            
-    
-    
+            else:
+                return '#' in trie # check if there's a word ends here
 
-    def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
-        """
-        return self.helper(word, self.trie)
-    
+        
+        return helper(self.trie, list(word))
 
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
 # obj.addWord(word)
 # param_2 = obj.search(word)
+
+
+# previous approach
+
+# class WordDictionary:
+
+#     def __init__(self):
+#         """
+#         Initialize your data structure here.
+#         """
+#         self.trie = {}
+
+#     def addWord(self, word: str) -> None:
+#         """
+#         Adds a word into the data structure.
+#         """
+#         if word[0] not in self.trie:
+#             self.trie[word[0]] = {}
+#         curr = self.trie[word[0]]    
+#         for c in word[1:]:
+#             if c not in curr:
+#                 curr[c] = {}
+#             curr = curr[c]
+#         curr['$'] = None
+    
+#     def helper(self, word, hmp):
+#         if len(word) == 0:
+#             return True if '$' in hmp else False
+       
+#         i = 0 
+#         while i < len(word):
+#             if word[i] in hmp:
+#                 hmp = hmp[word[i]]
+#                 i+=1
+#                 if i == len(word):
+#                     return True if '$' in hmp else False 
+                  
+#             else:
+#                 if word[i] == ".":
+#                     for h in hmp.values():
+#                         if h != None and self.helper(word[i+1:], h) == True:
+#                             return True
+#                     return False 
+#                 else:
+#                     return False
+            
+    
+    
+
+#     def search(self, word: str) -> bool:
+#         """
+#         Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+#         """
+#         return self.helper(word, self.trie)
+    
+
+
+# # Your WordDictionary object will be instantiated and called as such:
+# # obj = WordDictionary()
+# # obj.addWord(word)
+# # param_2 = obj.search(word)
 
 
 
