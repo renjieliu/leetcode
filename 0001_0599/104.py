66a6955f315fff75522ab429c0c2ff408ee4bce9
Renjie Liu <anlrj@qq.com>
2022-02-14 08:51:18 -0500

    104

--- 0001_0599/104.py
+++ 0001_0599/104.py
@@ -1,27 +1,47 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        def helper(node, output, curr):
-            if node.left == node.right == None:
-                output[0] = max(curr + 1, output[0])
-            else:
-                if node.left:
-                    helper(node.left, output, curr + 1)
-                if node.right:
-                    helper(node.right, output, curr + 1)
+    def maxDepth(self, root: 'Optional[TreeNode]') -> int: #just use this function to find the deepest node
+        if root == None: 
+            return 0 
+        elif root.left == root.right == None: #return the leaf node as level 1
+            return 1
+        else: 
+            return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right)) #curr + max(left, right)
 
-        if root == None:
-            return 0
-        else:
-            output = [0]
-            helper(root, output, 0)
-            return output[0]
+        
+            
+
+# previous approach
+
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class Solution:
+#     def maxDepth(self, root: TreeNode) -> int:
+#         def helper(node, output, curr):
+#             if node.left == node.right == None:
+#                 output[0] = max(curr + 1, output[0])
+#             else:
+#                 if node.left:
+#                     helper(node.left, output, curr + 1)
+#                 if node.right:
+#                     helper(node.right, output, curr + 1)
+
+#         if root == None:
+#             return 0
+#         else:
+#             output = [0]
+#             helper(root, output, 0)
+#             return output[0]
 
 # previous approach
 # # Definition for a binary tree node.
