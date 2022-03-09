2655a33c93f6e2adc4595893e7440f633b381c47
Renjie Liu <anlrj@qq.com>
2022-03-09 08:56:57 -0500

    82

--- 0001_0599/82.py
+++ 0001_0599/82.py
@@ -1,44 +1,83 @@
 # Definition for singly-linked list.
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def deleteDuplicates(self, head: ListNode) -> ListNode:
-        if head == None: return None
-        start = None
-        prev = head.val
-        cnt = 1
-        head = head.next
-        while head:
-            if head.val == prev:
-                cnt+=1
-            else: # current value differ from the previous
-                if cnt == 1: #prev repeating once, need prev as a node
-                    if start == None:
-                        start = ListNode(prev)
-                        node = start
-                    else:
-                        node.next = ListNode(prev)
-                        node = node.next
-                    prev = head.val
-                    cnt = 1
-                else: #prev repeating more than once
-                    prev = head.val
-                    cnt = 1
-
+    def deleteDuplicates(self, head: 'Optional[ListNode]') -> 'Optional[ListNode]': #O(N | 1)
+        if head == None:
+            return head 
+        else:
+            start = ListNode() #dummy start node, will need to return start.next
+            node = start
+            prevVal = head.val 
+            prevNode = head 
+            prevCnt = 1
             head = head.next
+            while head:
+                if head.val != prevVal:
+                    if prevCnt == 1: #if prev Node count is 1
+                        node.next = prevNode
+                        node = node.next 
+                    prevVal = head.val
+                    prevNode = head
+                    prevCnt = 1
+                else: # increment the prevCnt, if current head value is same as previous 
+                    prevCnt += 1
+                head = head.next
 
-        if cnt == 1:
-            if start == None:
-                start = ListNode(prev)
-            else:
-                node.next = ListNode(prev)
-                node = node.next
+            if prevCnt == 1 : #add the last node
+                node.next = prevNode
+            else: #cut off the last node
+                node.next = None
+            
+            return start.next
+        
 
-        return start
+
+# previous approach
+
+# # Definition for singly-linked list.
+
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# class Solution:
+#     def deleteDuplicates(self, head: ListNode) -> ListNode:
+#         if head == None: return None
+#         start = None
+#         prev = head.val
+#         cnt = 1
+#         head = head.next
+#         while head:
+#             if head.val == prev:
+#                 cnt+=1
+#             else: # current value differ from the previous
+#                 if cnt == 1: #prev repeating once, need prev as a node
+#                     if start == None:
+#                         start = ListNode(prev)
+#                         node = start
+#                     else:
+#                         node.next = ListNode(prev)
+#                         node = node.next
+#                     prev = head.val
+#                     cnt = 1
+#                 else: #prev repeating more than once
+#                     prev = head.val
+#                     cnt = 1
+
+#             head = head.next
+
+#         if cnt == 1:
+#             if start == None:
+#                 start = ListNode(prev)
+#             else:
+#                 node.next = ListNode(prev)
+#                 node = node.next
+
+#         return start
 
 
 
