550e6919e4086cc8a1611ad74efc6af635509d9a
Renjie Liu <anlrj@qq.com>
2022-03-12 08:24:19 -0500

    138

--- 0001_0599/138.py
+++ 0001_0599/138.py
@@ -1,29 +1,61 @@
+"""
 # Definition for a Node.
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
+"""
 
 class Solution:
-    def copyRandomList(self, head: 'Node') -> 'Node':
-        def copy(copied_set, node):
-            if node in copied_set:
-                return copied_set[node]
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        def helper(copied, node):
+            if node == None:
+                return None
+            elif node in copied: # the node is copied
+                return copied[node]
             else:
-                if node == None:
-                    copied_set[node] = None
-                    return copied_set[node]
-                else:
-                    copied = Node(node.val)
-                    copied_set[node] = copied
-                    copied.next = copy(copied_set, node.next)
-                    copied.random = copy(copied_set, node.random)
-                    return copied
-        return copy({}, head)
+                newNode = Node(node.val) # create the new node, and add to the hashmap
+                copied[node] = newNode
+                
+                newNode.next = helper(copied, node.next) #work on the next pointer
+                newNode.random = helper(copied, node.random) # work on the random pointer
+                return newNode
+        
+        return helper({}, head)
+        
+
+        
+        
+# previous approach
+
+# # Definition for a Node.
+# class Node:
+#     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+#         self.val = int(x)
+#         self.next = next
+#         self.random = random
+
+# class Solution:
+#     def copyRandomList(self, head: 'Node') -> 'Node':
+#         def copy(copied_set, node):
+#             if node in copied_set:
+#                 return copied_set[node]
+#             else:
+#                 if node == None:
+#                     copied_set[node] = None
+#                     return copied_set[node]
+#                 else:
+#                     copied = Node(node.val)
+#                     copied_set[node] = copied
+#                     copied.next = copy(copied_set, node.next)
+#                     copied.random = copy(copied_set, node.random)
+#                     return copied
+#         return copy({}, head)
 
 
-    # previous approach
+# previous approach
+
 # Definition for a Node.
 # class Node:
 #     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
