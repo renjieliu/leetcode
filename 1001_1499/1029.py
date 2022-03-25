b8bab5e928addec7d2b1b78f121df513ced3e82e
Renjie Liu <anlrj@qq.com>
2022-03-25 10:33:41 -0400

    1029

--- 1001_1499/1029.py
+++ 1001_1499/1029.py
@@ -1,4 +1,32 @@
 class Solution:
     def twoCitySchedCost(self, costs: 'List[List[int]]') -> int:
-        costs = sorted(costs, key = lambda x:x[0] - x[1])
-        return sum(map(lambda x: x[0], costs[:len(costs)//2])) + sum(map(lambda x: x[1], costs[len(costs)//2:]))
+        arr = sorted(costs, key = lambda x : x[0] - x[1]) #smaller x[0] will be placed to the A city.
+        L = len(costs)
+        return sum(a for a, b in arr[:L//2]) + sum(b for a, b in arr[L//2:]) #1st half to be placed to city a, 2nd to city b
+
+
+
+
+# previous approach
+
+# class Solution:
+#     def twoCitySchedCost(self, costs: 'List[List[int]]') -> int:
+#         costs = sorted(costs, key = lambda x:x[0] - x[1])
+#         return sum(map(lambda x: x[0], costs[:len(costs)//2])) + sum(map(lambda x: x[1], costs[len(costs)//2:]))
+
+
+# previous approach
+# class Solution:
+#     def twoCitySchedCost(self, costs: 'List[List[int]]') -> int:
+#         costs = sorted(costs, key= lambda x : x[0]- x[1] )
+#         output = 0
+#         cnt = 0
+#         for i in range(len(costs)):
+#             if cnt < len(costs)//2:
+#                 output += costs[i][0]
+#             else:
+#                 output +=costs[i][1]
+#             cnt += 1
+#         return  output
+
+
