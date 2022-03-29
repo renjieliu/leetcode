327d8e874cc2d98e42f96260701bb91a2b4bf863
Renjie Liu <anlrj@qq.com>
2022-03-29 11:19:20 -0400

    287

--- 0001_0599/287.py
+++ 0001_0599/287.py
@@ -1,16 +1,35 @@
-def findDuplicate(self, nums: 'List[int]') -> int: #Floyd's tortoise
-    h = nums[0]
-    t = nums[0]
-    while True:
-        h = nums[nums[h]]
-        t = nums[t]
-        if h == t:
-            break
-    t = nums[0]
-    while t != h:
-        t = nums[t]
-        h = nums[h]
-    return t
+class Solution:
+    def findDuplicate(self, nums: 'List[int]') -> int: # same as to find a cycle with Floyd's tortoise and hare algo
+        fast = slow = nums[0]
+        while True: 
+            fast = nums[nums[fast]]
+            slow = nums[slow]
+            if fast == slow:
+                break            
+        
+        slow = nums[0]
+        while fast!= slow:
+            fast = nums[fast] 
+            slow = nums[slow]
+        return slow
+    
+
+
+# previous approach
+
+# def findDuplicate(self, nums: 'List[int]') -> int: #Floyd's tortoise
+#     h = nums[0]
+#     t = nums[0]
+#     while True:
+#         h = nums[nums[h]]
+#         t = nums[t]
+#         if h == t:
+#             break
+#     t = nums[0]
+#     while t != h:
+#         t = nums[t]
+#         h = nums[h]
+#     return t
 
 
 
