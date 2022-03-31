fb0cce16129b391201fe2e05da8d83b7c7c7ac8a
Renjie Liu <anlrj@qq.com>
2022-03-31 10:48:57 -0400

    1102

--- 1001_1499/1102.py
+++ 1001_1499/1102.py
@@ -1,19 +1,41 @@
 class Solution:
-    def maximumMinimumPath(self, grid: 'List[List[int]]') -> int:
-        output = grid[0][0]
-        direction = [[0,1],[1,0], [-1,0], [0,-1]]
-        pq = [[-grid[0][0], 0, 0]]
+    def maximumMinimumPath(self, grid: 'List[List[int]]') -> int: #O(mn*logk | mn)
         seen = {(0,0)}
+        output=grid[0][0]
+        direction = [[0,1], [1,0], [-1, 0], [0, -1]] 
+        pq = [[-grid[0][0], 0,0]]
         heapq.heapify(pq)
-        while pq:
-            val, sr, sc = heapq.heappop(pq) #pop the lowest cost each time
-            output = min(output, -val)
-            if sr==len(grid)-1 and sc==len(grid[0])-1:
+        while pq: # go through all the neighbours of a cell, and take the largest one at each step
+            v, r, c = heapq.heappop(pq)
+            output = min(output, -v)
+            if r==len(grid)-1 and c == len(grid[0])-1:
                 return output
             for a, b in direction:
-                if -1 < sr+a < len(grid) and -1<sc+b<len(grid[0]) and (sr+a, sc+b) not in seen:
-                    seen.add((sr+a, sc+b))
-                    heapq.heappush(pq, [-grid[sr+a][sc+b], sr+a, sc+b])
+                if -1<r+a <len(grid) and -1 < c+b < len(grid[0]) and (r+a, c+b) not in seen:
+                    if (r+a, c+b) not in seen:
+                        heapq.heappush(pq,[-grid[r+a][c+b], r+a, c+b])
+                        seen.add((r+a, c+b))
+
+                        
+
+# previous approach
+
+# class Solution:
+#     def maximumMinimumPath(self, grid: 'List[List[int]]') -> int:
+#         output = grid[0][0]
+#         direction = [[0,1],[1,0], [-1,0], [0,-1]]
+#         pq = [[-grid[0][0], 0, 0]]
+#         seen = {(0,0)}
+#         heapq.heapify(pq)
+#         while pq:
+#             val, sr, sc = heapq.heappop(pq) #pop the lowest cost each time
+#             output = min(output, -val)
+#             if sr==len(grid)-1 and sc==len(grid[0])-1:
+#                 return output
+#             for a, b in direction:
+#                 if -1 < sr+a < len(grid) and -1<sc+b<len(grid[0]) and (sr+a, sc+b) not in seen:
+#                     seen.add((sr+a, sc+b))
+#                     heapq.heappush(pq, [-grid[sr+a][sc+b], sr+a, sc+b])
 
 
 
