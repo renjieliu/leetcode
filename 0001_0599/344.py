19caa9d2eb43f04e25dc7fdaaa364d02026aafeb
Renjie Liu <anlrj@qq.com>
2022-04-01 09:19:52 -0400

    344

--- 0001_0599/344.py
+++ 0001_0599/344.py
@@ -1,12 +1,24 @@
 class Solution:
-    def reverseString(self, s: 'List[str]') -> None:
+    def reverseString(self, s: 'List[str]') -> None: #O(n)
         """
         Do not return anything, modify s in-place instead.
         """
-        for i in range(len(s) // 2):  # in-place swap
-            t = s[i]
-            s[i] = s[-(i + 1)]
-            s[-(i + 1)] = t
+        for i in range(len(s)//2): #go through the first half, and swap with the corresponding character at the end
+            s[i], s[-i-1] = s[-i-1], s[i]
+
+            
+
+# previous solution
+
+# class Solution:
+#     def reverseString(self, s: 'List[str]') -> None:
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
+#         for i in range(len(s) // 2):  # in-place swap
+#             t = s[i]
+#             s[i] = s[-(i + 1)]
+#             s[-(i + 1)] = t
 
 # OLD solution, not optimal
 # def reverseString(s):
