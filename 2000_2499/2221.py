5713b9b498ab072b9019a72cd6e07d57d0ee2299
Renjie Liu <anlrj@qq.com>
2022-04-03 15:51:20 -0400

    2221

--- 2000_2499/2221.py
+++ 2000_2499/2221.py
@@ -1,11 +1,24 @@
 class Solution:
     def triangularSum(self, nums: 'List[int]') -> int:
-        while len(nums) > 1: #using the same structure as BFS, to eliminate the nums until only 1 element left
-            nxt =[]
+        while len(nums) > 1:
             for i in range(1, len(nums)):
-                nxt.append((nums[i] + nums[i-1])%10)
-            nums = nxt 
+                nums[i-1] = (nums[i-1] + nums[i])%10 #put result back to prev loc. As current loc will be used for next calc
+            nums.pop() #take out the last element of the nums
         return nums[0]
+    
+    
+
+
+# previous solution
+
+# class Solution:
+#     def triangularSum(self, nums: 'List[int]') -> int:
+#         while len(nums) > 1: #using the same structure as BFS, to eliminate the nums until only 1 element left
+#             nxt =[]
+#             for i in range(1, len(nums)):
+#                 nxt.append((nums[i] + nums[i-1])%10)
+#             nums = nxt 
+#         return nums[0]
     
     
 
