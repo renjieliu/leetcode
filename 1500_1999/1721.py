fc2b19453137aa04763c1f145fdb3154e2756a91
Renjie Liu <anlrj@qq.com>
2022-04-04 08:35:41 -0400

    1721

--- 1500_1999/1721.py
+++ 1500_1999/1721.py
@@ -1,23 +1,75 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def swapNodes(self, head: ListNode, k: int) -> ListNode:
-        curr = head
-        cnt = 0
-        b = None
-        while curr:
-            cnt +=1
-            if cnt == k:
-                a = curr #is the left pointer
-                b = head
-            curr = curr.next
-            if cnt > k: #move b, and so the distance b and curr is k, once curr is at the end of list, b is at end-k position
+    def swapNodes(self, head: 'Optional[ListNode]', k: int) -> 'Optional[ListNode]': #O(N | 1)
+        cnt = 0 
+        node = head
+        while node:
+            cnt += 1 
+            if cnt == k :
+                a = node
+                b = head # set b as head. when total node reaches end, b is at the -k location. (total = k + total-k)
+            if cnt > k:
                 b = b.next
-        b.val, a.val = a.val, b.val
+            node = node.next
+        a.val, b.val = b.val, a.val #swap a and b 
         return head
+            
+        
+        
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]: #O(N | 1)
+#         def helper(node, lvl, k, targetNodes):
+#             if node == None:
+#                 return 0
+#             else:
+#                 if lvl == k: #store current value as v[0]
+#                     targetNodes[0] = node
+#                 nxt = helper(node.next, lvl+1, k, targetNodes)
+                
+#                 if nxt == k-1: #from the bottom, need to put 
+#                     targetNodes[1] = node
+                
+#                 return nxt + 1
+        
+#         targetNodes = [None, None]
+#         helper(head, 1, k, targetNodes)
+#         targetNodes[0].val, targetNodes[1].val = targetNodes[1].val, targetNodes[0].val #swap the 2 values
+#         return head
+    
+ 
+
+
+# previous solution
+
+# # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def swapNodes(self, head: ListNode, k: int) -> ListNode:
+#         curr = head
+#         cnt = 0
+#         b = None
+#         while curr:
+#             cnt +=1
+#             if cnt == k:
+#                 a = curr #is the left pointer
+#                 b = head
+#             curr = curr.next
+#             if cnt > k: #move b, and so the distance b and curr is k, once curr is at the end of list, b is at end-k position
+#                 b = b.next
+#         b.val, a.val = a.val, b.val
+#         return head
 
 
 # previous approach
