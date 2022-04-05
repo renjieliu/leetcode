6048d17e11b9220216f32fec6fcaf9d5a4c70a3b
Renjie Liu <anlrj@qq.com>
2022-04-05 14:43:20 -0400

    2225

---
+++ 2000_2499/2225.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findWinners(self, matches: 'List[List[int]]') -> 'List[List[int]]': #O(logN | n)
+        win = {}
+        loss = {}
+        for w, l in matches: # record the winning times and loss times
+            if w not in win:
+                win[w] = 0 
+            win[w] += 1
+            if l not in loss:
+                loss[l] = 0 
+            loss[l] += 1
+        
+        return [sorted(list(set(win.keys()) - set(loss.keys()))), sorted([k for k, v in loss.items() if v == 1 ])]
+    
+    
+    
