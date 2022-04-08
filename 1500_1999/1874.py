c5f85ec26eecf7ae8a5e34ef68cd2fdb7ab5df8f
Renjie Liu <anlrj@qq.com>
2022-04-08 09:42:14 -0400

    1874

--- 1500_1999/1874.py
+++ 1500_1999/1874.py
@@ -1,8 +1,21 @@
 class Solution:
-    def minProductSum(self, nums1: 'List[int]', nums2: 'List[int]') -> int:
-        nums1.sort()
+    def minProductSum(self, nums1: 'List[int]', nums2: 'List[int]') -> int: #O(nlogn)
+        nums1.sort() #greedy, smallest * largest 
         nums2.sort(reverse = True)
         output = 0
-        while nums1:
-            output += nums1.pop() * nums2.pop()
+        for i in range(len(nums1)):
+            output += (nums1[i] * nums2[i])
         return output
+
+    
+
+# previous solution
+
+# class Solution:
+#     def minProductSum(self, nums1: 'List[int]', nums2: 'List[int]') -> int:
+#         nums1.sort()
+#         nums2.sort(reverse = True)
+#         output = 0
+#         while nums1:
+#             output += nums1.pop() * nums2.pop()
+#         return output
