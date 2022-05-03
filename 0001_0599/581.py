67e92e16d602884f8572c169e51c276ccd27c661
Renjie Liu <anlrj@qq.com>
2022-05-03 09:52:52 -0400

    581

--- 0001_0599/581.py
+++ 0001_0599/581.py
@@ -1,27 +1,49 @@
 class Solution:
-    def findUnsortedSubarray(self, nums: 'List[int]') -> int:
-        if len(nums) == 1: return 0
-        stk = []
+    def findUnsortedSubarray(self, nums: 'List[int]') -> int:  #O(N | N)
+        stk = [] #record the current smallest location
         left = float('inf')
+        for i in range(len(nums)):
+            while stk and nums[stk[-1]] > nums[i]:
+                 left = min(left, stk.pop())
+            stk.append(i)        
+        
+        stk = [] # record the current  largest location
         right = -float('inf')
-        i = 0
-        while i < len(nums):
-            while stk and nums[i] < nums[stk[-1]]: #keep popping the stack, until meeting a smaller
-                left = min(left, stk.pop())
+        for i in range(len(nums)-1, -1, -1):
+            while stk and nums[stk[-1]] < nums[i]:
+                right = max(right, stk.pop())
             stk.append(i)
-            i+=1
+        
+        return right - left + 1 if left != float('inf') else 0
+    
+    
+    
+# previous solution
 
-        j = len(nums)-1
-        stk = []
-        while j > -1:
-            while stk and nums[j] > nums[stk[-1]] : #keep popping the stack until meeting a larger..
-                right = max(right, stk.pop() )
-            stk.append(j)
-            j-=1
+# class Solution:
+#     def findUnsortedSubarray(self, nums: 'List[int]') -> int:
+#         if len(nums) == 1: return 0
+#         stk = []
+#         left = float('inf')
+#         right = -float('inf')
+#         i = 0
+#         while i < len(nums):
+#             while stk and nums[i] < nums[stk[-1]]: #keep popping the stack, until meeting a smaller
+#                 left = min(left, stk.pop())
+#             stk.append(i)
+#             i+=1
 
-        # print(left, right)
-        output = abs(right - left) + 1
-        return 0 if output == float('inf') else output
+#         j = len(nums)-1
+#         stk = []
+#         while j > -1:
+#             while stk and nums[j] > nums[stk[-1]] : #keep popping the stack until meeting a larger..
+#                 right = max(right, stk.pop() )
+#             stk.append(j)
+#             j-=1
+
+#         # print(left, right)
+#         output = abs(right - left) + 1
+#         return 0 if output == float('inf') else output
 
 
 
