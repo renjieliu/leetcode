9c1cfcd0c913568b26a1ab054a0ad86943e36391
Renjie Liu <anlrj@qq.com>
2022-05-07 13:30:48 -0400

    456

--- 0001_0599/456.py
+++ 0001_0599/456.py
@@ -1,21 +1,40 @@
 class Solution:
-    def find132pattern(self, nums: 'List[int]') -> bool:
-        if len(nums) < 3:
-            return False
-        else:
-            minStk = [float('inf')]
-            for i in range(1, len(nums)):  # smallest on the left
-                minStk.append(min(nums[i - 1], minStk[-1]))
-            stk = []
-            for i in range(len(nums) - 1, -1, -1):
-                curr = -float('inf')
-                while stk and stk[-1] < nums[
-                    i]:  # find the one smaller than the current one, but larger than the one in the minStk
-                    curr = stk.pop()
-                if curr > minStk[i]:
-                    return True
-                stk.append(nums[i])
-            return False
+    def find132pattern(self, nums: 'List[int]') -> bool: # O( N | N )
+        mi = [nums[0]]  #accumulate minn
+        for i in range(1, len(nums)):
+            mi.append(min(mi[-1], nums[i]))
+        
+        stk = [nums[-1]] # mono stk
+        for i in range(len(nums)-2, -1, -1):
+            while stk and nums[i] > stk[-1]: # if curr is greater than mono stk top (smallest in the stk)
+                if mi[i] < stk.pop(): # mi < curr, and stk top is > mi
+                    return True 
+            stk.append(nums[i]) # add curr to the stk
+        
+        return False
+
+
+
+# previous solution
+
+# class Solution:
+#     def find132pattern(self, nums: 'List[int]') -> bool:
+#         if len(nums) < 3:
+#             return False
+#         else:
+#             minStk = [float('inf')]
+#             for i in range(1, len(nums)):  # smallest on the left
+#                 minStk.append(min(nums[i - 1], minStk[-1]))
+#             stk = []
+#             for i in range(len(nums) - 1, -1, -1):
+#                 curr = -float('inf')
+#                 while stk and stk[-1] < nums[
+#                     i]:  # find the one smaller than the current one, but larger than the one in the minStk
+#                     curr = stk.pop()
+#                 if curr > minStk[i]:
+#                     return True
+#                 stk.append(nums[i])
+#             return False
 
 
 # previous approach
