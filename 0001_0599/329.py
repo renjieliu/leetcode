070723f71d879c55a2bf36dafaede394b920a28c
Renjie Liu <anlrj@qq.com>
2022-05-19 09:18:28 -0400

    329

--- 0001_0599/329.py
+++ 0001_0599/329.py
@@ -23,6 +23,33 @@ class Solution:
     
 
 
+# previous solution
+
+# class Solution:
+#     def longestIncreasingPath(self, matrix: 'List[List[int]]') -> int: #O(MN | MN)
+#         def helper(hmp, grid, r, c): #dfs to find the longest path from here
+#             if (r, c) in hmp: 
+#                 return hmp[(r,c)]
+#             else: 
+#                 hmp[(r, c)] = 1
+#                 direction = [[1,0], [0,-1], [-1, 0], [0,1]] #go 4 directions.
+#                 for a, b in direction:
+#                     nxt_r = r+a
+#                     nxt_c = c+b
+#                     if -1 < nxt_r < len(grid) and -1 < nxt_c < len(grid[0]) and grid[nxt_r][nxt_c] > grid[r][c]:
+#                         hmp[(r, c)] = max(hmp[(r, c)], 1+helper(hmp, grid, nxt_r, nxt_c))
+#                 return hmp[(r,c)]
+        
+#         hmp = {}
+#         output = 0
+#         for r in range(len(matrix)):
+#             for c in range(len(matrix[0])):
+#                 output = max(output, helper(hmp, matrix, r, c))
+#         # print(hmp)
+#         return output
+    
+
+
 
 # previous solution
 
