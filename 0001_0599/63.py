ca0341fae55142c39dd371b4d1fd0d6297bb1837
Renjie Liu <anlrj@qq.com>
2022-05-20 08:58:20 -0400

    63

--- 0001_0599/63.py
+++ 0001_0599/63.py
@@ -1,23 +1,44 @@
 class Solution:
-    def uniquePathsWithObstacles(self, obstacleGrid: 'List[List[int]]') -> int:
-        dp = []
-        for r in range(len(obstacleGrid)):
-            dp.append([])
+    def uniquePathsWithObstacles(self, obstacleGrid: 'List[List[int]]') -> int: #O(MN | MN)
+        dp = [[0] * (len(obstacleGrid[0])+1) ]  #pad the dummy row
+        for r in range(len(obstacleGrid)): # curr = left + up
+            dp.append([0]) # add a dummy column on the left
             for c in range(len(obstacleGrid[0])):
-                if obstacleGrid[r][c] == 1:
-                    dp[-1].append(0)
+                if r==0 and c == 0 : #this is the starting point
+                    dp[-1].append(1 if obstacleGrid[r][c] == 0 else 0)
                 else:
-                    if r == 0:
-                        if c == 0:
-                            dp[-1].append(1)
-                        else:
-                            dp[-1].append(dp[-1][-1])
+                    if obstacleGrid[r][c] == 1 :
+                        dp[-1].append(0)
                     else:
-                        if c == 0:
-                            dp[-1].append(dp[r-1][0])
-                        else:
-                            dp[-1].append(dp[r-1][c] + dp[r][c-1])
+                        dp[-1].append(dp[r+1][c]+dp[r][c+1]) # r, c in the obstacleGrid is the r+1 in the dp, and c+1 
+        # print(dp)
         return dp[-1][-1]
+                
+
+
+
+# previous solution
+
+# class Solution:
+#     def uniquePathsWithObstacles(self, obstacleGrid: 'List[List[int]]') -> int:
+#         dp = []
+#         for r in range(len(obstacleGrid)):
+#             dp.append([])
+#             for c in range(len(obstacleGrid[0])):
+#                 if obstacleGrid[r][c] == 1:
+#                     dp[-1].append(0)
+#                 else:
+#                     if r == 0:
+#                         if c == 0:
+#                             dp[-1].append(1)
+#                         else:
+#                             dp[-1].append(dp[-1][-1])
+#                     else:
+#                         if c == 0:
+#                             dp[-1].append(dp[r-1][0])
+#                         else:
+#                             dp[-1].append(dp[r-1][c] + dp[r][c-1])
+#         return dp[-1][-1]
 
 
 
