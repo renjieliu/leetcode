f08fa086753b63a8595e158dadd22bb727c35a32
Renjie Liu <anlrj@qq.com>
2022-05-26 08:56:46 -0400

    191

--- 0001_0599/191.py
+++ 0001_0599/191.py
@@ -1,11 +1,24 @@
 class Solution:
-    def hammingWeight(self, n: int) -> int:
-        cnt = 0
-        while n != 0:
-            if n%2 ==1:
-                cnt +=1
-            n = n >> 1
+    def hammingWeight(self, n: int) -> int: #O(K | 1), K is the bit length of n 
+        cnt = 0 
+        while n > 0:
+            cnt += n&1 #if 
+            n >>= 1
         return cnt
+    
+
+
+# previous solution
+
+
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         cnt = 0
+#         while n != 0:
+#             if n%2 ==1:
+#                 cnt +=1
+#             n = n >> 1
+#         return cnt
 
 # previous approach
 # def hammingWeight(n):
