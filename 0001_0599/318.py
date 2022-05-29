0fb2189539e575e86012640f240796601e32c6bc
Renjie Liu <anlrj@qq.com>
2022-05-29 10:00:14 -0400

    318

--- 0001_0599/318.py
+++ 0001_0599/318.py
@@ -1,11 +1,26 @@
 class Solution:
-    def maxProduct(self, words: 'List[str]') -> int:
+    def maxProduct(self, words: 'List[str]') -> int: #O(N2 | N)
         output = 0
-        arr = []
-        for w in words:
-            arr.append(set(w))
-        for i in range (len(arr)):
+        arr = [set(w) for w in words]
+        
+        for i in range(len(arr)): # for each possible pair, check if there're common letters between the two. 
             for j in range(i+1, len(arr)):
                 if arr[i] & arr[j] == set():
                     output = max(output, len(words[i]) * len(words[j]))
         return output
+    
+
+
+# previous solution
+
+# class Solution:
+#     def maxProduct(self, words: 'List[str]') -> int:
+#         output = 0
+#         arr = []
+#         for w in words:
+#             arr.append(set(w))
+#         for i in range (len(arr)):
+#             for j in range(i+1, len(arr)):
+#                 if arr[i] & arr[j] == set():
+#                     output = max(output, len(words[i]) * len(words[j]))
+#         return output
