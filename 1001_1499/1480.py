1ff156ac32f4272d817d5ac8e2c9409071e0768a
Renjie Liu <anlrj@qq.com>
2022-06-01 09:00:19 -0400

    1480

--- 1001_1499/1480.py
+++ 1001_1499/1480.py
@@ -1,7 +1,19 @@
 class Solution:
-    def runningSum(self, nums: 'List[int]') -> 'List[int]':
-        output = [nums.pop(0)]
-        while nums:
-            output.append(output[-1] + nums.pop(0))
-        return output
+    def runningSum(self, nums: 'List[int]') -> 'List[int]': #O(N | N)
+        dp = [nums[0]]
+        for i in range(1, len(nums)): # accu sum
+            dp.append(dp[-1] + nums[i])
+        return dp
+
+      
+
+
+# previous solution
+
+# class Solution:
+#     def runningSum(self, nums: 'List[int]') -> 'List[int]':
+#         output = [nums.pop(0)]
+#         while nums:
+#             output.append(output[-1] + nums.pop(0))
+#         return output
 
