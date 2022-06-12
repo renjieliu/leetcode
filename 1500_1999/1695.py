9ce9f8ab3a8b09db2b9700f90d2885f2cc85c848
Renjie Liu <anlrj@qq.com>
2022-06-12 10:03:59 -0400

    1695

--- 1500_1999/1695.py
+++ 1500_1999/1695.py
@@ -1,19 +1,38 @@
 class Solution:
-    def maximumUniqueSubarray(self, nums: 'List[int]') -> int:
-        output = 0
-        curr = 0
-        lkp= set()
-        l = r = 0
-        while r < len(nums):
-            while nums[r] in lkp and l <= r:
-                curr -= nums[l]
-                lkp.remove(nums[l])
-                l+=1
-            lkp.add(nums[r])
-            curr+=nums[r]
-            output = max(output, curr)
-            r+=1
-        return output
+    def maximumUniqueSubarray(self, nums: 'List[int]') -> int: # O(N | N)
+        pre = [0]
+        for n in nums: # prefix sum
+            pre.append(pre[-1] + n)
+        output = 0 
+        l = 0 
+        seen = set()
+        for r in range(len(nums)): #check if curr is seen. Keep taking out the left, and check the range sum
+            while l < r and nums[r] in seen:
+                seen.remove(nums[l])
+                l += 1 
+            seen.add(nums[r]) 
+            output = max(output, pre[r+1] - pre[l])
+        return output 
+    
+
+# previous solution
+
+# class Solution:
+#     def maximumUniqueSubarray(self, nums: 'List[int]') -> int:
+#         output = 0
+#         curr = 0
+#         lkp= set()
+#         l = r = 0
+#         while r < len(nums):
+#             while nums[r] in lkp and l <= r:
+#                 curr -= nums[l]
+#                 lkp.remove(nums[l])
+#                 l+=1
+#             lkp.add(nums[r])
+#             curr+=nums[r]
+#             output = max(output, curr)
+#             r+=1
+#         return output
 
 # previous approach
 # class Solution:
