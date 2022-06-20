cdd5b4aa86a071f5391c0db916ae616401d59738
Renjie Liu <anlrj@qq.com>
2022-06-20 09:14:26 -0400

    820

--- 0600_0999/820.py
+++ 0600_0999/820.py
@@ -1,14 +1,31 @@
 class Solution:
-    def minimumLengthEncoding(self, words: 'List[str]') -> int:
-        ref = set() #Suffix tree, to check if current word is a suffix of the previous words
-        words.sort(key = lambda x: len(x), reverse = True)
-        output = 0
-        for w in words:
-            if w not in ref:
-                output += (len(w)+1)
-                for i in range(len(w)):
-                    ref.add(w[i:])
-        return output
+    def minimumLengthEncoding(self, words: 'List[str]') -> int: #O( N*K | N*K), K being the length of the longest word
+        trie = {}
+        seen = []
+        for w in set(words):
+            curr = trie
+            for c in w[::-1]:
+                curr[c] = curr.get(c, {})
+                curr = curr[c]
+            seen.append([curr,  len(w) + 1]) # current trie, len(curr) is to check if the trie has children
+        # print(seen)
+        return sum(leaf for currTrie, leaf in seen if len(currTrie) == 0) #len(currTrie) == 0, no children, therefore, current word is not a suffix of any other word
+    
+
+
+# previous solution
+
+# class Solution:
+#     def minimumLengthEncoding(self, words: 'List[str]') -> int:
+#         ref = set() #Suffix tree, to check if current word is a suffix of the previous words
+#         words.sort(key = lambda x: len(x), reverse = True)
+#         output = 0
+#         for w in words:
+#             if w not in ref:
+#                 output += (len(w)+1)
+#                 for i in range(len(w)):
+#                     ref.add(w[i:])
+#         return output
 
 # previous approach
 # class Solution:
