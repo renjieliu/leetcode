786fe273b5f2243bf16e8ec2a343bc9b8d132cb3
Renjie Liu <anlrj@qq.com>
2022-06-22 08:43:02 -0400

    215

--- 0001_0599/215.py
+++ 0001_0599/215.py
@@ -1,3 +1,24 @@
 class Solution:
-    def findKthLargest(self, nums: 'List[int]', k: int) -> int:
-        return sorted(list(nums), reverse = True)[k-1]
+    def findKthLargest(self, nums: 'List[int]', k: int) -> int: #O( NlogN | 1 )
+        nums.sort()
+        return nums[-k]
+    
+
+# previous solution
+
+# class Solution:
+#     def findKthLargest(self, nums: 'List[int]', k: int) -> int:
+#         return sorted(list(nums), reverse = True)[k-1]
+
+
+# previous solution
+
+# class Solution:
+#     def findKthLargest(self, nums: 'List[int]', k: int) -> int:
+#         if len(nums)==1:
+#             return nums[0]      
+        
+#         return sorted(nums)[::-1][k-1]
+
+
+
