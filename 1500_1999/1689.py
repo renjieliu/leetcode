d207b8cccc69d36bcc105b4f9d3f642b3aefe1d0
Renjie Liu <anlrj@qq.com>
2022-06-27 09:11:37 -0400

    1689

--- 1500_1999/1689.py
+++ 1500_1999/1689.py
@@ -1,3 +1,13 @@
 class Solution:
-    def minPartitions(self, n: str) -> int: #just need to find the max of the string, as others can be tweaked to 0
-        return int(max(n))
+    def minPartitions(self, n: str) -> int: #O(K | 1), K is the length of n
+        return int(max(n)) #just to check the max digits in the n. Other digits can be manipulated with 0 or 1
+            
+        
+        
+        
+# previous solution
+
+# class Solution:
+#     def minPartitions(self, n: str) -> int: #just need to find the max of the string, as others can be tweaked to 0
+#         return int(max(n))
+
