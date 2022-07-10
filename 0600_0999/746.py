935f472e87b4a7d9442eea25e9467bc4a7b85403
Renjie Liu <anlrj@qq.com>
2022-07-10 08:23:07 -0400

    746

--- 0600_0999/746.py
+++ 0600_0999/746.py
@@ -30,6 +30,22 @@ class Solution:
 #             return min(a, b) # min(curr-1, curr-2) + curr
 
 
+# previous solution
+
+# class Solution:
+#     def minCostClimbingStairs(self, cost: 'List[int]') -> int:
+#         if len(cost) <= 2 :
+#             return min(cost[0], cost[-1])
+#         else:
+#             a = cost[0]
+#             b = cost[1]
+#             for i in range(2, len(cost)):
+#                 c = cost[i] + min(a, b)
+#                 a = b
+#                 b = c 
+#             return min(a, b)
+
+
 # previous approach
 # class Solution:
 #     def minCostClimbingStairs(self, cost: 'List[int]') -> int:
@@ -49,3 +65,22 @@ class Solution:
 #         dfs(dp, 0, cost)
 #         return min(dp[0], dp[1])
 
+
+
+# previous solution
+
+# class Solution:
+#     def minCostClimbingStairs(self, cost: 'List[int]') -> int:
+#         if len(cost) == 0:
+#             return 0
+#         elif len(cost) in [1, 2]:
+#             return min(cost)
+
+#         a0 = cost[0]
+#         a1 = cost[1]
+#         for i in range(2, len(cost)):
+#             a2 = cost[i] + min(a0, a1)
+#             a0 = a1
+#             a1 = a2
+
+#         return min(a1, a0)
