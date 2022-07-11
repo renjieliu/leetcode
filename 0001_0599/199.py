da86d026526ccd3782a8d405fc6615889ed089a2
Renjie Liu <anlrj@qq.com>
2022-07-11 08:45:48 -0400

    199

--- 0001_0599/199.py
+++ 0001_0599/199.py
@@ -1,26 +1,49 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def rightSideView(self, root: TreeNode) -> 'List[int]':
-        def look(output, order, node):
-            if order == len(output):
-                output.append(node.val)
-            if node.right:
-                look(output, order + 1, node.right)
-            if node.left:
-                look(output, order + 1, node.left)
-
+    def rightSideView(self, root: 'Optional[TreeNode]') -> 'List[int]': # O( N | 1 )
+        def helper(output, n, node):
+            if node:
+                if len(output) == n: # using the length of the output, to determine if current is the one to be recorded.
+                    output.append(node.val)
+                helper(output, n+1, node.right) #right first
+                helper(output, n+1, node.left) 
         output = []
-        if root == None:
-            return output
-        else:
-            look(output, 0, root)
-            return output
+        helper(output, 0, root)
+        return output
+
+    
+
+
+# previous solution
+
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# class Solution:
+#     def rightSideView(self, root: TreeNode) -> 'List[int]':
+#         def look(output, order, node):
+#             if order == len(output):
+#                 output.append(node.val)
+#             if node.right:
+#                 look(output, order + 1, node.right)
+#             if node.left:
+#                 look(output, order + 1, node.left)
+
+#         output = []
+#         if root == None:
+#             return output
+#         else:
+#             look(output, 0, root)
+#             return output
 
 # previous approach
 # Definition for a binary tree node.
