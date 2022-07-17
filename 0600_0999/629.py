56c8693ebb18946f61264aa88765d4f5b84ecd00
Renjie Liu <anlrj@qq.com>
2022-07-16 21:03:23 -0400

    629

--- 0600_0999/629.py
+++ 0600_0999/629.py
@@ -1,5 +1,5 @@
 class Solution:
-    def kInversePairs(self, n: int, k: int) -> int:
+    def kInversePairs(self, n: int, k: int) -> int: #RL 20220716: copied solution, O( N*k | N*k )
         mod = 10**9 + 7
         ds = [0] + [1] * (k + 1)
         for n in range(2, n+1):
@@ -10,4 +10,24 @@ class Solution:
                 new.append( (new[-1] + v) % mod )
             ds = new
         return (ds[k+1] - ds[k]) % mod
+    
+    
+    
+# previous solution
+
+# class Solution:
+#     def kInversePairs(self, n: int, k: int) -> int:
+#         mod = 10**9 + 7
+#         ds = [0] + [1] * (k + 1)
+#         for n in range(2, n+1):
+#             new = [0]
+#             for k in range(k+1):
+#                 v = ds[k+1]
+#                 v -= ds[k-n+1] if k >= n else 0
+#                 new.append( (new[-1] + v) % mod )
+#             ds = new
+#         return (ds[k+1] - ds[k]) % mod
+
+
+
 
