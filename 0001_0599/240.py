73d9a8a5a7a12fce34b41187216687a65f086aa2
Renjie Liu <anlrj@qq.com>
2022-07-24 14:16:52 -0400

    240

--- 0001_0599/240.py
+++ 0001_0599/240.py
@@ -1,23 +1,40 @@
 class Solution:
-    def searchMatrix(self, matrix: 'List[List[int]]', target: int) -> bool:
-        def bin(arr,n):
-            s = 0
-            e = len(arr)-1
-            while s <=e:
-                mid = s - (s-e)//2
-                if arr[mid] == n:
-                    return True
-                else:
-                    if arr[mid] < n:
-                        s = mid +1
-                    elif arr[mid] > n:
-                        e = mid -1
-            return False
+    def searchMatrix(self, matrix: 'List[List[int]]', target: int) -> bool: # O( M+N | 1 ), M is len(matrix), N is len(matrix[0])
+        r = len(matrix)-1
+        c = 0
+        while r > -1 and c < len(matrix[0]): #since both rows and columns are sorted, we can go from bottom->top->right to find the target
+            if matrix[r][c] < target:
+                c += 1 
+            elif matrix[r][c] == target:
+                return True 
+            else: 
+                r -= 1 
+        return False 
+    
 
-        for r in matrix:
-            if bin(r,target)==True:
-                return True
-        return False
+
+# previous solution
+
+# class Solution:
+#     def searchMatrix(self, matrix: 'List[List[int]]', target: int) -> bool:
+#         def bin(arr,n):
+#             s = 0
+#             e = len(arr)-1
+#             while s <=e:
+#                 mid = s - (s-e)//2
+#                 if arr[mid] == n:
+#                     return True
+#                 else:
+#                     if arr[mid] < n:
+#                         s = mid +1
+#                     elif arr[mid] > n:
+#                         e = mid -1
+#             return False
+
+#         for r in matrix:
+#             if bin(r,target)==True:
+#                 return True
+#         return False
 
 
 
