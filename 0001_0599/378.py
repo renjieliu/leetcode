f85ee8a39fdae05d130d7ab284cca7b41224d92a
Renjie Liu <anlrj@qq.com>
2022-08-02 09:16:04 -0400

    378

--- 0001_0599/378.py
+++ 0001_0599/378.py
@@ -1,15 +1,33 @@
 class Solution:
-    def kthSmallest(self, matrix: 'List[List[int]]', k: int) -> int:
+    def kthSmallest(self, matrix: 'List[List[int]]', k: int) -> int: #O( NlogN | N )
         pq = []
-        heapq.heapify(pq)
-        for r in matrix:
-            for c in r:
-                heapq.heappush(pq, c)
-        cnt = 0
-        while cnt < k-1:
-            heapq.heappop(pq)
-            cnt += 1
-        return heapq.heappop(pq)
+        for i, r in enumerate(matrix):
+            heapq.heappush(pq, (r[0], i, 0)) #output, row, col
+        while k:
+            output, r, c = heapq.heappop(pq)
+            if c+1 < len(matrix[0]):
+                heapq.heappush(pq, (matrix[r][c+1], r, c+1)) # add the next element on the same row to the heap
+            k-=1
+        return output
+
+
+
+
+
+# previous solution
+
+# class Solution:
+#     def kthSmallest(self, matrix: 'List[List[int]]', k: int) -> int:
+#         pq = []
+#         heapq.heapify(pq)
+#         for r in matrix:
+#             for c in r:
+#                 heapq.heappush(pq, c)
+#         cnt = 0
+#         while cnt < k-1:
+#             heapq.heappop(pq)
+#             cnt += 1
+#         return heapq.heappop(pq)
 
 
 # previous approach
