317bc8c364cbc0c6a9da09ce5641f4875de45616
Renjie Liu <anlrj@qq.com>
2022-08-06 07:57:33 -0400

    458

--- 0001_0599/458.py
+++ 0001_0599/458.py
@@ -1,6 +1,17 @@
-import math;
-
 class Solution:
-    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int: # RL 20220805 copied solution, O( 1 | 1 )
         states = minutesToTest // minutesToDie + 1
         return math.ceil(math.log(buckets) / math.log(states))
+    
+    
+
+# previous solution 
+
+# import math;
+
+# class Solution:
+#     def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+#         states = minutesToTest // minutesToDie + 1
+#         return math.ceil(math.log(buckets) / math.log(states))
+
+
