6976513f188a0dfd08bd03ad0318ac746b2d4c76
Renjie Liu <anlrj@qq.com>
2022-08-08 17:16:37 -0400

    2364

---
+++ 2000_2499/2364.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countBadPairs(self, nums: 'List[int]') -> int: #O( N | N )
+        gauss = lambda x: x*(1+x) //2 
+        N = len(nums)
+        total = gauss(N-1) # total for i < j is 1+2...+len(arr)-1
+        dp = defaultdict(lambda :0) 
+        good = 0 #good pairs are j - nums[j] == i - nums[i]
+        for i, n in enumerate(nums): # j-i != nums[j] - nums[i] ==> j - nums[j] != i - nums[i]
+            good += dp[i-n] # how many pairs with previous numbers can be formed with current number
+            dp[i-n] += 1
+        return total - good
+
+
+    
