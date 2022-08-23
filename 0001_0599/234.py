d2aa7ceede3247d527cdb9d3fb321527c2220397
Renjie Liu <anlrj@qq.com>
2022-08-23 08:41:18 -0400

    234.py

--- 0001_0599/234.py
+++ 0001_0599/234.py
@@ -1,12 +1,29 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def isPalindrome(self, head: ListNode) -> bool:
+    def isPalindrome(self, head: 'Optional[ListNode]') -> bool: # O( N | N )
         arr = []
-        while head:
+        while head: # flat and compare
             arr.append(head.val)
             head = head.next
         return arr == arr[::-1]
+    
+
+
+# previous solution
+
+# # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def isPalindrome(self, head: ListNode) -> bool:
+#         arr = []
+#         while head:
+#             arr.append(head.val)
+#             head = head.next
+#         return arr == arr[::-1]
