3bcef62531450de04a6410eb4ffb381e947ffc94
Renjie Liu <anlrj@qq.com>
2022-09-06 16:54:15 -0400

    2396.py

---
+++ 2000_2499/2396.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool: # O( 1 | 1 )
+        return False # n-2 base will always be 12, therefore, the result is always be false
+    
+
+# previous approach
+
+# class Solution:
+#     def isStrictlyPalindromic(self, n: int) -> bool: # O( NlogN | 1 )
+#         def cast(v, base): #cast v to base
+#             output = ""
+#             while v > 0:
+#                 output = str(v%base) + output
+#                 v //= base
+#             return output
+        
+#         for base in range(2, n-1): #cast all the numbers to different base, and check if it's palindromic
+#             t = cast(n, base)
+#             if t != t[::-1]:
+#                 return False
+#         return True
+    
