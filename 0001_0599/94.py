170b02ee5a28e695bf9e59329a4f9a9bb3d79016
Renjie Liu <anlrj@qq.com>
2022-09-07 20:20:02 -0400

    94.py

--- 0001_0599/94.py
+++ 0001_0599/94.py
@@ -1,24 +1,42 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def inorderTraversal(self, root: TreeNode) -> 'List[int]':
-        def flat(output, node):
-            if node.left == node.right ==None:
-                output.append(node.val)
-            else:
-                if node.left != None:
-                    flat(output, node.left)
-                output.append(node.val)
-                if node.right !=None:
-                    flat(output, node.right)
+    def inorderTraversal(self, root: 'Optional[TreeNode]') -> 'List[int]': # O( N | 1 )
+        if root:
+            return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right) # left, curr, right
+        return []
+        
 
-        output = []
-        if root == None: return output
-        else:
-            flat(output, root)
-        return output
+
+# previous solution
+
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# class Solution:
+#     def inorderTraversal(self, root: TreeNode) -> 'List[int]':
+#         def flat(output, node):
+#             if node.left == node.right ==None:
+#                 output.append(node.val)
+#             else:
+#                 if node.left != None:
+#                     flat(output, node.left)
+#                 output.append(node.val)
+#                 if node.right !=None:
+#                     flat(output, node.right)
+
+#         output = []
+#         if root == None: return output
+#         else:
+#             flat(output, root)
+#         return output
+
+
