e7575f273b1209d3bccffc10697109c2eea277e6
Renjie Liu <anlrj@qq.com>
2022-09-11 14:32:33 -0400

    1383.py

--- 1001_1499/1383.py
+++ 1001_1499/1383.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxPerformance(self, n: int, speed: 'List[int]', efficiency: 'List[int]', k: int) -> int:
-        eng = sorted(list(zip(speed, efficiency)), reverse = True, key = lambda x: x[1])
+        eng = sorted(list(zip(speed, efficiency)), reverse = True, key = lambda x: x[1]) 
         totalSpeed =0
         output = 0
         pq = []
@@ -12,6 +12,26 @@ class Solution:
             totalSpeed += s
             output = max(output, totalSpeed * eff)
         return output % (10**9+7)
+    
+
+    
+
+# previous solution 
+
+# class Solution:
+#     def maxPerformance(self, n: int, speed: 'List[int]', efficiency: 'List[int]', k: int) -> int:
+#         eng = sorted(list(zip(speed, efficiency)), reverse = True, key = lambda x: x[1])
+#         totalSpeed =0
+#         output = 0
+#         pq = []
+#         heapq.heapify(pq)
+#         for s , eff in eng:
+#             if len(pq) > k-1:
+#                 totalSpeed -= heapq.heappop(pq) #lowest speed in the pq
+#             heapq.heappush(pq, s)
+#             totalSpeed += s
+#             output = max(output, totalSpeed * eff)
+#         return output % (10**9+7)
 
 
 # previous approach
