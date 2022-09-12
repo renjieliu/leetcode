a80365a254f445b23b16e4204aba3b4615972b7e
Renjie Liu <anlrj@qq.com>
2022-09-12 08:56:46 -0400

    948.py

--- 0600_0999/948.py
+++ 0600_0999/948.py
@@ -1,15 +1,41 @@
-class Solution(object):
-    def bagOfTokensScore(self, tokens, P):
+class Solution:
+    def bagOfTokensScore(self, tokens: 'List[int]', power: int) -> int: # O( NlogN | 1 )
         tokens.sort()
-        output = curr = 0
-        while tokens and (P >= tokens[0] or curr!=0):
-            while tokens and P >= tokens[0]:
-                P -= tokens.pop(0)
-                curr += 1
-            output = max(output, curr)
-
-            if tokens and curr:
-                P += tokens.pop()
-                curr -= 1
-
+        score = 0 
+        output = 0
+        while tokens:
+            if power >= tokens[0]:
+                power -= tokens.pop(0) #take out the smallest power
+                score +=1
+                output = max(score, output)
+            elif score > 0: # add the largest power
+                power += tokens.pop()
+                score -= 1
+                output = max(score, output)
+            else: #not enough power or score to play the next round
+                return output          
         return output
+    
+
+
+
+# previous solution
+
+# class Solution(object):
+#     def bagOfTokensScore(self, tokens, P):
+#         tokens.sort()
+#         output = curr = 0
+#         while tokens and (P >= tokens[0] or curr!=0):
+#             while tokens and P >= tokens[0]:
+#                 P -= tokens.pop(0)
+#                 curr += 1
+#             output = max(output, curr)
+
+#             if tokens and curr:
+#                 P += tokens.pop()
+#                 curr -= 1
+
+#         return output
+
+
+
