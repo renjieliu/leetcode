bd90d011ac07da1f74026b0cc0ea9f66b2881d5c
Renjie Liu <anlrj@qq.com>
2022-09-19 08:58:47 -0400

    609.py

--- 0600_0999/609.py
+++ 0600_0999/609.py
@@ -1,21 +1,39 @@
 class Solution:
-    def findDuplicate(self, paths: 'List[str]') -> 'List[List[str]]':
-        hmp = {} #using a hashmap to store content as key, and array of the file path as value
+    def findDuplicate(self, paths: 'List[str]') -> 'List[List[str]]': # O( N*p | N ) P is the longest length of the path
+        files = defaultdict(lambda : [])
         for p in paths:
-            arr = p.split(' ')
-            d = arr[0]
-            for file in arr[1:]: # input: "root/a 1.txt(abcd) 2.txt(efgh)"
-                stk = file.split('(')
-                file = d+"/" + stk[0]
-                content = stk[1][:-1]
-                if content not in hmp:
-                    hmp[content] = []
-                hmp[content].append(file)
-        output = []
-        for k, v in hmp.items():
-            if len(v) > 1:
-                output.append(v)
-        return output
+            arr = p.split(' ') # split to folder, files
+            path = arr[0]
+            for f in arr[1:]: # go through files
+                file, content = f.split('(')
+                c = content[:-1] # take out the last ")"
+                files[c].append(path + '/' + file)
+        return [v for k,v in files.items() if len(v)> 1 ]
+    
+
+
+
+
+# previous solution
+
+# class Solution:
+#     def findDuplicate(self, paths: 'List[str]') -> 'List[List[str]]':
+#         hmp = {} #using a hashmap to store content as key, and array of the file path as value
+#         for p in paths:
+#             arr = p.split(' ')
+#             d = arr[0]
+#             for file in arr[1:]: # input: "root/a 1.txt(abcd) 2.txt(efgh)"
+#                 stk = file.split('(')
+#                 file = d+"/" + stk[0]
+#                 content = stk[1][:-1]
+#                 if content not in hmp:
+#                     hmp[content] = []
+#                 hmp[content].append(file)
+#         output = []
+#         for k, v in hmp.items():
+#             if len(v) > 1:
+#                 output.append(v)
+#         return output
 
 # previous approach
 # def findDuplicate(paths: 'List[str]'):
