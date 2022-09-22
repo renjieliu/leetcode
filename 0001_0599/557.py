cc63a9b9b3f34d3a4bac7a646e2a07b99c3519a8
Renjie Liu <anlrj@qq.com>
2022-09-22 08:59:56 -0400

    557.py

--- 0001_0599/557.py
+++ 0001_0599/557.py
@@ -1,16 +1,27 @@
-def reverseString(s):
-    output = ""
-    for i in range(len(s)-1, -1,-1):
-        output += s[i]
-    return  output
-
-def reverseWords(s):
-    wordList = list()
-    wordList = str(s).split(" ")
-    output  = ""
-    for i in range(0,len(wordList)):
-        output+=reverseString(wordList[i]) + " "
-    return  output.strip(" ")
+class Solution:
+    def reverseWords(self, s: str) -> str: # O( N | N )
+        return ' '.join(_[::-1] for _ in s.split(' '))  #split the s, and reverse each part
+    
 
 
-print(reverseWords("Let's take LeetCode contest"))
+# previous solution
+
+# def reverseString(s):
+#     output = ""
+#     for i in range(len(s)-1, -1,-1):
+#         output += s[i]
+#     return  output
+
+# def reverseWords(s):
+#     wordList = list()
+#     wordList = str(s).split(" ")
+#     output  = ""
+#     for i in range(0,len(wordList)):
+#         output+=reverseString(wordList[i]) + " "
+#     return  output.strip(" ")
+
+
+# print(reverseWords("Let's take LeetCode contest"))
+
+
+
