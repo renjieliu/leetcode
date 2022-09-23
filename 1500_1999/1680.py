6a98542c15ec51bfafd3680c5751b3fe6d65eb39
Renjie Liu <anlrj@qq.com>
2022-09-23 09:10:13 -0400

    1680.py

--- 1500_1999/1680.py
+++ 1500_1999/1680.py
@@ -1,9 +1,21 @@
 class Solution:
-    def concatenatedBinary(self, n: int) -> int:
-        s = ""
-        for i in range(1, n+1):
-            s+= bin(i).replace('0b', '')
-        return int(s, 2) % (10**9+7)
+    def concatenatedBinary(self, n: int) -> int: # O( N | 1 )
+        curr = ""
+        for i in range(1, n+1): # go through from 1 to n, convert to binary, concatenate, and calc the result 
+            curr += bin(i).replace('0b', '')
+        return int(curr, 2) % (10**9+7)
+
+
+
+
+# previous solution
+
+# class Solution:
+#     def concatenatedBinary(self, n: int) -> int:
+#         s = ""
+#         for i in range(1, n+1):
+#             s+= bin(i).replace('0b', '')
+#         return int(s, 2) % (10**9+7)
 
 # previous approach
 # class Solution:
