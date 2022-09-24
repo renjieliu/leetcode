bfa061ca03a9124b1f5c38f46c9767ce1740b885
Renjie Liu <anlrj@qq.com>
2022-09-24 08:16:54 -0400

    113.py

--- 0001_0599/113.py
+++ 0001_0599/113.py
@@ -1,28 +1,55 @@
 # Definition for a binary tree node.
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def pathSum(self, root: TreeNode, targetSum: int) -> 'List[List[int]]':
-        def dfs(output, node, curr, total, target):
+    def pathSum(self, root: 'Optional[TreeNode]', targetSum: int) -> 'List[List[int]]': # O( N | N )
+        def dfs(output, curr, total, node,target): # flat and check each node and path, and see if the path sum up to target
             if node.left == node.right == None:
-                if total == target:
-                    output.append(curr)
-            else:
-                if node.left:
-                    dfs(output, node.left, curr+[node.left.val], total+node.left.val, target)
-                if node.right:
-                    dfs(output, node.right, curr+[node.right.val], total+node.right.val, target)
-
+                if total + node.val == target:
+                    output.append(curr + [node.val])
+            if node.left:
+                dfs(output, curr + [node.val], total + node.val, node.left, target)
+            if node.right:
+                dfs(output, curr + [node.val], total + node.val, node.right, target)
+        
         if root == None:
             return []
         output = []
-        dfs(output, root, [root.val], root.val, targetSum)
+        dfs(output, [], 0, root, targetSum)
         return output
+    
+
+
+# previous solution
+
+# # Definition for a binary tree node.
+
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class Solution:
+#     def pathSum(self, root: TreeNode, targetSum: int) -> 'List[List[int]]':
+#         def dfs(output, node, curr, total, target):
+#             if node.left == node.right == None:
+#                 if total == target:
+#                     output.append(curr)
+#             else:
+#                 if node.left:
+#                     dfs(output, node.left, curr+[node.left.val], total+node.left.val, target)
+#                 if node.right:
+#                     dfs(output, node.right, curr+[node.right.val], total+node.right.val, target)
+
+#         if root == None:
+#             return []
+#         output = []
+#         dfs(output, root, [root.val], root.val, targetSum)
+#         return output
 
 
 
