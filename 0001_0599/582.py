d7d602f00648a2fec8ad1b8d99a6923a338fee10
Renjie Liu <anlrj@qq.com>
2022-09-29 09:27:53 -0400

    582.py

--- 0001_0599/582.py
+++ 0001_0599/582.py
@@ -1,29 +1,54 @@
 class Solution:
-    def killProcess(self, pid: 'List[int]', ppid: 'List[int]', kill: int) -> 'List[int]':
-        prc = {}
-        for i, p in enumerate(ppid):
-            if p not in prc:
-                prc[p] = []
-            prc[p].append(pid[i])
-        # print(prc)
-        output = [kill]
-        if kill not in prc: #it does not have a child process.
-            return output
-        else:
-            stk = prc[kill] #first layer processes mapped to kill
-            del prc[kill]
-            while stk: #BFS to keep finding the child processes to kill
-                nxt = []
-                while stk:
-                    child = stk.pop(0)
-                    output.append(child)
-                    if child in prc:
-                        nxt += prc[child]
-                        del prc[child]
-                stk = nxt
+    def killProcess(self, pid: 'List[int]', ppid: 'List[int]', kill: int) -> 'List[int]': # O( N | N )
+        follow = {}
+        for i, p in enumerate(ppid): #generate the follower hashmap
+            if p not in follow:
+                follow[p] = []
+            follow[p].append(pid[i])
+        
+        def dfs(output, follow, curr):  #go through all the followers from curr node
+            output.append(curr)
+            if curr in follow:
+                for nxt in follow[curr]:
+                    dfs(output, follow, nxt)
+        
+        output = []
+        dfs(output, follow, kill)
+        return output
+    
 
 
-            return output
+# previous solution
+
+# class Solution:
+#     def killProcess(self, pid: 'List[int]', ppid: 'List[int]', kill: int) -> 'List[int]':
+#         prc = {}
+#         for i, p in enumerate(ppid):
+#             if p not in prc:
+#                 prc[p] = []
+#             prc[p].append(pid[i])
+#         # print(prc)
+#         output = [kill]
+#         if kill not in prc: #it does not have a child process.
+#             return output
+#         else:
+#             stk = prc[kill] #first layer processes mapped to kill
+#             del prc[kill]
+#             while stk: #BFS to keep finding the child processes to kill
+#                 nxt = []
+#                 while stk:
+#                     child = stk.pop(0)
+#                     output.append(child)
+#                     if child in prc:
+#                         nxt += prc[child]
+#                         del prc[child]
+#                 stk = nxt
+
+
+#             return output
+
+
+
 
 #previous DFS approach
 # class Solution:
