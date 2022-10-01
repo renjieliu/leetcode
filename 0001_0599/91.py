1b60d240e3986048a676966b8860660c510818b6
Renjie Liu <anlrj@qq.com>
2022-10-01 10:50:27 -0400

    91.py

--- 0001_0599/91.py
+++ 0001_0599/91.py
@@ -1,31 +1,66 @@
 class Solution:
-    def numDecodings(self, s: str) -> int:
-        def helper(dp, s, loc):
-            if loc in dp:
-                return dp[loc]
+    def numDecodings(self, s: str) -> int: # O( N | N )
+        def helper(hmp, s): #store the current s in the hmp, to avoid double counting
+            if s in hmp:
+                return hmp[s]
             else:
-                if loc >= len(s):
-                    dp[loc] = 1
-                elif len(s[loc:]) == 1:
-                    dp[loc] = 0 if s[loc:] == "0" else 1
-                else:
-                    if s[loc] == "0": #cannot start the number with 0
-                        dp[loc] = 0
-                    elif int(s[loc:loc+2]) <= 26:
-                        if s[loc+1] == "0": #for 10, 20, need to take first 2 character
-                            dp[loc] = helper(dp, s, loc+2)
-                        else:
-                            A = helper(dp, s, loc+1) #take away first number, calc the rest
-                            B = helper(dp, s, loc+2) #take away first two numbers, calc the rest
-                            dp[loc] = A + B
+                hmp[s] = 0 
+                if s and s[0] != "0":
+                    if len(s) == 1:
+                        hmp[s] = 1
                     else:
-                        dp[loc] = helper(dp, s, loc+1)
-                return dp[loc]
+                        if int(s[:2])<=26:
+                            if s[1] == "0": # take the first 2 characters as a whole
+                                hmp[s] = helper(hmp, s[2:])
+                            else:
+                                A = helper(hmp, s[1:])
+                                B = helper(hmp, s[2:])
+                                hmp[s] = A + B
+                        else:
+                            hmp[s] = helper(hmp, s[1:])
+                # print(hmp)
+                return hmp[s]
 
-        dp = {}
-        helper(dp, s, 0)
-        #print(dp)
-        return dp[0] # if 0 not in dp.values() else 0
+        return helper({"": 1}, s) #if the string can be used as a whole - "" remaining, it's 1 way to decode.
+                        
+                            
+                
+
+                
+
+
+
+
+# previous solution
+
+# class Solution:
+#     def numDecodings(self, s: str) -> int:
+#         def helper(dp, s, loc):
+#             if loc in dp:
+#                 return dp[loc]
+#             else:
+#                 if loc >= len(s):
+#                     dp[loc] = 1
+#                 elif len(s[loc:]) == 1:
+#                     dp[loc] = 0 if s[loc:] == "0" else 1
+#                 else:
+#                     if s[loc] == "0": #cannot start the number with 0
+#                         dp[loc] = 0
+#                     elif int(s[loc:loc+2]) <= 26:
+#                         if s[loc+1] == "0": #for 10, 20, need to take first 2 character
+#                             dp[loc] = helper(dp, s, loc+2)
+#                         else:
+#                             A = helper(dp, s, loc+1) #take away first number, calc the rest
+#                             B = helper(dp, s, loc+2) #take away first two numbers, calc the rest
+#                             dp[loc] = A + B
+#                     else:
+#                         dp[loc] = helper(dp, s, loc+1)
+#                 return dp[loc]
+
+#         dp = {}
+#         helper(dp, s, 0)
+#         #print(dp)
+#         return dp[0] # if 0 not in dp.values() else 0
 
 
 
