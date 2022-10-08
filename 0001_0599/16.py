695f2eb194a42bd66ea24efaf3db03722d9c1423
Renjie Liu <anlrj@qq.com>
2022-10-08 10:10:08 -0400

    16.py

--- 0001_0599/16.py
+++ 0001_0599/16.py
@@ -1,39 +1,68 @@
 class Solution:
-    def threeSumClosest(self, nums: 'List[int]', target: int) -> int:
-        def binFind(arr, v): #to find the closest element in arrr to v
-            if v >= arr[-1]:
-                return arr[-1]
-            elif v <= arr[0]:
-                return arr[0]
-            else:
-                s = 0
-                e = len(arr)-1
-                diff = float('inf')
-                while s<=e:
-                    mid = (s+e)//2
-                    if arr[mid] == v:
-                        return arr[mid]
-                    else:
-                        if abs(arr[mid] - v) < diff:
-                            diff = abs(arr[mid]-v)
-                            output = arr[mid]
-                        if arr[mid] > v:
-                            e = mid - 1
-                        elif arr[mid] <  v:
-                            s = mid + 1
-                return output
-
-        output = None
+    def threeSumClosest(self, nums: 'List[int]', target: int) -> int: # O( N**2 | N )
         diff = float('inf')
-        nums.sort()
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)-1):
-                curr = nums[i] + nums[j]
-                t = binFind(nums[j+1:], target - curr) #for each curr, find the closet to the remaining in arr
-                if abs(curr + t - target) < diff: #current found is the closest to the target
-                    diff = abs(curr + t - target)
-                    output = curr + t
-        return output
+        nums.sort() 
+        for i in range(len(nums)):
+            l = i+1
+            r = len(nums)-1 
+            while l < r:
+                total = nums[i] + nums[l] + nums[r] 
+                if abs(target - total) < abs(diff): #current dist is closer than prev
+                    diff = target - total #compare diff with abs, but assign the real diff
+                if total < target:
+                    l += 1 
+                else:
+                    r -= 1 
+                
+                if diff == 0 : # found the exact numbers
+                    return target
+        return target - diff #target - diff is the actual total 3 sum can be found in the nums
+                
+
+
+
+# previous solution
+
+# class Solution:
+#     def threeSumClosest(self, nums: 'List[int]', target: int) -> int:
+#         def binFind(arr, v): #to find the closest element in arrr to v
+#             if v >= arr[-1]:
+#                 return arr[-1]
+#             elif v <= arr[0]:
+#                 return arr[0]
+#             else:
+#                 s = 0
+#                 e = len(arr)-1
+#                 diff = float('inf')
+#                 while s<=e:
+#                     mid = (s+e)//2
+#                     if arr[mid] == v:
+#                         return arr[mid]
+#                     else:
+#                         if abs(arr[mid] - v) < diff:
+#                             diff = abs(arr[mid]-v)
+#                             output = arr[mid]
+#                         if arr[mid] > v:
+#                             e = mid - 1
+#                         elif arr[mid] <  v:
+#                             s = mid + 1
+#                 return output
+
+#         output = None
+#         diff = float('inf')
+#         nums.sort()
+#         for i in range(len(nums)-1):
+#             for j in range(i+1, len(nums)-1):
+#                 curr = nums[i] + nums[j]
+#                 t = binFind(nums[j+1:], target - curr) #for each curr, find the closet to the remaining in arr
+#                 if abs(curr + t - target) < diff: #current found is the closest to the target
+#                     diff = abs(curr + t - target)
+#                     output = curr + t
+#         return output
+
+
+
+
 
 
 # previous approach
