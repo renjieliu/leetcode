fee9fa5a6558104d1f49e6df93349266353b64cd
Renjie Liu <anlrj@qq.com>
2022-10-13 08:57:50 -0400

    237.py

--- 0001_0599/237.py
+++ 0001_0599/237.py
@@ -5,17 +5,36 @@
 #         self.next = None
 
 class Solution:
-    def deleteNode(self, node): # O( N | 1 )
+    def deleteNode(self, node): # O( 1 | 1 )
         """
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        while node.next: # exist next node
-            node.val = node.next.val #mode next node val to current node 
-            if node.next.next: # if the next node is not the last node
-                node = node.next
-            else: # next node is the last node, trim it, and return the result
-                node.next = None 
+        node.val = node.next.val # move the next to current 
+        node.next = node.next.next # advance the pointer
+    
+
+
+# previous solution
+
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.next = None
+
+# class Solution:
+#     def deleteNode(self, node): # O( N | 1 )
+#         """
+#         :type node: ListNode
+#         :rtype: void Do not return anything, modify node in-place instead.
+#         """
+#         while node.next: # exist next node
+#             node.val = node.next.val #mode next node val to current node 
+#             if node.next.next: # if the next node is not the last node
+#                 node = node.next
+#             else: # next node is the last node, trim it, and return the result
+#                 node.next = None 
 
 
 
