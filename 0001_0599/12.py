75f75061ee6dafb49b8c59864e539b756d96dd0d
Renjie Liu <anlrj@qq.com>
2022-10-20 09:03:08 -0400

    12.py

--- 0001_0599/12.py
+++ 0001_0599/12.py
@@ -1,11 +1,27 @@
 class Solution:
-    def intToRoman(self, num: int) -> str:
-        lkp = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"}
+    def intToRoman(self, num: int) -> str: # O( logN | 1 )
+        roman = [[1000, 'M'] ,[900, 'CM'] ,[500, 'D'] ,[400, 'CD'] ,[100, 'C'] ,[90, 'XC'] ,[50, 'L'] ,[40, 'XL'] ,[10, 'X'] ,[9, 'IX'] ,[5, 'V'] ,[4, 'IV'] ,[1, 'I']]
         output = ""
-        for k in sorted(lkp.keys(), reverse = True):
-            output += lkp[k] * (num//k)
-            num %= k
+        for rm, c in roman: # go through the number from the arr from the largest
+            if num >= rm :
+                output += c * (num // rm ) #find the roman number, and repeat times
+                num %= rm # mod the current number by roman number
         return output
+    
+
+
+
+
+# previous solution 
+
+# class Solution:
+#     def intToRoman(self, num: int) -> str:
+#         lkp = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"}
+#         output = ""
+#         for k in sorted(lkp.keys(), reverse = True):
+#             output += lkp[k] * (num//k)
+#             num %= k
+#         return output
 
 # previous approach
 # class Solution:
