776d4f3a1b3fae0c88be80bd6c9b5ef1eb9fbaf2
Renjie Liu <anlrj@qq.com>
2022-10-29 08:29:14 -0400

    2136.py

---
+++ 2000_2499/2136.py
@@ -0,0 +1,12 @@
+class Solution:
+    def earliestFullBloom(self, plantTime: 'List[int]', growTime: 'List[int]') -> int: # RL 20221029 copied solution, O( NlogN | n )
+        cur_plant_time = 0
+        result = 0
+        indices = sorted(range(len(plantTime)), key=lambda x: -growTime[x]) 
+        for i in indices:
+            cur_plant_time += plantTime[i]
+            result = max(result, cur_plant_time + growTime[i])
+        return result
+
+
+    
