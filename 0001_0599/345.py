21027b746eb60fb3dc5637ec85cc64876c964e5b
Renjie Liu <anlrj@qq.com>
2022-11-04 09:05:10 -0400

    345.py

--- 0001_0599/345.py
+++ 0001_0599/345.py
@@ -1,19 +1,40 @@
 class Solution:
     def reverseVowels(self, s: str) -> str: # O( N | N )
-        loc = [] # record all the vowel location
+        s = list(s)
+        l = 0 
+        r = len(s)-1 
+        while l <= r: # check vowels from start
+            if s[l].lower() in "aeiou": # meet vowel
+                while r >= l: # searching for the counterpart from the end, and swap
+                    if s[r].lower() in "aeiou":
+                        s[l], s[r] = s[r], s[l] #swap s[l] and s[r]
+                        r-=1 # move the right pointer after swap
+                        break
+                    r -= 1 
+            l += 1 # advance the left pointer
         
-        for i, c in enumerate(s):
-            if c.lower() in "aeiou":
-                loc.append(i)
-        output = ""
+        return "".join(s)
+
+
+
+# previous solution
+
+# class Solution:
+#     def reverseVowels(self, s: str) -> str: # O( N | N )
+#         loc = [] # record all the vowel location
         
-        for i, c in enumerate(s): # find the corresponding vowel from the end
-            if c.lower() in "aeiou":
-                output += s[loc.pop()]
-            else:
-                output += c
+#         for i, c in enumerate(s):
+#             if c.lower() in "aeiou":
+#                 loc.append(i)
+#         output = ""
         
-        return output
+#         for i, c in enumerate(s): # find the corresponding vowel from the end
+#             if c.lower() in "aeiou":
+#                 output += s[loc.pop()]
+#             else:
+#                 output += c
+        
+#         return output
 
 
 
