afc71ca5758b687a82f03fcea7675db9d30315ee
Renjie Liu <anlrj@qq.com>
2022-11-06 09:16:50 -0500

    899.py

--- 0600_0999/899.py
+++ 0600_0999/899.py
@@ -1,10 +1,28 @@
 class Solution:
-    def orderlyQueue(self, s: str, k: int) -> str:
-        if k == 1:  # try all the possible movements
+    def orderlyQueue(self, s: str, k: int) -> str: # O( NlogN | N )
+        if k == 1: #try all the possible movements
             arr = []
             for i in range(len(s)):
                 arr.append(s[i:] + s[:i])
             return min(arr)
-        else:  # it can always find a way to be fully sorted
+        else:#it can always find a way to be fully sorted
             return "".join(sorted(list(s)))
+        
+
+
+
+
+# previous solution
+
+# class Solution:
+#     def orderlyQueue(self, s: str, k: int) -> str:
+#         if k == 1:  # try all the possible movements
+#             arr = []
+#             for i in range(len(s)):
+#                 arr.append(s[i:] + s[:i])
+#             return min(arr)
+#         else:  # it can always find a way to be fully sorted
+#             return "".join(sorted(list(s)))
+
+
 
