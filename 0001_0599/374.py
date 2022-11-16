f85a3082a1bfd3e3b770df392ae6cff1468ae8e0
Renjie Liu <anlrj@qq.com>
2022-11-16 09:03:58 -0500

    374.py

--- 0001_0599/374.py
+++ 0001_0599/374.py
@@ -1,21 +1,49 @@
 # The guess API is already defined for you.
 # @param num, your guess
-# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
 # def guess(num: int) -> int:
 
 class Solution:
-    def guessNumber(self, n: int) -> int:
+    def guessNumber(self, n: int) -> int: # O( logN | 1 )
         s = 1 
-        e = n
-        while s <= e:
-            mid = s-(s-e)//2
-            t = guess(mid)
-            if t == 0:
+        e = n 
+        while s <= e: # binary search for the number picked
+            mid = s - (s-e)//2 
+            res = guess(mid) # internal API, as described in the problem
+            if res == 0: # number found
                 return mid
-            elif t == -1:
+            elif res == 1: # too small
+                s = mid + 1 
+            else: # too big
                 e = mid - 1
-            elif t == 1:
-                s = mid + 1
+            
+
+
+
+
+
+# previous solution
+
+# # The guess API is already defined for you.
+# # @param num, your guess
+# # @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# # def guess(num: int) -> int:
+
+# class Solution:
+#     def guessNumber(self, n: int) -> int:
+#         s = 1 
+#         e = n
+#         while s <= e:
+#             mid = s-(s-e)//2
+#             t = guess(mid)
+#             if t == 0:
+#                 return mid
+#             elif t == -1:
+#                 e = mid - 1
+#             elif t == 1:
+#                 s = mid + 1
         
 
 
