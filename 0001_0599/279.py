ccb462f931cd5b828ed784e00542f4d9f55f7a74
Renjie Liu <anlrj@qq.com>
2022-11-22 09:37:02 -0500

    279.py

--- 0001_0599/279.py
+++ 0001_0599/279.py
@@ -1,23 +1,39 @@
 class Solution:
-    def numSquares(self, n: int) -> int:
-        dp = [0] #initialize the dp, 0 as a dummy placeholder, to make the array lkp easier
-        lkp = []
-        for i in range(1, int(n**0.5) +1):
-            lkp.append(i**2)
-
-        for i in range(1, n+1): #to check the min to construct current number.
+    def numSquares(self, n: int) -> int: # O( N**(3/2) | N )
+        stk = [_**2 for _ in range(1, int(n**0.5)+1)] 
+        dp = [0, 1]  #[0 squares to form 0, 1 square to form 1]
+        for i in range(2, n+1): #for each number, find the minimum required
             curr = float('inf')
-            for v in lkp: 
-                if v <= i:
-                    if i % v == 0:
-                        curr = min(curr, i//v)
-                    else:
-                        curr = min(curr, i//v + dp[i%v]) #perfect square + remaining
-                else:
-                    break
+            for s in stk:
+                if i-s >= 0:
+                    curr = min(curr, dp[i-s] + 1)
             dp.append(curr)
-            # print(i, curr)
         return dp[-1]
+    
+
+
+# previous solution
+
+# class Solution:
+#     def numSquares(self, n: int) -> int:
+#         dp = [0] #initialize the dp, 0 as a dummy placeholder, to make the array lkp easier
+#         lkp = []
+#         for i in range(1, int(n**0.5) +1):
+#             lkp.append(i**2)
+
+#         for i in range(1, n+1): #to check the min to construct current number.
+#             curr = float('inf')
+#             for v in lkp: 
+#                 if v <= i:
+#                     if i % v == 0:
+#                         curr = min(curr, i//v)
+#                     else:
+#                         curr = min(curr, i//v + dp[i%v]) #perfect square + remaining
+#                 else:
+#                     break
+#             dp.append(curr)
+#             # print(i, curr)
+#         return dp[-1]
     
 
 
