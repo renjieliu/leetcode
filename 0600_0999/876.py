99664d6683f2357c5332b14c1e289633a8c2bf05
Renjie Liu <anlrj@qq.com>
2022-12-05 09:01:37 -0500

    876.py

--- 0600_0999/876.py
+++ 0600_0999/876.py
@@ -4,6 +4,34 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def middleNode(self, head: 'Optional[ListNode]') -> 'Optional[ListNode]': # O( N | 1 )
+        def helper(output, node, idx): 
+            if node.next == None:
+                if idx == (idx+1) // 2: #idx is total // 2. Since current node is the last one, the total is idx+1
+                    output[0] = node 
+                return idx+1 # total nodes in the list
+            else: 
+                total = helper(output, node.next, idx+1)
+                if idx == total // 2:
+                    output[0] = node 
+                return total
+        
+        output = [None]
+        helper(output, head, 0)
+        return output[0]
+
+
+
+
+
+# previous solution
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
     def middleNode(self, head: 'Optional[ListNode]') -> 'Optional[ListNode]':
         output = [None] 
         def helper(output, node, level): #check how many levels in total, and if current level is total//2+1 (the middle node)
