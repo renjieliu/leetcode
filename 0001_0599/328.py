8f7799dcb9d892e439fc77ba30dca735b4d4a84c
Renjie Liu <anlrj@qq.com>
2022-12-05 20:35:59 -0500

    328.py

--- 0001_0599/328.py
+++ 0001_0599/328.py
@@ -3,32 +3,72 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution: # O(N) + O(1) space
-    def oddEvenList(self, head: 'Optional[ListNode]') -> 'Optional[ListNode]':
-        if head == None:
-            return None 
-        start = head #the return start
-        prevOdd = head  #previous odd
-        prevEven = None #previous even 
-        firstEven = None #first Even. the most recent found odd node need to point to this one as next 
+class Solution:
+    def oddEvenList(self, head: 'Optional[ListNode]') -> 'Optional[ListNode]': # O( N | N )
+        if head == None: 
+            return head 
+        odd = []
+        even = []
         i = 1 
-        head = head.next 
-        while head:
-            i += 1
-            if i % 2: 
-                t = head.next #save the curent next pointer
-                prevOdd.next = head
-                prevEven.next = head.next #point the prevEven to next
-                head.next = firstEven # move curr odd the last odd sequence, and point to the first Even
-                prevOdd = head
-                head = t
+        while head: # flat based on the index and generate a new list
+            if i % 2:
+                odd.append(head.val)
             else:
-                if firstEven == None: #save the first even
-                    firstEven = head
-                prevEven = head  #save current node as prevEven
-                head = head.next # move to the next 
+                even.append(head.val)
+            i+=1
+            head = head.next
+        
+
+        
+        newRoot = ListNode(odd.pop(0))
+        curr = newRoot
+        while odd:
+            curr.next = ListNode(odd.pop(0))
+            curr = curr.next
+
+        while even:
+            curr.next = ListNode(even.pop(0))
+            curr = curr.next
+
+        return newRoot 
+    
+    
+    
+    
+    
+# previous solution
+
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution: # O(N) + O(1) space
+#     def oddEvenList(self, head: 'Optional[ListNode]') -> 'Optional[ListNode]':
+#         if head == None:
+#             return None 
+#         start = head #the return start
+#         prevOdd = head  #previous odd
+#         prevEven = None #previous even 
+#         firstEven = None #first Even. the most recent found odd node need to point to this one as next 
+#         i = 1 
+#         head = head.next 
+#         while head:
+#             i += 1
+#             if i % 2: 
+#                 t = head.next #save the curent next pointer
+#                 prevOdd.next = head
+#                 prevEven.next = head.next #point the prevEven to next
+#                 head.next = firstEven # move curr odd the last odd sequence, and point to the first Even
+#                 prevOdd = head
+#                 head = t
+#             else:
+#                 if firstEven == None: #save the first even
+#                     firstEven = head
+#                 prevEven = head  #save current node as prevEven
+#                 head = head.next # move to the next 
                 
-        return start
+#         return start
                 
             
                 
