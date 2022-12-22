147486cd8da2b29e684c5af581be1320ae9fb284
Renjie Liu <anlrj@qq.com>
2022-12-22 09:05:58 -0500

    1167.py

--- 1001_1499/1167.py
+++ 1001_1499/1167.py
@@ -1,10 +1,25 @@
 class Solution:
-    def connectSticks(self, sticks: 'List[int]') -> int:
-        heapq.heapify(sticks) #using a heap to pop smallest 2 items each time, and add the sum back to the heap
-        cost = 0
-        while len(sticks) > 1 :
-            curr = heapq.heappop(sticks) + heapq.heappop(sticks)
-            cost += curr
-            heapq.heappush(sticks, curr)
-        return cost
+    def connectSticks(self, sticks: 'List[int]') -> int: # O( NlogN | N )
+        total = 0 
+        heapq.heapify(sticks)
+        while len(sticks) > 1: #greedy, to take the smallest 2, add together and push back
+            nxt = heapq.heappop(sticks) + heapq.heappop(sticks)
+            total += nxt 
+            heapq.heappush(sticks, nxt) 
+        return total
+
+    
+
+
+# previous solution
+
+# class Solution:
+#     def connectSticks(self, sticks: 'List[int]') -> int:
+#         heapq.heapify(sticks) #using a heap to pop smallest 2 items each time, and add the sum back to the heap
+#         cost = 0
+#         while len(sticks) > 1 :
+#             curr = heapq.heappop(sticks) + heapq.heappop(sticks)
+#             cost += curr
+#             heapq.heappush(sticks, curr)
+#         return cost
 
