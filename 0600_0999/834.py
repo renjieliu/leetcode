4d70abf98682db7d1154ed77450c9f264846ded6
Renjie Liu <anlrj@qq.com>
2022-12-22 09:03:53 -0500

    834.py

--- 0600_0999/834.py
+++ 0600_0999/834.py
@@ -1,5 +1,5 @@
 class Solution: 
-    def sumOfDistancesInTree(self, n: int, edges: 'List[List[int]]') -> 'List[int]': #RL 20221221: Copied solution, O( N | N )
+    def sumOfDistancesInTree(self, n: int, edges: 'List[List[int]]') -> 'List[int]': # O( N | N )
         graph = defaultdict(lambda: set())
         for u, v in edges:
             graph[u].add(v)
@@ -7,22 +7,55 @@ class Solution:
 
         count = [1] * n
         ans = [0] * n
-        def dfs(node = 0, parent = None):
+        def dfs(count, ans, graph, node, parent):
             for child in graph[node]:
-                if child != parent:
-                    dfs(child, node)
+                if child != parent: # avoid loop
+                    dfs(count, ans, graph, child, node)
                     count[node] += count[child]
-                    ans[node] += ans[child] + count[child]
+                    ans[node] += ans[child] + count[child] #answer of child + how many nodes below (steps from curr to reach all the nodes below)
 
-        def dfs2(node = 0, parent = None):
+        def dfs2(count, ans, graph, node, parent):
             for child in graph[node]:
-                if child != parent:
-                    ans[child] = ans[node] - count[child] + n - count[child]
-                    dfs2(child, node)
+                if child!=parent: # avoid loop
+                    ans[child] = (ans[node] - count[child]) + (n - count[child]) # curr - child branch + (remaining count)
+                    dfs2(count, ans, graph, child, node)
 
-        dfs()
-        dfs2()
+        dfs(count, ans, graph, 0, None)
+        dfs2(count, ans, graph, 0 , None)
         return ans
+
+
+
+
+
+
+# previous solution
+
+# class Solution: 
+#     def sumOfDistancesInTree(self, n: int, edges: 'List[List[int]]') -> 'List[int]': #RL 20221221: Copied solution, O( N | N )
+#         graph = defaultdict(lambda: set())
+#         for u, v in edges:
+#             graph[u].add(v)
+#             graph[v].add(u)
+
+#         count = [1] * n
+#         ans = [0] * n
+#         def dfs(node = 0, parent = None):
+#             for child in graph[node]:
+#                 if child != parent:
+#                     dfs(child, node)
+#                     count[node] += count[child]
+#                     ans[node] += ans[child] + count[child]
+
+#         def dfs2(node = 0, parent = None):
+#             for child in graph[node]:
+#                 if child != parent:
+#                     ans[child] = ans[node] - count[child] + n - count[child]
+#                     dfs2(child, node)
+
+#         dfs()
+#         dfs2()
+#         return ans
 
 
 
