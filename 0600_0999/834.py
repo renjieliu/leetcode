b2d3ad843760d474128986563d65699195759a94
Renjie Liu <anlrj@qq.com>
2022-12-21 21:53:11 -0500

    834.py

--- 0600_0999/834.py
+++ 0600_0999/834.py
@@ -1,5 +1,5 @@
-class Solution:  # RL 20210904: Copied solution
-    def sumOfDistancesInTree(self, n: int, edges: 'List[List[int]]') -> 'List[int]':
+class Solution: 
+    def sumOfDistancesInTree(self, n: int, edges: 'List[List[int]]') -> 'List[int]': #RL 20221221: Copied solution, O( N | N )
         graph = defaultdict(lambda: set())
         for u, v in edges:
             graph[u].add(v)
@@ -7,15 +7,14 @@ class Solution:  # RL 20210904: Copied solution
 
         count = [1] * n
         ans = [0] * n
-
-        def dfs(node=0, parent=None):
+        def dfs(node = 0, parent = None):
             for child in graph[node]:
                 if child != parent:
                     dfs(child, node)
                     count[node] += count[child]
                     ans[node] += ans[child] + count[child]
 
-        def dfs2(node=0, parent=None):
+        def dfs2(node = 0, parent = None):
             for child in graph[node]:
                 if child != parent:
                     ans[child] = ans[node] - count[child] + n - count[child]
@@ -24,3 +23,37 @@ class Solution:  # RL 20210904: Copied solution
         dfs()
         dfs2()
         return ans
+
+
+
+# previous solution
+
+# class Solution:  # RL 20210904: Copied solution
+#     def sumOfDistancesInTree(self, n: int, edges: 'List[List[int]]') -> 'List[int]':
+#         graph = defaultdict(lambda: set())
+#         for u, v in edges:
+#             graph[u].add(v)
+#             graph[v].add(u)
+
+#         count = [1] * n
+#         ans = [0] * n
+
+#         def dfs(node=0, parent=None):
+#             for child in graph[node]:
+#                 if child != parent:
+#                     dfs(child, node)
+#                     count[node] += count[child]
+#                     ans[node] += ans[child] + count[child]
+
+#         def dfs2(node=0, parent=None):
+#             for child in graph[node]:
+#                 if child != parent:
+#                     ans[child] = ans[node] - count[child] + n - count[child]
+#                     dfs2(child, node)
+
+#         dfs()
+#         dfs2()
+#         return ans
+
+
+
