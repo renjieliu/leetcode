dff7c0761f1b867935729b8a54832f9b1f6ca02f
Renjie Liu <anlrj@qq.com>
2022-12-22 22:23:46 -0500

    309.py

--- 0001_0599/309.py
+++ 0001_0599/309.py
@@ -1,17 +1,33 @@
 class Solution:
-    def maxProfit(self, prices: 'List[int]') -> int:
-        dp = [0 for _ in range(len(prices))]
-        for buy in range(len(prices)-1,-1,-1): # assuming buying here
-            A = 0 
-            for sell in range(buy+1, len(prices)): #assuming selling here
-                profit = prices[sell] - prices[buy] 
-                A = max(A, profit + (0 if sell + 2 >= len(prices) else dp[sell+2]) )
-            
-            B = 0 if buy+1>=len(prices) else dp[buy+1]
-            
-            dp[buy] = max(A,B)
-        
+    def maxProfit(self, prices: 'List[int]') -> int: # O( N**2 | N )
+        dp = [0] * len(prices)
+        for i in range(len(prices)-1, -1, -1): # buy
+            curr = prices[i]
+            for j in range(i+1, len(prices)): # sell 
+                if prices[j] > curr:
+                    dp[i] = max(dp[i], prices[j] - curr + (dp[j+2] if j+2 < len(prices) else 0))
+            dp[i] = max(dp[i], 0 if i+1 >= len(prices) else dp[i+1]) #no action from current location 
         return dp[0]
+    
+
+
+
+# previous solution
+
+# class Solution:
+#     def maxProfit(self, prices: 'List[int]') -> int:
+#         dp = [0 for _ in range(len(prices))]
+#         for buy in range(len(prices)-1,-1,-1): # assuming buying here
+#             A = 0 
+#             for sell in range(buy+1, len(prices)): #assuming selling here
+#                 profit = prices[sell] - prices[buy] 
+#                 A = max(A, profit + (0 if sell + 2 >= len(prices) else dp[sell+2]) )
+            
+#             B = 0 if buy+1>=len(prices) else dp[buy+1]
+            
+#             dp[buy] = max(A,B)
+        
+#         return dp[0]
 
     
 
