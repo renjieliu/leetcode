e0c7860c559eb3b9febfef89c85d4356a7cb02c2
Renjie Liu <anlrj@qq.com>
2022-12-30 20:30:14 -0500

    980.py

--- 0600_0999/980.py
+++ 0600_0999/980.py
@@ -25,6 +25,36 @@ class Solution:
         return output[0]
     
     
+
+# previous solution
+
+# class Solution:
+#     def uniquePathsIII(self, grid: 'List[List[int]]') -> int:
+#         def dfs(output, seen, path, total, grid, r, c):
+#             if grid[r][c] == 2:
+#                 if path == total:
+#                     output[0] += 1 
+#             else:
+#                 direction = [[0,1],[1,0], [-1,0], [0,-1]]
+#                 for a, b in direction:
+#                     if -1<r+a<len(grid) and -1<c+b< len(grid[0]) and grid[r+a][c+b] not in (1, -1) and (r+a, c+b) not in seen:
+#                         seen.add((r+a, c+b))
+#                         dfs(output, seen, path+1, total, grid, r+a, c+b)
+#                         seen.remove((r+a, c+b))
+        
+#         total = 0 #all the cells with 0 and 2
+#         for r in range(len(grid)):
+#             for c in range(len(grid[0])):
+#                 if grid[r][c] == 1: 
+#                     sr = r
+#                     sc = c
+#                 elif grid[r][c] in (0,2):
+#                     total += 1 
+#         output = [0]
+#         dfs(output, set(), 0, total, grid, sr, sc)
+#         return output[0]
+    
+    
     
     
 
